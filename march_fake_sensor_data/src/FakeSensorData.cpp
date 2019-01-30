5c58a9b8278d4349e2729b47b9e56e0de7983260
tim <timbuckers@gmail.com>
2019-01-30 10:29:33 +0100

    sensors defined dynamic as param

--- march_fake_sensor_data/src/FakeSensorData.cpp
+++ march_fake_sensor_data/src/FakeSensorData.cpp
@@ -3,6 +3,8 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Temperature.h"
 #include <dynamic_reconfigure/server.h>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
 
 #include <march_shared_resources/TopicNames.h>
 
@@ -10,9 +12,15 @@
 
 int min_temperature = 0;
 int max_temperature = 50;
-std::vector<std::string> sensor_names = {"WRONG", "MORE_WRONG"};
+std::vector<std::string> sensor_names;
 std::vector<ros::Publisher> temperature_publishers;
 
+/**
+ * This callback is called when parameters from the config file are changed during run-time.
+ * This method updates the local values which depend on these parameters to make ensure the values are not out-of-date.
+ * @param config the config file with all the parameters
+ * @param level A bitmask
+ */
 void callback(march_fake_sensor_data::TemperaturesConfig& config, uint32_t level)
 {
   min_temperature = config.min_temperature;
@@ -39,13 +47,14 @@ void publishTemperature(ros::Publisher temperature_pub)
  * @param name the sensor name
  * @return
  */
-std::string createTopicName(const char* base, const char* name){
+std::string createTopicName(const char* base, const char* name)
+{
   char slash[] = "/";
   // Create a char array of the combined size of all three parts
   char full_topic[strlen(base) + strlen(slash) + strlen(name)];
-  strcpy(full_topic, base);      // copy string one into full_topic.
-  strcat(full_topic, slash);     // append the slash to full_topic.
-  strcat(full_topic, name);      // append the name two to full_topic.
+  strcpy(full_topic, base);   // copy string one into full_topic.
+  strcat(full_topic, slash);  // append the slash to full_topic.
+  strcat(full_topic, name);   // append the name two to full_topic.
   return full_topic;
 }
 
@@ -57,17 +66,15 @@ int main(int argc, char** argv)
 
   n.getParam(n.getNamespace() + "/min_temperature", min_temperature);
   n.getParam(n.getNamespace() + "/max_temperature", max_temperature);
-  n.getParam(n.getNamespace() + "/sensors", sensor_names);
-  //TODO(Tim) sensor_names is empty???
-  ROS_INFO("%i sensor names loaded", sensor_names.size());
+  n.getParam("/sensors", sensor_names);
 
+  // Create a publisher for each sensor
   for (std::string sensor_name : sensor_names)
   {
-    ros::Publisher temperature_pub = n.advertise<sensor_msgs::Temperature>(createTopicName(TopicNames::temperature, sensor_name.c_str()), 1000);
+    ros::Publisher temperature_pub =
+        n.advertise<sensor_msgs::Temperature>(createTopicName(TopicNames::temperature, sensor_name.c_str()), 1000);
     temperature_publishers.push_back(temperature_pub);
   }
-
-  ROS_INFO("%i temperature publishers created", temperature_publishers.size());
 
   // Make the temperature values dynamic reconfigurable
   dynamic_reconfigure::Server<march_fake_sensor_data::TemperaturesConfig> server;
@@ -76,6 +83,7 @@ int main(int argc, char** argv)
 
   while (ros::ok())
   {
+    // Loop through all publishers
     for (ros::Publisher temperature_pub : temperature_publishers)
     {
       publishTemperature(temperature_pub);
