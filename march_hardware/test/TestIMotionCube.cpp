3194deddd7c27b2a48f705522ff55fc5c32bc62b
tim <timbuckers@gmail.com>
2019-02-21 11:41:26 +0100

    change imotioncube tests to support constructor with encoder

--- march_hardware/test/TestIMotionCube.cpp
+++ march_hardware/test/TestIMotionCube.cpp
@@ -6,22 +6,27 @@
 class TestIMotionCube : public ::testing::Test
 {
 protected:
+  march4cpp::Encoder encoder;
+
+  void SetUp() override {
+    encoder = march4cpp::Encoder();
+  }
 };
 
 TEST_F(TestIMotionCube, SlaveIndexOne)
 {
- march4cpp::IMotionCube imc = march4cpp::IMotionCube(1);
+ march4cpp::IMotionCube imc = march4cpp::IMotionCube(1, encoder);
  ASSERT_EQ(1, imc.getSlaveIndex());
 }
 
 TEST_F(TestIMotionCube, SlaveIndexZero)
 {
-    ASSERT_THROW(march4cpp::IMotionCube(0), std::invalid_argument);
+    ASSERT_THROW(march4cpp::IMotionCube(0, encoder), std::invalid_argument);
 }
 
 TEST_F(TestIMotionCube, SlaveIndexMinusOne)
 {
-    ASSERT_THROW(march4cpp::IMotionCube(-1), std::invalid_argument);
+    ASSERT_THROW(march4cpp::IMotionCube(-1, encoder), std::invalid_argument);
 }
 
 TEST_F(TestIMotionCube, NoSlaveIndexConstructor)
