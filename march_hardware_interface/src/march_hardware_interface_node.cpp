fe6275c4709a2c8e1c86d2789a9f8f091bf0235f
Isha Dijcks <Ishadijcks@gmail.com>
2019-03-22 11:06:25 +0100

    Remove ros::spin

--- march_hardware_interface/src/march_hardware_interface_node.cpp
+++ march_hardware_interface/src/march_hardware_interface_node.cpp
@@ -8,6 +8,6 @@ int main(int argc, char** argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
   march_hardware_interface::MarchHardwareInterface march(nh);
-  ros::waitForShutdown()
+  ros::waitForShutdown();
   return 0;
 }
