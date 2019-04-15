986c1c3461e2e704b2092abf01d374cc15669a6f
Isha Dijcks <Ishadijcks@gmail.com>
2019-04-15 11:20:26 +0200

    Remove incorrect trajectory creation

--- march_gait_generator/src/TrajectoryPreview.cpp
+++ march_gait_generator/src/TrajectoryPreview.cpp
@@ -10,7 +10,5 @@ TrajectoryPreview::TrajectoryPreview(Gait gait) {
 
     trajectory_msgs::JointTrajectory trajectory = gait.toJointTrajectory();
 
-    joint_trajectory_controller::initJointTrajectory<KDL::Trajectory>(trajectory, ros::Time(0));
-
     ROS_INFO_STREAM(trajectory);
 }
