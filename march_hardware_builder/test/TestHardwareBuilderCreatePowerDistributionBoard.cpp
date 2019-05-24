5d41602c89d651595d0f1287f9b2d7a2822e2534
TimBuckers <timbuckers@gmail.com>
2019-05-24 14:57:21 +0200

    pdb tests for the hardware builder

--- march_hardware_builder/test/TestHardwareBuilderCreatePowerDistributionBoard.cpp
+++ march_hardware_builder/test/TestHardwareBuilderCreatePowerDistributionBoard.cpp
@@ -27,21 +27,39 @@ protected:
   }
 };
 
-// TEST_F(PowerDistributionBoardTest, ValidPowerDistributionBoard)
-//{
-//  std::string fullPath = this->fullPath("/power_distribution_board.yaml");
-//  YAML::Node config = YAML::LoadFile(fullPath);
-//
-//  march4cpp::PowerDistributionBoard createdPowerDistributionBoard =
-//      hardwareBuilder.createPowerDistributionBoard(config);
-//  march4cpp::PowerDistributionBoard powerDistributionBoard = march4cpp::PowerDistributionBoard(1, 0, 5);
-//
-//  ASSERT_EQ(powerDistributionBoard, createdPowerDistributionBoard);
-//}
-//
-// TEST_F(PowerDistributionBoardTest, MissingSlaveIndexPowerDistributionBoard)
-//{
-//  std::string fullPath = this->fullPath("/power_distribution_board_missing_slave_index.yaml");
-//  YAML::Node config = YAML::LoadFile(fullPath);
-//  ASSERT_THROW(hardwareBuilder.createPowerDistributionBoard(config), MissingKeyException);
-//}
+TEST_F(PowerDistributionBoardTest, ValidPowerDistributionBoard)
+{
+  std::string fullPath = this->fullPath("/power_distribution_board.yaml");
+  YAML::Node config = YAML::LoadFile(fullPath);
+
+  march4cpp::PowerDistributionBoard createdPowerDistributionBoard =
+      hardwareBuilder.createPowerDistributionBoard(config);
+  NetMonitorOffsets netMonitoringOffsets(5, 9, 13, 17, 3, 2, 1, 4);
+  NetDriverOffsets netDriverOffsets(4, 3, 2);
+  BootShutdownOffsets bootShutdownOffsets(0, 0, 1);
+  march4cpp::PowerDistributionBoard powerDistributionBoard =
+      march4cpp::PowerDistributionBoard(1, netMonitoringOffsets, netDriverOffsets, bootShutdownOffsets);
+
+  ASSERT_EQ(powerDistributionBoard, createdPowerDistributionBoard);
+}
+
+TEST_F(PowerDistributionBoardTest, MissingSlaveIndex)
+{
+  std::string fullPath = this->fullPath("/power_distribution_board_missing_slave_index.yaml");
+  YAML::Node config = YAML::LoadFile(fullPath);
+  ASSERT_THROW(hardwareBuilder.createPowerDistributionBoard(config), MissingKeyException);
+}
+
+TEST_F(PowerDistributionBoardTest, MissingHighVoltageStateIndex)
+{
+  std::string fullPath = this->fullPath("/power_distribution_board_missing_high_voltage_state_index.yaml");
+  YAML::Node config = YAML::LoadFile(fullPath);
+  ASSERT_THROW(hardwareBuilder.createPowerDistributionBoard(config), MissingKeyException);
+}
+
+TEST_F(PowerDistributionBoardTest, NegativeOffset)
+{
+  std::string fullPath = this->fullPath("/power_distribution_board_negative_offset.yaml");
+  YAML::Node config = YAML::LoadFile(fullPath);
+  ASSERT_THROW(hardwareBuilder.createPowerDistributionBoard(config), std::runtime_error);
+}
