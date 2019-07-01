9812c7ba27aebf62f10111d0b6f0a30a40c8189a
Isha Dijcks <ishadijcks@gmail.com>
2019-07-01 11:19:18 +0200

    Add hardware_state_controller to linter

--- build.sh
+++ build.sh
@@ -28,12 +28,14 @@ catkin lint -W2 --pkg march_hardware || build_failed "Catkin lint failed in marc
 catkin lint -W2 --pkg march_hardware_builder || build_failed "Catkin lint failed in march_hardware_builder"
 catkin lint -W2 --pkg march_hardware_interface || build_failed "Catkin lint failed in march_hardware_interface"
 catkin lint -W2 --pkg march_temperature_sensor_controller || build_failed "Catkin lint failed in march_temperature_sensor_controller"
+catkin lint -W2 --pkg march_hardware_state_controller || build_failed "Catkin lint failed in march_hardware_state_controller"
 
 # Roslint
 catkin build --no-deps --verbose march_hardware --no-notify --catkin-make-args roslint || build_failed "Roslint failed in march_hardware"
 catkin build --no-deps --verbose march_hardware_builder --no-notify --catkin-make-args roslint || build_failed "Roslint failed in march_hardware_builder"
 catkin build --no-deps --verbose march_hardware_interface --no-notify --catkin-make-args roslint || build_failed "Roslint failed in march_hardware_interface"
 catkin build --no-deps --verbose march_temperature_sensor_controller --no-notify --catkin-make-args roslint || build_failed "Roslint failed in march_temperature_sensor_controller"
+catkin build --no-deps --verbose march_hardware_state_controller --no-notify --catkin-make-args roslint || build_failed "Roslint failed in march_hardware_state_controller"
 
 # Run all tests in the workspace, including roslaunch-checks if they exist
 catkin build --summarize --catkin-make-args run_tests && catkin_test_results build/ --verbose || build_failed "Tests failed"
