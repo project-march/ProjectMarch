b5a87eb1fd301a20f2d8f72785022df279806887
Isha Dijcks <ishadijcks@gmail.com>
2019-07-05 11:30:01 +0200

    Fix docs not opening automatically

--- build_locally.sh
+++ build_locally.sh
@@ -13,4 +13,4 @@ rosdoc_lite -o build .
 htmlproofer ./build --only-4xx --check-html --file-ignore ./build/html/genindex.html,./build/html/search.html,./build/html/index-msg.html --alt-ignore '/.*/' --url-ignore '#'
 
 # Run
-#xdg-open ./build/html/index.html
+xdg-open ./build/html/index.html
