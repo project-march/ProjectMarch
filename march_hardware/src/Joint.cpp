73c3af54842ec3f89f9b8621507bdb90fcc69040
TimBuckers <TimBuckers@users.noreply.github.com>
2019-07-12 10:37:49 +0200

    Update march_hardware/src/Joint.cpp
    Co-Authored-By: Isha Dijcks <ishadijcks@gmail.com>

--- march_hardware/src/Joint.cpp
+++ march_hardware/src/Joint.cpp
@@ -55,7 +55,7 @@ void Joint::prepareActuation()
 {
   if (this->allowActuation)
   {
-    ROS_INFO("Trying to prepare joint %s for actuation", this->name.c_str());
+    ROS_INFO("Preparing joint %s for actuation", this->name.c_str());
     this->iMotionCube.goToOperationEnabled();
     ROS_INFO("Joint %s successfully prepared for actuation", this->name.c_str());
   }
