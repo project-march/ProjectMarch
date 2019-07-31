a408b889cf86a30617c2c7d4c5ed58d31139f80e
JitskedeVries <jitske.martje@quicknet.nl>
2019-07-31 16:15:33 +0200

    Add tests

--- march_hardware/test/TestIMotionCube.cpp
+++ march_hardware/test/TestIMotionCube.cpp
@@ -57,13 +57,38 @@ TEST_F(IMotionCubeTest, NoActuationMode)
   ASSERT_DEATH(imc.actuateRad(1), "trying to actuate rad, while actuationmode = unknown");
 }
 
-TEST_F(IMotionCubeTest, ChangeActuationMode)
+TEST_F(IMotionCubeTest, ActuationModeTorqueActuateRad)
+{
+    march4cpp::IMotionCube imc = march4cpp::IMotionCube(1, encoder);
+
+    imc.setActuationMode(march4cpp::ActuationMode("torque"));
+    ASSERT_DEATH(imc.actuateRad(1), "trying to actuate rad, while actuationmode = torque");
+}
+
+TEST_F(IMotionCubeTest, ActuationModePositionActuateTorque)
+{
+    march4cpp::IMotionCube imc = march4cpp::IMotionCube(1, encoder);
+
+    imc.setActuationMode(march4cpp::ActuationMode("position"));
+    ASSERT_DEATH(imc.actuateTorque(1), "trying to actuate torque, while actuationmode = position");
+}
+
+TEST_F(IMotionCubeTest, ChangeActuationModePosition)
 {
   march4cpp::IMotionCube imc = march4cpp::IMotionCube(1, encoder);
   ASSERT_EQ(march4cpp::ActuationMode::unknown, imc.getActuationMode().getValue());
 
   imc.setActuationMode(march4cpp::ActuationMode("position"));
   ASSERT_EQ(march4cpp::ActuationMode::position, imc.getActuationMode().getValue());
+}
+
+TEST_F(IMotionCubeTest, ChangeActuationModeTorque)
+{
+    march4cpp::IMotionCube imc = march4cpp::IMotionCube(1, encoder);
+    ASSERT_EQ(march4cpp::ActuationMode::unknown, imc.getActuationMode().getValue());
+
+    imc.setActuationMode(march4cpp::ActuationMode("torque"));
+    ASSERT_EQ(march4cpp::ActuationMode::torque, imc.getActuationMode().getValue());
 }
 
 TEST_F(IMotionCubeTest, ChangeActuationModeToUnknown)
@@ -75,4 +100,22 @@ TEST_F(IMotionCubeTest, ChangeActuationModeToUnknown)
   ASSERT_EQ(march4cpp::ActuationMode::position, imc.getActuationMode().getValue());
 
   ASSERT_THROW(imc.setActuationMode(march4cpp::ActuationMode("unknown")), std::runtime_error);
+}
+
+TEST_F(IMotionCubeTest, ChangeActuationModeFromPositionToTorque)
+{
+    march4cpp::IMotionCube imc = march4cpp::IMotionCube(1, encoder);
+    imc.setActuationMode(march4cpp::ActuationMode("position"));
+    ASSERT_EQ(march4cpp::ActuationMode::position, imc.getActuationMode().getValue());
+
+    ASSERT_THROW(imc.setActuationMode(march4cpp::ActuationMode("torque")), std::runtime_error);
+}
+
+TEST_F(IMotionCubeTest, ChangeActuationModeFromTorqueToPosition)
+{
+    march4cpp::IMotionCube imc = march4cpp::IMotionCube(1, encoder);
+    imc.setActuationMode(march4cpp::ActuationMode("torque"));
+    ASSERT_EQ(march4cpp::ActuationMode::torque, imc.getActuationMode().getValue());
+
+    ASSERT_THROW(imc.setActuationMode(march4cpp::ActuationMode("position")), std::runtime_error);
 }
