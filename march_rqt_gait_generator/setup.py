9b8bd8a08688e1d669d40c3f218232466276eb06
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-10-02 15:55:17 +0200

    Fix march_rqt_gait_generator catkin lint warnings

--- march_rqt_gait_generator/setup.py
+++ march_rqt_gait_generator/setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['march_rqt_gait_generator'],
+    packages=['march_rqt_gait_generator', 'march_rqt_gait_generator.model'],
     package_dir={'': 'src'},
-    scripts=['scripts/rqt_gait_generator']
+    scripts=['scripts/march_rqt_gait_generator']
 )
 
 setup(**d)
