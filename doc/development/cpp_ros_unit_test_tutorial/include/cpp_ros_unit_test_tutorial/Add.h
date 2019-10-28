e7e63dab3aa667b9dd252d986e0f88765cff5dbb
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-10-28 16:39:26 +0100

    Add ros unit test tutorial

---
+++ doc/development/cpp_ros_unit_test_tutorial/include/cpp_ros_unit_test_tutorial/Add.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Add
+{
+    public:
+        int add(int, int);
+};
---
+++ doc/development/cpp_ros_unit_test_tutorial/src/Add.cpp
@@ -0,0 +1,6 @@
+#include "cpp_ros_unit_test_tutorial/Add.h"
+
+int Add::add(int a, int b)
+{
+    return a + b;
+}
