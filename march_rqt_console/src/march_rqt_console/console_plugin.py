22d8208973403e8984c361508ae2f6b2cf8c20a6
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2019-11-04 17:12:25 +0100

    Add functionality for inserting rows

--- march_rqt_console/src/march_rqt_console/console_plugin.py
+++ march_rqt_console/src/march_rqt_console/console_plugin.py
@@ -7,19 +7,31 @@ from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 
+from march_rqt_console.message import Message
+from march_rqt_console.message_model import MessageModel
+
 
 class ConsolePlugin(Plugin):
 
     def __init__(self, context):
         super(ConsolePlugin, self).__init__(context)
-        self.setObjectName('Console')
 
         self._widget = QWidget()
         self.init_ui(context)
+
+        self._model = MessageModel()
+
+        self._widget.table_view.setModel(self._model)
+        self._widget.input_field.returnPressed.connect(self.insert_message)
 
     def init_ui(self, context):
         ui_file = os.path.join(rospkg.RosPack().get_path('march_rqt_console'), 'resource', 'console.ui')
         loadUi(ui_file, self._widget)
 
-        self._widget.setObjectName('ConsoleUi')
         context.add_widget(self._widget)
+
+    def insert_message(self):
+        msg = self._widget.input_field.text()
+        if msg:
+            self._model.insert_row(Message(msg))
+            self._widget.input_field.clear()
---
+++ march_rqt_console/src/march_rqt_console/message.py
@@ -0,0 +1,24 @@
+from python_qt_binding.QtCore import QDateTime, QObject
+
+class Message(QObject):
+
+    DEBUG = 1
+    INFO = 2
+    WARN = 3
+    ERROR = 4
+    FATAL = 5
+
+    LEVEL_LABELS = {
+        DEBUG: 'Debug',
+        INFO: 'Info',
+        WARN: 'Warn',
+        ERROR: 'Error',
+        FATAL: 'Fatal',
+    }
+
+    def __init__(self, message, level=INFO):
+        super(Message, self).__init__()
+
+        self.message = message
+        self.level = level
+
