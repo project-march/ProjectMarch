1818573dd6fdc0e8f32caa4d1c419fa13540827e
roel <roel@vossenopreis.nl>
2019-11-27 15:23:44 +0100

    Remove more.

--- march_rqt_input_device/src/march_rqt_input_device/input_device.py
+++ march_rqt_input_device/src/march_rqt_input_device/input_device.py
@@ -2,11 +2,9 @@ import os
 import rospy
 import std_msgs.msg
 import rospkg
-import threading
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtCore import QSize
 from python_qt_binding.QtWidgets import QWidget
 from march_shared_resources.msg import Error
 from march_shared_resources.msg import GaitInstruction
@@ -23,15 +21,6 @@ class InputDevicePlugin(Plugin):
         super(InputDevicePlugin, self).__init__(context)
         # Give QObjects reasonable names
         self.setObjectName('InputDevicePlugin')
-
-        # Process standalone plugin command-line arguments
-        from argparse import ArgumentParser
-        parser = ArgumentParser()
-        # Add argument(s) to the parser.
-        parser.add_argument('-q', '--quiet', action='store_true',
-                            dest='quiet',
-                            help='Put plugin in silent mode')
-        args, unknowns = parser.parse_known_args(context.argv())
 
         # Create QWidget
         self._widget = QWidget()
