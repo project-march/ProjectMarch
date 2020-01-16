3549798d36ce31f0590d8ca155bdb950c490481a
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-01-16 16:13:53 +0100

    Fix tests

--- march_hardware/test/TestRunner.cpp
+++ march_hardware/test/TestRunner.cpp
@@ -1,20 +1,6 @@
 // Copyright 2018 Project March.
 
-#include "gtest/gtest.h"
-#include <gmock/gmock.h>
-#include "mocks/MockTemperatureGES.cpp"
-
-using ::testing::AtLeast;
-using ::testing::Return;
-
-TEST(ExampleTest, MockTest)
-{
-  ASSERT_EQ(3, 3);
-  MockTemperatureGES mockTemperatureSensor;
-  EXPECT_CALL(mockTemperatureSensor, getTemperature()).Times(AtLeast(1));
-  ON_CALL(mockTemperatureSensor, getTemperature()).WillByDefault(Return(10));
-  ASSERT_EQ(10, mockTemperatureSensor.getTemperature());
-}
+#include <gtest/gtest.h>
 
 /**
  * The main method which runs all the tests
@@ -22,7 +8,6 @@ TEST(ExampleTest, MockTest)
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  testing::FLAGS_gtest_death_test_style = "threadsafe";
 
   return RUN_ALL_TESTS();
 }
