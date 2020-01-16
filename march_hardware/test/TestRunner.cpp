7c4b08f96a05de0eabd924e8551cf78f741e9506
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-01-16 15:24:08 +0100

    Remove ros dependency on unit tests

--- march_hardware/test/TestRunner.cpp
+++ march_hardware/test/TestRunner.cpp
@@ -1,25 +1,13 @@
 // Copyright 2018 Project March.
 
 #include "gtest/gtest.h"
-#include "ros/ros.h"
 #include <gmock/gmock.h>
-#include "march_hardware/TemperatureGES.h"
 #include "mocks/MockTemperatureGES.cpp"
 
 using ::testing::AtLeast;
 using ::testing::Return;
 
-class ExampleTest : public ::testing::Test
-{
-protected:
-};
-
-TEST_F(ExampleTest, AlwaysTrue)
-{
-  ASSERT_EQ(3, 3);
-}
-
-TEST_F(ExampleTest, MockTest)
+TEST(ExampleTest, MockTest)
 {
   ASSERT_EQ(3, 3);
   MockTemperatureGES mockTemperatureSensor;
@@ -33,12 +21,8 @@ TEST_F(ExampleTest, MockTest)
  */
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "march_test");
   testing::InitGoogleTest(&argc, argv);
-  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  testing::FLAGS_gtest_death_test_style = "threadsafe";
 
-  auto res = RUN_ALL_TESTS();
-
-  ros::shutdown();
-  return res;
+  return RUN_ALL_TESTS();
 }
