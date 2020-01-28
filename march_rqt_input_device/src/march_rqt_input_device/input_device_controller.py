87d7536bf5f8819025718b78a2d4fdf5e414cff4
JorisWeeda <45165354+JorisWeeda@users.noreply.github.com>
2020-01-28 11:18:20 +0100

    Feature/pm 249 mock rockerswitch (#17)
    * Created a more MVC (model, view, controller) based program. The structure is better readable and easier to change in the future. Also added two new buttons to simulate the rocker switch input and reordered the button layout to be more logical.
    
    The model of the MVC has not been separately created because this GUI is relatively easy and an extra file would only make it less readable.
    
    * Removed the deprecated files and changed the reference of the GUI in the plugin to the newly created program.
    
    * Fixed flake8 errors
    
    * changed the rocker switch publish to use the increment or decrement variable.
    
    * Added the new gait instruction msg variable to send feedback on incrementing and decrementing using the rocker switch.
    
    Co-authored-by: Olav de Haas <olavh@protonmail.com>

---
+++ march_rqt_input_device/src/march_rqt_input_device/input_device_controller.py
@@ -0,0 +1,59 @@
+import rospy
+import std_msgs.msg
+
+from march_shared_resources.msg import Error, GaitInstruction
+
+
+class InputDeviceController(object):
+    def __init__(self):
+        self.instruction_gait_pub = rospy.Publisher('/march/input_device/instruction', GaitInstruction, queue_size=10)
+
+        self.error_pub = rospy.Publisher('/march/error', Error, queue_size=10)
+        self._ping = rospy.get_param('~ping_safety_node', True)
+
+        if self._ping:
+            self._alive_pub = rospy.Publisher('/march/input_device/alive', std_msgs.msg.Time, queue_size=10)
+            self._alive_timer = rospy.Timer(rospy.Duration(0.05), self._timer_callback)
+
+    def shutdown_plugin(self):
+        if self._ping:
+            self._alive_timer.shutdown()
+            self._alive_pub.unregister()
+
+    def _timer_callback(self, event):
+        self._alive_pub.publish(event.current_real)
+
+    def publish_increment_step_size(self):
+        rospy.logdebug('Mock Input Device published step size increment')
+        self.instruction_gait_pub.publish(GaitInstruction(std_msgs.msg.Header(stamp=rospy.Time.now()),
+                                                          GaitInstruction.INCREMENT_STEP_SIZE, ''))
+
+    def publish_decrement_step_size(self):
+        rospy.logdebug('Mock Input Device published step size decrement')
+        self.instruction_gait_pub.publish(GaitInstruction(std_msgs.msg.Header(stamp=rospy.Time.now()),
+                                                          GaitInstruction.DECREMENT_STEP_SIZE, ''))
+
+    def publish_gait(self, string):
+        rospy.logdebug('Mock Input Device published gait: ' + string)
+        self.instruction_gait_pub.publish(GaitInstruction(std_msgs.msg.Header(stamp=rospy.Time.now()),
+                                                          GaitInstruction.GAIT, string))
+
+    def publish_stop(self):
+        rospy.logdebug('Mock Input Device published stop')
+        self.instruction_gait_pub.publish(GaitInstruction(std_msgs.msg.Header(stamp=rospy.Time.now()),
+                                                          GaitInstruction.STOP, ''))
+
+    def publish_continue(self):
+        rospy.logdebug('Mock Input Device published continue')
+        self.instruction_gait_pub.publish(GaitInstruction(std_msgs.msg.Header(stamp=rospy.Time.now()),
+                                                          GaitInstruction.CONTINUE, ''))
+
+    def publish_pause(self):
+        rospy.logdebug('Mock Input Device published pause')
+        self.instruction_gait_pub.publish(GaitInstruction(std_msgs.msg.Header(stamp=rospy.Time.now()),
+                                                          GaitInstruction.PAUSE, ''))
+
+    def publish_error(self):
+        rospy.logdebug('Mock Input Device published error')
+        self.error_pub.publish(Error(std_msgs.msg.Header(stamp=rospy.Time.now()),
+                                     'Fake error thrown by the develop input device.', Error.FATAL))
