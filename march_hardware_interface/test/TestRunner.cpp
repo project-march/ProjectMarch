290c06e1a369cee3f54c16567e277de33be0395e
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-01-29 16:12:51 +0100

    Add coverage report generation

--- march_hardware_interface/test/TestRunner.cpp
+++ march_hardware_interface/test/TestRunner.cpp
@@ -1,20 +1,9 @@
 // Copyright 2019 Project March.
 
-#include "gtest/gtest.h"
-#include <gmock/gmock.h>
-
-#include <march_hardware_interface/march_hardware.h>
-/**
- * Empty class to create coverage report
- */
-class TestDummy : public ::testing::Test
-{
-};
+#include <gtest/gtest.h>
 
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
-  auto res = RUN_ALL_TESTS();
-  return res;
+  return RUN_ALL_TESTS();
 }
