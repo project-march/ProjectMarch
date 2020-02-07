41c6f7ffab27f3bcec32d108ba490ea7b94961e3
Project March <tech@projectmarch.nl>
2020-02-07 15:14:37 +0100

    Add printing of hardware exception

--- march_hardware_interface/src/march_hardware_interface_node.cpp
+++ march_hardware_interface/src/march_hardware_interface_node.cpp
@@ -1,6 +1,8 @@
 // Copyright 2019 Project March.
 #include <ros/ros.h>
+#include <sstream>
 #include <march_hardware_interface/march_hardware_interface.h>
+#include <march_hardware/error/hardware_exception.h>
 
 int main(int argc, char** argv)
 {
@@ -24,6 +26,11 @@ int main(int argc, char** argv)
   {
     march.init();
   }
+  catch (const march::error::HardwareException& e)
+  {
+    ROS_FATAL_STREAM(e);
+    return 1;
+  }
   catch (const std::exception& e)
   {
     ROS_FATAL("Hardware interface caught an exception during init: %s", e.what());
@@ -39,6 +46,11 @@ int main(int argc, char** argv)
     {
       march.update(rate.expectedCycleTime());
       rate.sleep();
+    }
+    catch (const march::error::HardwareException& e)
+    {
+      ROS_FATAL_STREAM(e);
+      return 1;
     }
     catch (const std::exception& e)
     {
