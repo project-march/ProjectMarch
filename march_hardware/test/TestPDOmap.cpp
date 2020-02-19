427cc86e9f7aca8d3f8eefc9d9b90a2169851a73
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-02-19 15:36:33 +0100

    Fix some missed errors

--- march_hardware/test/TestPDOmap.cpp
+++ march_hardware/test/TestPDOmap.cpp
@@ -18,8 +18,8 @@ TEST_F(PDOTest, sortPDOmap)
   pdoMapMISO.addObject(march::IMCObjectName::ActualPosition);
   std::unordered_map<march::IMCObjectName, uint8_t> misoByteOffsets = pdoMapMISO.map(1, march::DataDirection::MISO);
 
-  ASSERT_EQ(0, misoByteOffsets[march::IMCObjectName::ActualPosition]);
-  ASSERT_EQ(4, misoByteOffsets[march::IMCObjectName::StatusWord]);
+  ASSERT_EQ(0u, misoByteOffsets[march::IMCObjectName::ActualPosition]);
+  ASSERT_EQ(4u, misoByteOffsets[march::IMCObjectName::StatusWord]);
 }
 
 TEST_F(PDOTest, multipleAddObjects)
@@ -30,7 +30,7 @@ TEST_F(PDOTest, multipleAddObjects)
   pdoMapMISO.addObject(march::IMCObjectName::StatusWord);
   pdoMapMISO.addObject(march::IMCObjectName::StatusWord);
   std::unordered_map<march::IMCObjectName, uint8_t> misoByteOffsets = pdoMapMISO.map(1, march::DataDirection::MISO);
-  ASSERT_EQ(2, misoByteOffsets.size());
+  ASSERT_EQ(2u, misoByteOffsets.size());
 }
 
 TEST_F(PDOTest, ObjectCounts)
@@ -38,6 +38,6 @@ TEST_F(PDOTest, ObjectCounts)
   march::PDOmap pdoMapMISO = march::PDOmap();
   pdoMapMISO.addObject(march::IMCObjectName::CurrentLimit);
   std::unordered_map<march::IMCObjectName, uint8_t> misoByteOffsets = pdoMapMISO.map(1, march::DataDirection::MISO);
-  ASSERT_EQ(1, misoByteOffsets.count(march::IMCObjectName::CurrentLimit));
-  ASSERT_EQ(0, misoByteOffsets.count(march::IMCObjectName::DCLinkVoltage));
+  ASSERT_EQ(1u, misoByteOffsets.count(march::IMCObjectName::CurrentLimit));
+  ASSERT_EQ(0u, misoByteOffsets.count(march::IMCObjectName::DCLinkVoltage));
 }
