ca99f40b7c3814d3466d3cab92214d75741ad8ab
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-04-03 15:38:07 +0200

    Refactor exceptions a bit

--- march_hardware_builder/include/march_hardware_builder/hardware_config_exceptions.h
+++ march_hardware_builder/include/march_hardware_builder/hardware_config_exceptions.h
@@ -2,41 +2,42 @@
 #ifndef MARCH_HARDWARE_BUILDER_HARDWARE_CONFIG_EXCEPTIONS_H
 #define MARCH_HARDWARE_BUILDER_HARDWARE_CONFIG_EXCEPTIONS_H
 #include <exception>
+#include <sstream>
 #include <string>
-
-#include <ros/ros.h>
 
 class HardwareConfigException : public std::exception
 {
 public:
-  std::string msg;
-
-  HardwareConfigException() : std::exception(), msg("Invalid hardware configuration")
+  HardwareConfigException() : msg("Invalid hardware configuration")
   {
   }
 
-  explicit HardwareConfigException(std::string msg) : std::exception(), msg(msg)
+  explicit HardwareConfigException(std::string msg) : msg(std::move(msg))
   {
   }
 
-  const char* what() const throw()
+  const char* what() const noexcept override
   {
     return msg.c_str();
   }
+
+protected:
+  std::string msg;
 };
 
 class MissingKeyException : public HardwareConfigException
 {
 public:
-  std::string key_name;
-  std::string object_name;
-
-  explicit MissingKeyException(std::string key_name, std::string object_name)
-    : HardwareConfigException(), key_name(key_name), object_name(object_name)
+  MissingKeyException(std::string key_name, std::string object_name)
+    : key_name(std::move(key_name)), object_name(std::move(object_name))
   {
     std::ostringstream ss;
-    ss << "Missing key '" << key_name << "' while creating object '" << object_name << "'";
+    ss << "Missing required key '" << key_name << "' while creating object '" << object_name << "'";
     this->msg = ss.str();
   }
+
+private:
+  std::string key_name;
+  std::string object_name;
 };
 #endif  // MARCH_HARDWARE_BUILDER_HARDWARE_CONFIG_EXCEPTIONS_H
