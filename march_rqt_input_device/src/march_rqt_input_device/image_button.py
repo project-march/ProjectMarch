00758de7091deb3cb42ec3d7b03fb7b14362d06e
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-04-20 15:21:26 +0200

    Add graying out image buttons when disabled

--- march_rqt_input_device/src/march_rqt_input_device/image_button.py
+++ march_rqt_input_device/src/march_rqt_input_device/image_button.py
@@ -1,8 +1,8 @@
-from PyQt5.QtGui import QPainter, QPixmap
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtGui import QColor, QPainter, QPixmap
+from PyQt5.QtWidgets import QAbstractButton
 
 
-class ImageButton(QPushButton):
+class ImageButton(QAbstractButton):
     def __init__(self, image_path):
         super(ImageButton, self).__init__()
         self._pixmap = QPixmap(image_path)
@@ -10,3 +10,6 @@ class ImageButton(QPushButton):
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawPixmap(0, 0, self._pixmap)
+        if not self.isEnabled():
+            painter.setOpacity(0.3)
+            painter.fillRect(event.rect(), QColor(0, 0, 0))
