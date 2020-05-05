a6121f8c7099bbc301b55a4f62d4a97c920fc7f9
RutgerVanBeek <rvbG14-22>
2020-05-05 14:33:31 +0200

    remove having a minimum elapsed time

--- march_hardware_interface/src/march_hardware_interface_node.cpp
+++ march_hardware_interface/src/march_hardware_interface_node.cpp
@@ -58,7 +58,7 @@ int main(int argc, char** argv)
       march.waitForPdo();
 
       const ros::Time now = ros::Time::now();
-      ros::Duration elapsed_time = std::min(now - last_update_time, minimal_elapsed_time);
+      ros::Duration elapsed_time = now - last_update_time;
       last_update_time = now;
 
       march.read(now, elapsed_time);
