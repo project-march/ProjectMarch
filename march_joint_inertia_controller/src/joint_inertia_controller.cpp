1cf4c3fa14417ba5fc42a79b143bba850b62bd26
cilia <ciliaclaij@gmail.com>
2020-05-12 10:28:03 +0200

    Deleted comments

--- march_joint_inertia_controller/src/joint_inertia_controller.cpp
+++ march_joint_inertia_controller/src/joint_inertia_controller.cpp
@@ -4,10 +4,10 @@
 
 namespace joint_inertia_controller_ns
 {
-// Controller initialization
+
 bool InertiaController::init(hardware_interface::PositionJointInterface* hw, ros::NodeHandle& nh)
 {
-  // Retrieve the joint object to control
+
   std::string joint_name;
   if (!nh.getParam("joint_name", joint_name))
   {
@@ -18,24 +18,19 @@ bool InertiaController::init(hardware_interface::PositionJointInterface* hw, ros
   return true;
 }
 
-// Controller startup
 void InertiaController::starting(const ros::Time& /* time */)
 {
-  // Get initial position to use in the control procedure
   init_pos_ = joint_.getPosition();
 }
 
-// Controller running
 void InertiaController::update(const ros::Time& /* time */, const ros::Duration& /* period */)
 {
-  //---Perform a sinusoidal motion for joint
   double dpos = init_pos_ + 10 * sin(ros::Time::now().toSec());
   double cpos = joint_.getPosition();
-  joint_.setCommand(-10 * (cpos - dpos));  // Apply command to the selected joint
+  joint_.setCommand(-10 * (cpos - dpos));
 }
-// Controller exiting
 void InertiaController::stopping(const ros::Time& /* time */)
 {
 }
-}  // namespace joint_inertia_controller_ns
+}
 PLUGINLIB_EXPORT_CLASS(joint_inertia_controller_ns::InertiaController, controller_interface::ControllerBase);
