9140a0129910bca1c6a196c66d5a90a4f48cb26c
jorisweeda <joris.weeda@hotmail.com>
2020-05-18 15:49:47 +0200

    Added the position, velocity and effort checks to the updater of the control category and removed the respawn-argument of the launch file.
    Deleting the respawn argument is motivated by the fact that if rqt gui fails for some reason it would be a code fault and should be fixed instead of restarting.

--- march_rqt_robot_monitor/src/march_rqt_robot_monitor/diagnostic_analyzers/control.py
+++ march_rqt_robot_monitor/src/march_rqt_robot_monitor/diagnostic_analyzers/control.py
@@ -1,5 +1,6 @@
 
 from diagnostic_msgs.msg import DiagnosticStatus
+from urdf_parser_py import urdf
 import rospy
 
 
@@ -9,11 +10,29 @@ class CheckJointValues(object):
     def __init__(self, topic, msg_type):
         rospy.Subscriber(topic, msg_type, self.cb)
 
+        # callback variables
         self._timestamp = None
         self._joint_names = None
         self._position = None
         self._velocity = None
         self._effort = None
+
+        # robot properties
+        self._lower_soft_limits = {}
+        self._upper_soft_limits = {}
+        self._velocity_limits = {}
+        self._effort_limits = {}
+
+        self._robot = urdf.Robot.from_parameter_server('/robot_description')
+        for joint in self._robot.joints:
+            try:
+                self._lower_soft_limits[joint.name] = joint.safety_controller.soft_lower_limit
+                self._upper_soft_limits[joint.name] = joint.safety_controller.soft_upper_limit
+                self._velocity_limits[joint.name] = joint.limit.velocity
+                self._effort_limits[joint.name] = joint.limit.effort
+                print(joint.name)
+            except AttributeError:
+                pass
 
     def cb(self, msg):
         """Save the latest published movement values with corresponding timestamp."""
@@ -29,11 +48,22 @@ class CheckJointValues(object):
             stat.add(' Topic error ', 'No events recorded.')
             return stat
 
-        stat.summary(DiagnosticStatus.OK, 'OK')
         stat.add('Timestamp', self._timestamp)
 
+        joint_outside_soft_limits = []
         for index in range(len(self._joint_names)):
             stat.add(self._joint_names[index], self._position[index])
+
+            if self.is_within_limit(self._position[index], self._joint_names[index], self._upper_soft_limits):
+                joint_outside_soft_limits.append(self._joint_names[index])
+
+            if self.is_within_limit(self._position[index], self._joint_names[index], self._lower_soft_limits, 'lower'):
+                joint_outside_soft_limits.append(self._joint_names[index])
+
+        if joint_outside_soft_limits:
+            stat.summary(DiagnosticStatus.ERROR, 'Outside soft limits: {ls}'.format(ls=str(joint_outside_soft_limits)))
+        else:
+            stat.summary(DiagnosticStatus.OK, 'OK')
 
     def velocity_diagnostics(self, stat):
         """The diagnostic message to display the velocities in standard format."""
@@ -44,8 +74,17 @@ class CheckJointValues(object):
         stat.summary(DiagnosticStatus.OK, 'OK')
         stat.add('Timestamp', self._timestamp)
 
+        joints_at_velocity_limit = []
         for index in range(len(self._joint_names)):
-            stat.add(self._joint_names[index], self._position[index])
+            stat.add(self._joint_names[index], self._velocity[index])
+
+            if self.is_within_limit(self._velocity[index], self._joint_names[index], self._velocity_limits):
+                joints_at_velocity_limit.append(self._joint_names[index])
+
+        if joints_at_velocity_limit:
+            stat.summary(DiagnosticStatus.ERROR, 'At velocity limit: {ls}'.format(ls=str(joints_at_velocity_limit)))
+        else:
+            stat.summary(DiagnosticStatus.OK, 'OK')
 
     def effort_diagnostics(self, stat):
         """The diagnostic message to display the efforts in standard format."""
@@ -56,5 +95,25 @@ class CheckJointValues(object):
         stat.summary(DiagnosticStatus.OK, 'OK')
         stat.add('Timestamp', self._timestamp)
 
+        joints_at_effort_limit = []
         for index in range(len(self._joint_names)):
             stat.add(self._joint_names[index], self._position[index])
+
+            if self.is_within_limit(self._effort[index], self._joint_names[index], self._effort_limits):
+                joints_at_effort_limit.append(self._joint_names[index])
+
+        if joints_at_effort_limit:
+            stat.summary(DiagnosticStatus.ERROR, 'At effort limit: {ls}'.format(ls=str(joints_at_effort_limit)))
+        else:
+            stat.summary(DiagnosticStatus.OK, 'OK')
+
+    @staticmethod
+    def is_within_limit(value, joint_name, limit_dict, operator='upper'):
+        if operator == 'upper':
+            if value >= limit_dict[joint_name]:
+                return True
+        elif operator == 'lower':
+            if value <= limit_dict[joint_name]:
+                return True
+        else:
+            raise KeyError('Non valid operator given')
