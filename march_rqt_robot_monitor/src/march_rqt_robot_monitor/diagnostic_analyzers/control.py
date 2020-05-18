180de2768360239b1eaf0b917f5f2817f4643321
jorisweeda <joris.weeda@hotmail.com>
2020-05-18 16:15:09 +0200

    Removed the check function and used the check directly inside the update function.

--- march_rqt_robot_monitor/src/march_rqt_robot_monitor/diagnostic_analyzers/control.py
+++ march_rqt_robot_monitor/src/march_rqt_robot_monitor/diagnostic_analyzers/control.py
@@ -48,15 +48,16 @@ class CheckJointValues(object):
             return stat
 
         stat.add('Timestamp', self._timestamp)
-
         joint_outside_soft_limits = []
+
         for index in range(len(self._joint_names)):
+            joint_name = self._joint_names[index]
             stat.add(self._joint_names[index], self._position[index])
 
-            if self.is_within_limit(self._position[index], self._joint_names[index], self._upper_soft_limits):
+            if self._position[index] >= self._upper_soft_limits[joint_name]:
                 joint_outside_soft_limits.append(self._joint_names[index])
 
-            if self.is_within_limit(self._position[index], self._joint_names[index], self._lower_soft_limits, 'lower'):
+            if self._position[index] <= self._lower_soft_limits[joint_name]:
                 joint_outside_soft_limits.append(self._joint_names[index])
 
         if joint_outside_soft_limits:
@@ -64,20 +65,22 @@ class CheckJointValues(object):
         else:
             stat.summary(DiagnosticStatus.OK, 'OK')
 
+        return stat
+
     def velocity_diagnostics(self, stat):
         """The diagnostic message to display the velocities in standard format."""
         if self._timestamp is None:
             stat.add(' Topic error ', 'No events recorded.')
             return stat
 
-        stat.summary(DiagnosticStatus.OK, 'OK')
         stat.add('Timestamp', self._timestamp)
-
         joints_at_velocity_limit = []
+
         for index in range(len(self._joint_names)):
+            joint_name = self._joint_names[index]
             stat.add(self._joint_names[index], self._velocity[index])
 
-            if self.is_within_limit(self._velocity[index], self._joint_names[index], self._velocity_limits):
+            if self._position[index] >= self._velocity_limits[joint_name]:
                 joints_at_velocity_limit.append(self._joint_names[index])
 
         if joints_at_velocity_limit:
@@ -85,20 +88,22 @@ class CheckJointValues(object):
         else:
             stat.summary(DiagnosticStatus.OK, 'OK')
 
+        return stat
+
     def effort_diagnostics(self, stat):
         """The diagnostic message to display the efforts in standard format."""
         if self._timestamp is None:
             stat.add(' Topic error ', 'No events recorded.')
             return stat
 
-        stat.summary(DiagnosticStatus.OK, 'OK')
         stat.add('Timestamp', self._timestamp)
-
         joints_at_effort_limit = []
+
         for index in range(len(self._joint_names)):
+            joint_name = self._joint_names[index]
             stat.add(self._joint_names[index], self._position[index])
 
-            if self.is_within_limit(self._effort[index], self._joint_names[index], self._effort_limits):
+            if self._position[index] >= self._effort_limits[joint_name]:
                 joints_at_effort_limit.append(self._joint_names[index])
 
         if joints_at_effort_limit:
@@ -106,13 +111,4 @@ class CheckJointValues(object):
         else:
             stat.summary(DiagnosticStatus.OK, 'OK')
 
-    @staticmethod
-    def is_within_limit(value, joint_name, limit_dict, operator='upper'):
-        if operator == 'upper':
-            if value >= limit_dict[joint_name]:
-                return True
-        elif operator == 'lower':
-            if value <= limit_dict[joint_name]:
-                return True
-        else:
-            raise KeyError('Non valid operator given')
+        return stat
