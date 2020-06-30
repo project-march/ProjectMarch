829cdf352b240765bdb7bef4c4aabbe36bbbb0f7
rogier <rogierkrijnen@live.nl>
2020-06-30 10:58:20 +0200

    implemented vibration calculation, mean computation, median computation, absolute computation. It is messy but works I guess.

--- march_joint_inertia_controller/include/march_joint_inertia_controller/joint_inertia_controller.h
+++ march_joint_inertia_controller/include/march_joint_inertia_controller/joint_inertia_controller.h
@@ -28,15 +28,34 @@ public:
   void stopping(const ros::Time& time);
 
   // Estimate the inertia using the acceleration and torque
-  void inertia_estimate();
+  double inertia_estimate();
   // Calculate a discrete derivative of the speed measurements
   void discrete_speed_derivative(const ros::Duration&);
+  // Calculate the alpha coefficient for the inertia estimate
+  double alpha_calculation();
+  // Calculate the inertia gain for the inertia estimate
+  double gain_calculation();
+  // Calculate the correlation coefficient of the acceleration buffer
+  double correlation_calculation();
+  // Calculate the vibration based on the acceleration
+  double vibration_calculation();
+
+  // Fill the buffers with corresponding values. Are arrays efficient enough/is this method efficient enough? There
+  // exist vector methods such as pushback() and resize as well. HALP!
+  bool fill_buffers(const ros::Duration& period);
 
 private:
   hardware_interface::JointHandle joint_;
   double init_pos_;
   std::string joint_names_;
   int num_joints_;
+
+  // Of length 12 for the acceleration buffer
+  double joint_velocity_[12];
+  // Of length 12 for the alpha calculation
+  double joint_acceleration_[12];
+  // Of length 2 for the butterworth filter
+  double joint_torque_[2];
 };
 }  // namespace joint_inertia_controller
 
--- march_joint_inertia_controller/src/joint_inertia_controller.cpp
+++ march_joint_inertia_controller/src/joint_inertia_controller.cpp
@@ -7,6 +7,66 @@ using joint_limits_interface::PositionJointSoftLimitsHandle;
 using joint_limits_interface::PositionJointSoftLimitsInterface;
 using joint_limits_interface::SoftJointLimits;
 
+// Compute the absolute value of array a and store it back in b so that a remains intact.
+void absolute(double a[], double b[], size_t sz)
+{
+  for (size_t i = 0; i < sz; ++i)
+  {
+    if (a[i] < 0)
+    {
+      b[i] = a[i] - 1;
+    }
+  }
+}
+
+// Compute the absolute value of a double and return it.
+double absolute(double a)
+{
+  if (a < 0)
+  {
+    return a * (-1);
+  }
+  else
+  {
+    return a;
+  }
+}
+
+int maximum(int a, int b, int c)
+{
+  int max = (a < b) ? b : a;
+  return ((max < c) ? c : max);
+}
+
+double median(double a[], size_t sz)
+{
+  int uneven = sz % 2;
+  // I do not want the original array to remain sorted because I may need it to compute a derivative or filter it.
+  double b[sz];
+  std::memcpy(b, a, sz);
+  std::sort(b, b + sz);
+
+  if (uneven == 1)
+  {
+    return b[sz / 2];
+  }
+  else if (uneven == 0)
+  {
+    return (b[sz / 2] + b[sz / 2 + 1]) / 2;
+  }
+  return 0.0;
+}
+
+double mean(double a[], size_t sz)
+{
+  double sum = 0.0;
+  for (size_t i = 0; i < sz; ++i)
+  {
+    sum += a[i];
+  }
+  return sum / sz;
+}
+
 namespace joint_inertia_controller
 {
 bool InertiaController::init(hardware_interface::PositionJointInterface* hw, ros::NodeHandle& nh)
@@ -19,7 +79,20 @@ bool InertiaController::init(hardware_interface::PositionJointInterface* hw, ros
   num_joints_ = joint_names_.size();
   joint_ = hw->getHandle(joint_names_);
 
-  // setup acceleration and torque arrays
+  // Fill the buffers
+  int vel_len = sizeof(joint_velocity_) / sizeof(joint_velocity_[0]);
+  int acc_len = sizeof(joint_acceleration_) / sizeof(joint_acceleration_[0]);
+  int tor_len = sizeof(joint_torque_) / sizeof(joint_torque_[0]);
+
+  int longest_array = maximum(vel_len, acc_len, tor_len);
+
+  ros::Duration first(0.004);
+  for (int i = 0; i < longest_array; i++)
+  {
+    // Bad practice (de time attribute), maar wat anders? en gaat dit wel goed met elke keer dat we data ontvangen?
+    // Krijgen we niet dubbele waarden op deze manier?
+    this->fill_buffers(first);
+  }
 
   // Setup Butterworth filter
 
@@ -29,24 +102,99 @@ bool InertiaController::init(hardware_interface::PositionJointInterface* hw, ros
 void InertiaController::starting(const ros::Time& /* time */)
 {
   init_pos_ = joint_.getPosition();
+  for (size_t i = 1; i < sizeof(joint_velocity_) / sizeof(joint_velocity_[0]); ++i)
+  {
+    joint_velocity_[i] = 0.0;
+  }
+
+  for (size_t i = 1; i < sizeof(joint_acceleration_) / sizeof(joint_acceleration_[0]); ++i)
+  {
+    joint_acceleration_[i] = 0.0;
+  }
+
+  for (size_t i = 1; i < sizeof(joint_torque_) / sizeof(joint_torque_[0]); ++i)
+  {
+    joint_torque_[i] = 0.0;
+  }
 }
 
-void InertiaController::update(const ros::Time& /* time */, const ros::Duration& /* period */)
+void InertiaController::update(const ros::Time& /* time */, const ros::Duration& period /* period */)
 {
+  if (this->fill_buffers(period))
+  {
+  }
   joint_.setCommand(0);
 }
 void InertiaController::stopping(const ros::Time& /* time */)
 {
 }
 
-// Estimate the inertia using the acceleration and torque
-void InertiaController::inertia_estimate()
+// Fills the buffers so that non-zero values may be computed by the inertia estimator
+bool InertiaController::fill_buffers(const ros::Duration& period)
 {
+  for (size_t i = 1; i < sizeof(joint_velocity_) / sizeof(joint_velocity_[0]); ++i)
+  {
+    joint_velocity_[i] = joint_velocity_[i - 1];
+  }
+  joint_velocity_[0] = 0.0;
+
+  for (size_t i = 1; i < sizeof(joint_acceleration_) / sizeof(joint_acceleration_[0]); ++i)
+  {
+    joint_acceleration_[i] = joint_acceleration_[i - 1];
+  }
+  // Automatically fills the zero'th position of the acceleration array
+  this->discrete_speed_derivative(period);
+
+  for (size_t i = 1; i < sizeof(joint_torque_) / sizeof(joint_torque_[0]); ++i)
+  {
+    joint_torque_[i] = joint_torque_[i - 1];
+  }
+  joint_torque_[0] = 0.0;
+
+  return false;
+}
+
+// Estimate the inertia using the acceleration and torque
+double InertiaController::inertia_estimate()
+{
+  return 0.0;
+}
+
+// Calculate the alpha coefficient for the inertia estimate
+double InertiaController::alpha_calculation()
+{
+  return 0.0;
+}
+
+// Calculate the inertia gain for the inertia estimate
+double InertiaController::gain_calculation()
+{
+  return 0.0;
+}
+
+// Calculate the correlation coefficient of the acceleration buffer
+double InertiaController::correlation_calculation()
+{
+  return 0.0;
+}
+
+// Calculate the vibration based on the acceleration
+double InertiaController::vibration_calculation()
+{
+  size_t len = sizeof(joint_acceleration_) / sizeof(joint_acceleration_[0]);
+  double b[len];
+  absolute(joint_acceleration_, b, len);
+  // moa = mean of the absolute
+  double moa = mean(b, len);
+  // aom = absolute of the mean
+  double aom = absolute(mean(joint_acceleration_, len));
+  return moa / aom;
 }
 
 // Calculate a discrete derivative of the speed measurements
-void InertiaController::discrete_speed_derivative(const ros::Duration&)
+void InertiaController::discrete_speed_derivative(const ros::Duration& period)
 {
+  joint_acceleration_[0] = (joint_velocity_[0] - joint_velocity_[1]) / period.toSec();
 }
 
 }  // namespace joint_inertia_controller
