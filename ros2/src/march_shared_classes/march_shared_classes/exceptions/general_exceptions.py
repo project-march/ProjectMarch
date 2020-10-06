ca4e2be57ee14c9495383151091be8e94d36b2d0
Katja Schmahl <katjaschmahl@hotmail.com>
2020-10-06 16:50:59 +0200

    Remove FileNotFoundError from general exceptions

--- ros2/src/march_shared_classes/march_shared_classes/exceptions/general_exceptions.py
+++ ros2/src/march_shared_classes/march_shared_classes/exceptions/general_exceptions.py
@@ -1,16 +1,3 @@
-class FileNotFoundError(Exception):
-    def __init__(self, file_path, msg=None):
-        """Class to raise an error when a file cannot be found.
-
-        :param file_path:
-            The file path which is not found by os.path.isfile()
-        """
-        if msg is None:
-            msg = 'File path: {fp} could not be found.'.format(fp=file_path)
-
-        super(FileNotFoundError, self).__init__(msg)
-
-
 class PackageNotFoundError(Exception):
     def __init__(self, package_name, msg=None):
         """Class to raise an error when a ros package cannot be found.
