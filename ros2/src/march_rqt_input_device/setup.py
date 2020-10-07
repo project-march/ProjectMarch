7ffeccfe71f4ba80b95910aa0fa9afc055f0e7bc
Katja Schmahl <katjaschmahl@hotmail.com>
2020-10-07 16:21:47 +0200

    Remove string concatanations

--- ros2/src/march_rqt_input_device/setup.py
+++ ros2/src/march_rqt_input_device/setup.py
@@ -10,8 +10,8 @@ setup(
     packages=['src'],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+            [os.path.join('resource', package_name)]),
+        (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name), ['plugin.xml']),
