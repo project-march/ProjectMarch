180df7a2961cb5dc5770b4031269e7dd890df916
Katja Schmahl <katjaschmahl@hotmail.com>
2020-10-12 11:47:11 +0200

    Fix test setup for the package

--- ros2/src/march_shared_classes/setup.py
+++ ros2/src/march_shared_classes/setup.py
@@ -1,23 +1,36 @@
+import os
+from glob import glob, iglob
+
 from setuptools import setup
 
 package_name = 'march_shared_classes'
+
+
+def data_files():
+    data = [
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'test', 'resources'), ['test/resources/default.yaml']),
+        (os.path.join('share', package_name, 'test', 'resources', 'walk'), ['test/resources/walk/walk.gait'])
+    ]
+    for file in iglob('test/resources/**/*.subgait', recursive=True):
+        data.append((os.path.join('share', package_name, os.path.dirname(file)), [file]))
+    return data
+
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=['src', 'src.gait', 'src.exceptions'],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files(),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='katja',
     maintainer_email='katjaschmahl@hotmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
+    tests_require=['pytest', 'urfdom_py', 'unittest'],
     entry_points={
         'console_scripts': [
         ],
