42465034b358f35a79433c5790840916223b150b
wnederpel <wolf.nederpel@gmail.com>
2020-10-15 15:46:33 +0200

    added another test and fixed certain bugs in the (inverse) calculation

--- ros1/src/march_shared_classes/src/march_shared_classes/gait/setpoint.py
+++ ros1/src/march_shared_classes/src/march_shared_classes/gait/setpoint.py
@@ -1,6 +1,8 @@
 from math import *
 from march_shared_classes.exceptions.gait_exceptions import SubgaitInterpolationError
 
+import rospkg
+from urdf_parser_py import urdf
 
 class Setpoint(object):
     """Base class to define the setpoints of a subgait."""
@@ -102,7 +104,6 @@ class Setpoint(object):
                 raise SubgaitInterpolationError("Angles do not adhere to the hard limits: l_haa = {0}, l_hfe = {1}, "
                                                 "l_kfe = {2}, r_haa = {3}, r_hfe = {4}, r_kfe = {5}".
                                                 format(l_haa, l_hfe, l_kfe, r_haa, r_hfe, r_kfe))
-        # l_haa_circles = round()
 
         bb = 1.0
         ll = 1.0
@@ -112,13 +113,14 @@ class Setpoint(object):
 
         # x is positive in the walking direction, z is in the downward direction, y is directed to the right side
         # the origin in the middle of the hip structure
-        haa_to_foot_length = ul * cos(l_hfe) + ll * cos(l_hfe - l_kfe)
-        left_y = - cos(l_haa) * ph - sin(l_haa) * haa_to_foot_length - hip / 2.0
-        left_z = - sin(l_haa) * ph + cos(l_haa) * haa_to_foot_length
+        haa_to_foot_length_left = ul * cos(l_hfe) + ll * cos(l_hfe - l_kfe)
+        left_y = - cos(l_haa) * ph - sin(l_haa) * haa_to_foot_length_left - hip / 2.0
+        left_z = - sin(l_haa) * ph + cos(l_haa) * haa_to_foot_length_left
         left_x = bb + sin(l_hfe) + sin(l_hfe - l_kfe)
 
-        right_y = cos(r_haa) * ph + sin(r_haa) * haa_to_foot_length + hip / 2.0
-        right_z = - sin(r_haa) * ph + cos(r_haa) * haa_to_foot_length
+        haa_to_foot_length_right = ul * cos(r_hfe) + ll * cos(r_hfe - r_kfe)
+        right_y = cos(r_haa) * ph + sin(r_haa) * haa_to_foot_length_right + hip / 2.0
+        right_z = - sin(r_haa) * ph + cos(r_haa) * haa_to_foot_length_right
         right_x = bb + sin(r_hfe) + sin(r_hfe - r_kfe)
 
         left_foot_pos = [left_x, left_y, left_z]
@@ -138,32 +140,38 @@ class Setpoint(object):
         hip = 1.0
 
         pos_x = position[0]
-        # so the positive direction is to the outside, easier for calculation, compensate for hip structure
+        # so the positive direction is to the outside, easier for calculation, change origin to pivot of haa joint
         if foot == 'left':
-            pos_y = - position[1] - hip / 2.0
+            pos_y = - (position[1] + hip / 2.0)
         else:
             pos_y = position[1] - hip / 2.0
         pos_z = position[2]
 
-        # first find the haa angle, this can be done by looking at the y-z plane with the knowledge that there must be
-        # a straight line from the foot position tangent to the circle with r = ph around the origin
+        # first calculate the haa angle
+        # this assume that pos_z > 0
         if pos_y != 0:
             slope_y_to_or = pos_z / pos_y
-            alpha = atan(slope_y_to_or)  # the angle with the y axis of that line
             if pos_y > 0:
+                alpha = atan(slope_y_to_or)  # the angle with the y axis of that line
                 haa = acos(ph / sqrt(pos_z * pos_z + pos_y * pos_y)) - alpha
-            else:
-                haa = - acos(ph / sqrt(pos_z * pos_z + pos_y * pos_y)) + alpha
+            elif pos_y < 0:
+                alpha = atan(abs(slope_y_to_or))
+                haa = acos(ph / sqrt(pos_z * pos_z + pos_y * pos_y)) - pi + alpha
         else:
             alpha = pi / 2
             haa = acos(ph / sqrt(pos_z * pos_z + pos_y * pos_y)) - alpha
+        if pos_z < 0:
+            raise SubgaitInterpolationError("desired z_pos is less then zero, inverse kinematic calculation is not"
+                                            " capable of this")
+
 
         # once the haa angle is known, use https://www.wolframalpha.com/input/?i=solve+%5Bsin%28x%29+%2B+sin%28x+-+y%29
         # *c%2C+cos%28x%29+%2B+cos%28x+-+y%29*c%5D+%3D+%5Ba%2C+b%5D to calculate the angles of the hfe and kfe
         # rescale for easier solving, and check if position is valid
         rescaled_x = pos_x - bb
-        rescaled_z = sqrt((pos_z + sin(haa) * ph) * (pos_z + sin(haa) * ph)
-                     + (cos(haa) * ph - pos_y) * (cos(haa) * ph - pos_y))
+        # rescaled_z = sqrt((pos_z + sin(haa) * ph) * (pos_z + sin(haa) * ph)
+        #              + (cos(haa) * ph - pos_y) * (cos(haa) * ph - pos_y))
+        rescaled_z = sqrt(- ph * ph + pos_y * pos_y + pos_z * pos_z)
         ll = ll / ul
         ul = 1.0
         rescaled_x = rescaled_x / ul
@@ -236,4 +244,10 @@ class Setpoint(object):
             kfe = kfe_two
             hfe = hfe_two
 
+        print("haa, hfe, kfe = {0}, {1}, {2}".format(haa, hfe, kfe))
+
         return [haa, hfe, kfe]
+
+if __name__ == '__main__':
+    a = Setpoint.get_angles_from_pos([1,1,1], 'left')
+    print('hello world!')
