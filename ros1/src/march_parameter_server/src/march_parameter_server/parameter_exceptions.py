01bc061e1eddbb5be6eba446b7baaf7c4b0c0a0c
BasVolkers <basvolkers99@gmail.com>
2020-10-15 16:06:50 +0200

    Removed returned value from parameter setters

--- ros1/src/march_parameter_server/src/march_parameter_server/parameter_exceptions.py
+++
@@ -1,8 +0,0 @@
-class InvalidParamName(Exception):
-    """Custom exception for when a parameter is not available in the parameter server."""
-
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return "Param with name %s doesn't exist in the parameter server" % self.name
