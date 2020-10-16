72a15c140d2e6c16a2d96703de45463eece347b0
Katja Schmahl <katjaschmahl@hotmail.com>
2020-10-16 10:34:17 +0200

    Change another string concatanation

--- ros2/src/march_rqt_input_device/setup.py
+++ ros2/src/march_rqt_input_device/setup.py
@@ -9,13 +9,13 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
+        (os.path.join('share', 'ament_index', 'resource_index', 'packages'),
             [os.path.join('resource', package_name)]),
         (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name), ['plugin.xml']),
-        (os.path.join('share', package_name), ['resource/input_device.ui']),
+        (os.path.join('share', package_name), [os.path.join('resource', 'input_device.ui')]),
         (os.path.join('share', package_name, 'resource', 'img'), glob('resource/img/*.png'))
     ],
     install_requires=['setuptools'],
