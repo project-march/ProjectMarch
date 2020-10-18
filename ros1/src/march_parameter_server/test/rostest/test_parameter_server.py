df5ae72373e2ae79c3226e4cc3bb86fb7ee09131
Bas Volkers <basvolkers99@gmail.com>
2020-10-18 18:24:00 +0200

    Add rostest for paramater server

---
+++ ros1/src/march_parameter_server/test/rostest/test_parameter_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import unittest
+
+import rospy
+
+from march_shared_resources.srv import GetParamString, GetParamStringRequest, \
+    SetParamString, SetParamStringRequest
+
+
+PKG = 'march_parameter_server'
+NAME = 'test_parameter_server'
+
+
+class TestParameterServer(unittest.TestCase):
+    def test_get_parameter(self):
+        rospy.set_param('/test', 'test_string123')
+
+        rospy.wait_for_service('/march/parameter_server/get_param_string')
+
+        service = rospy.ServiceProxy('/march/parameter_server/get_param_string', GetParamString)
+        response = service.call(GetParamStringRequest('/test'))
+
+        self.assertEqual(response.value, 'test_string123')
+        self.assertTrue(response.success)
+
+    def test_set_parameter(self):
+        rospy.wait_for_service('/march/parameter_server/set_param_string')
+
+        service = rospy.ServiceProxy('/march/parameter_server/set_param_string', SetParamString)
+        response = service.call(SetParamStringRequest('/test2', 'test_string2'))
+
+        self.assertEqual(rospy.get_param('/test2'), 'test_string2')
+        self.assertTrue(response.success)
+
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun(PKG, NAME, TestParameterServer)
