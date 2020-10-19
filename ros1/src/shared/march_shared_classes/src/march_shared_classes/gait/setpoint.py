d05cb3aee225a8177105d7fcba9fcd52f9ba15be
wnederpel <wolf.nederpel@gmail.com>
2020-10-19 12:34:10 +0200

    add lengths from urdf

--- ros1/src/shared/march_shared_classes/src/march_shared_classes/gait/setpoint.py
+++ ros1/src/shared/march_shared_classes/src/march_shared_classes/gait/setpoint.py
@@ -1,10 +1,9 @@
 from math import *
 from march_shared_classes.exceptions.gait_exceptions import SubgaitInterpolationError
-# from .joint_trajectory import JointTrajectory
-# from .limits import Limits
-#
-# import rospkg
-# from urdf_parser_py import urdf
+
+import rospkg
+from urdf_parser_py import urdf
+
 
 class Setpoint(object):
     """Base class to define the setpoints of a subgait."""
@@ -77,16 +76,6 @@ class Setpoint(object):
         """ calculated the position of the foot (ankle, ADFP is not taken into account) from joint angles. The origin of
         the local coordinate system is in the rotation point of the haa joint of the corresponding foot"""
 
-        # if velocity:
-        #     pos_or_vel = 'velocity'
-        # else:
-        #     pos_or_vel = 'position'
-        # l_haa = eval(('{0}.' + pos_or_vel).format(setpoint_dic['left_hip_aa']))
-        # l_hfe = eval(('{0}.' + pos_or_vel).format(setpoint_dic['left_hip_fe']))
-        # l_kfe = eval(('{0}.' + pos_or_vel).format(setpoint_dic['left_knee']))
-        # r_haa = eval(('{0}.' + pos_or_vel).format(setpoint_dic['right_hip_aa']))
-        # r_hfe = eval(('{0}.' + pos_or_vel).format(setpoint_dic['right_hip_fe']))
-        # r_kfe = eval(('{0}.' + pos_or_vel).format(setpoint_dic['right_knee']))
         if velocity:
             l_haa = setpoint_dic['left_hip_aa'].velocity
             l_hfe = setpoint_dic['left_hip_fe'].velocity
@@ -107,23 +96,30 @@ class Setpoint(object):
                                                 "l_kfe = {2}, r_haa = {3}, r_hfe = {4}, r_kfe = {5}".
                                                 format(l_haa, l_hfe, l_kfe, r_haa, r_hfe, r_kfe))
 
-        bb = 1.0
-        ll = 1.0
-        ul = 1.0
-        ph = 1.0
-        hip = 1.0
+        robot = urdf.Robot.from_xml_file(rospkg.RosPack().get_path('march_description') + '/urdf/march4.urdf')
+
+        ul_l = robot.link_map['upper_leg_left'].collisions[0].geometry.size[2]
+        ll_l = robot.link_map['lower_leg_left'].collisions[0].geometry.size[2]
+        bb_l = robot.link_map['hip_aa_frame_left_front'].collisions[0].geometry.size[0]
+        ph_l = robot.link_map['hip_aa_frame_left_side'].collisions[0].geometry.size[1]
+        ul_r = robot.link_map['upper_leg_right'].collisions[0].geometry.size[2]
+        ll_r = robot.link_map['lower_leg_right'].collisions[0].geometry.size[2]
+        bb_r = robot.link_map['hip_aa_frame_right_front'].collisions[0].geometry.size[0]
+        ph_r = robot.link_map['hip_aa_frame_right_side'].collisions[0].geometry.size[1]
+        base = robot.link_map['hip_base'].collisions[0].geometry.size[1]
 
         # x is positive in the walking direction, z is in the downward direction, y is directed to the right side
         # the origin in the middle of the hip structure
-        haa_to_foot_length_left = ul * cos(l_hfe) + ll * cos(l_hfe - l_kfe)
-        left_y = - cos(l_haa) * ph - sin(l_haa) * haa_to_foot_length_left - hip / 2.0
-        left_z = - sin(l_haa) * ph + cos(l_haa) * haa_to_foot_length_left
-        left_x = bb + sin(l_hfe) + sin(l_hfe - l_kfe)
+        print('haa in forward = ', l_haa)
+        haa_to_foot_length_left = ul_l * cos(l_hfe) + ll_l * cos(l_hfe - l_kfe)
+        left_y = - cos(l_haa) * ph_l - sin(l_haa) * haa_to_foot_length_left - base / 2.0
+        left_z = - sin(l_haa) * ph_l + cos(l_haa) * haa_to_foot_length_left
+        left_x = bb_l + sin(l_hfe) * ul_l + sin(l_hfe - l_kfe) * ll_l
 
-        haa_to_foot_length_right = ul * cos(r_hfe) + ll * cos(r_hfe - r_kfe)
-        right_y = cos(r_haa) * ph + sin(r_haa) * haa_to_foot_length_right + hip / 2.0
-        right_z = - sin(r_haa) * ph + cos(r_haa) * haa_to_foot_length_right
-        right_x = bb + sin(r_hfe) + sin(r_hfe - r_kfe)
+        haa_to_foot_length_right = ul_r * cos(r_hfe) + ll_r * cos(r_hfe - r_kfe)
+        right_y = cos(r_haa) * ph_r + sin(r_haa) * haa_to_foot_length_right + base / 2.0
+        right_z = - sin(r_haa) * ph_r + cos(r_haa) * haa_to_foot_length_right
+        right_x = bb_r + sin(r_hfe) * ul_r + sin(r_hfe - r_kfe) * ll_r
 
         left_foot_pos = [left_x, left_y, left_z]
         right_foot_pos = [right_x, right_y, right_z]
@@ -135,18 +131,25 @@ class Setpoint(object):
         """Calculates the angles of the joints corresponding to a certain position of the right and left foot
         w.r.t. the origin in the rotation point of the haa"""
 
-        bb = 1.0
-        ll = 1.0
-        ul = 1.0
-        ph = 1.0
-        hip = 1.0
+        robot = urdf.Robot.from_xml_file(rospkg.RosPack().get_path('march_description') + '/urdf/march4.urdf')
+        if foot == 'left':
+            ul = robot.link_map['upper_leg_left'].collisions[0].geometry.size[2]
+            ll = robot.link_map['lower_leg_left'].collisions[0].geometry.size[2]
+            bb = robot.link_map['hip_aa_frame_left_front'].collisions[0].geometry.size[0]
+            ph = robot.link_map['hip_aa_frame_left_side'].collisions[0].geometry.size[1]
+        else:
+            ul = robot.link_map['upper_leg_right'].collisions[0].geometry.size[2]
+            ll = robot.link_map['lower_leg_right'].collisions[0].geometry.size[2]
+            bb = robot.link_map['hip_aa_frame_right_front'].collisions[0].geometry.size[0]
+            ph = robot.link_map['hip_aa_frame_right_side'].collisions[0].geometry.size[1]
+        base = robot.link_map['hip_base'].collisions[0].geometry.size[1]
 
         pos_x = position[0]
         # so the positive direction is to the outside, easier for calculation, change origin to pivot of haa joint
         if foot == 'left':
-            pos_y = - (position[1] + hip / 2.0)
+            pos_y = - (position[1] + base / 2.0)
         else:
-            pos_y = position[1] - hip / 2.0
+            pos_y = position[1] - base / 2.0
         pos_z = position[2]
 
         # first calculate the haa angle
@@ -166,7 +169,6 @@ class Setpoint(object):
             raise SubgaitInterpolationError("desired z_pos is less then zero, inverse kinematic calculation is not"
                                             " capable of this")
 
-
         # once the haa angle is known, use https://www.wolframalpha.com/input/?i=solve+%5Bsin%28x%29+%2B+sin%28x+-+y%29
         # *c%2C+cos%28x%29+%2B+cos%28x+-+y%29*c%5D+%3D+%5Ba%2C+b%5D to calculate the angles of the hfe and kfe
         # rescale for easier solving, and check if position is valid
@@ -175,10 +177,13 @@ class Setpoint(object):
         #              + (cos(haa) * ph - pos_y) * (cos(haa) * ph - pos_y))
         rescaled_z = sqrt(- ph * ph + pos_y * pos_y + pos_z * pos_z)
         ll = ll / ul
-        ul = 1.0
         rescaled_x = rescaled_x / ul
         rescaled_z = rescaled_z / ul
-        if rescaled_x * rescaled_x + rescaled_z * rescaled_z > (ll + ul)*(ll + ul):
+        ul = 1.0
+        print('haa = ', haa)
+        print('rescaled_z = ', rescaled_z, 'rescaled_x = ', rescaled_x, 'll = ', ll, 'ul = ', ul)
+
+        if rescaled_x * rescaled_x + rescaled_z * rescaled_z > (ll + ul) * (ll + ul):
             raise SubgaitInterpolationError("The desired foot position, ({0}, {1}, {2}), is out of reach".
                                             format(pos_x, pos_y, pos_z))
 
@@ -186,7 +191,8 @@ class Setpoint(object):
         try:
             big_sqrt_plus = sqrt(-rescaled_x * rescaled_x - rescaled_z * rescaled_z + ll * ll + 2 * ll + 1)
             big_sqrt_min = sqrt(rescaled_x * rescaled_x + rescaled_z * rescaled_z - ll * ll + 2 * ll - 1)
-            denom_hip = (rescaled_x * rescaled_x + rescaled_z * rescaled_z + 2 * rescaled_z - ll * ll + 1) * big_sqrt_min
+            denom_hip = (rescaled_x * rescaled_x + rescaled_z * rescaled_z + 2 * rescaled_z - ll * ll + 1) \
+                        * big_sqrt_min
             numer_op_one = - rescaled_x * rescaled_x * big_sqrt_plus + 2 * rescaled_x * big_sqrt_min - rescaled_z \
                            * rescaled_z \
                            * big_sqrt_plus + ll * ll * big_sqrt_plus - 2 * ll * big_sqrt_plus + big_sqrt_plus
@@ -195,27 +201,33 @@ class Setpoint(object):
                            * big_sqrt_plus - ll * ll * big_sqrt_plus + 2 * ll * big_sqrt_plus - big_sqrt_plus
             safety_check_large_op_one = ll * (rescaled_x * rescaled_x * rescaled_z * big_sqrt_min + 2 * rescaled_x
                                               * rescaled_x
-                                       * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus + rescaled_x * ll * ll
-                                       * big_sqrt_plus - 2 * rescaled_x * ll * big_sqrt_plus + rescaled_x *
+                                              * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus + rescaled_x
+                                              * ll * ll
+                                              * big_sqrt_plus - 2 * rescaled_x * ll * big_sqrt_plus + rescaled_x *
                                               big_sqrt_plus +
-                                       2 * rescaled_z * rescaled_z * big_sqrt_min - rescaled_z * ll * ll * big_sqrt_min
-                                       + rescaled_z * big_sqrt_min + rescaled_z * rescaled_z * rescaled_z * big_sqrt_min
-                                       - rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
+                                              2 * rescaled_z * rescaled_z * big_sqrt_min - rescaled_z * ll * ll
+                                              * big_sqrt_min
+                                              + rescaled_z * big_sqrt_min + rescaled_z * rescaled_z * rescaled_z
+                                              * big_sqrt_min
+                                              - rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
             safety_check_large_op_two = - ll * (rescaled_x * rescaled_x * rescaled_z * big_sqrt_min - 2 * rescaled_x
-                                              * rescaled_x
-                                       * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus + rescaled_x * ll * ll
-                                       * big_sqrt_plus - 2 * rescaled_x * ll * big_sqrt_plus + rescaled_x *
-                                              big_sqrt_plus -
-                                       2 * rescaled_z * rescaled_z * big_sqrt_min + rescaled_z * ll * ll * big_sqrt_min
-                                       - rescaled_z * big_sqrt_min - rescaled_z * rescaled_z * rescaled_z * big_sqrt_min
-                                       - rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
+                                                * rescaled_x
+                                                * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus + rescaled_x
+                                                * ll * ll
+                                                * big_sqrt_plus - 2 * rescaled_x * ll * big_sqrt_plus + rescaled_x *
+                                                big_sqrt_plus -
+                                                2 * rescaled_z * rescaled_z * big_sqrt_min + rescaled_z * ll * ll
+                                                * big_sqrt_min
+                                                - rescaled_z * big_sqrt_min - rescaled_z * rescaled_z * rescaled_z
+                                                * big_sqrt_min
+                                                - rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
         except:
             raise SubgaitInterpolationError("The calculation method cannot find the angles corresponding to the desired"
-                                        "foot position, ({0}, {1}, {2}).".
-                                        format(pos_x, pos_y, pos_z))
+                                            "foot position, ({0}, {1}, {2}).".
+                                            format(pos_x, pos_y, pos_z))
 
         if rescaled_x * rescaled_x + rescaled_z * rescaled_z - ll * ll + 2 * rescaled_z - 1 == 0 or big_sqrt_min == 0 \
-                or safety_check_large_op_one == 0 or safety_check_large_op_two ==0:
+                or safety_check_large_op_one == 0 or safety_check_large_op_two == 0:
             raise SubgaitInterpolationError("The calculation method cannot find the angles corresponding to the desired"
                                             "foot position, ({0}, {1}, {2}).".
                                             format(pos_x, pos_y, pos_z))
@@ -257,6 +269,8 @@ class Setpoint(object):
 
         return [haa, hfe, kfe]
 
-# if __name__ == '__main__':
-#     print('testestetsdfsdfs')
-#     robot = urdf.Robot.from_xml_file(rospkg.RosPack().get_path('march_description') + '/urdf/march4.urdf')
+if __name__ == "__main__":
+    print('hallo')
+    robot = urdf.Robot.from_xml_file(rospkg.RosPack().get_path('march_description') + '/urdf/march4.urdf')
+    hip = robot.link_map['hip_base'].collisions[0].geometry.size[1]
+    print(hip)
