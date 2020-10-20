3e8b925ee919e6cffe7be8272f7c640555dc74de
wnederpel <wolf.nederpel@gmail.com>
2020-10-20 15:23:01 +0200

    change pd values of CoM controller plugin for better tuning

--- ros1/src/march_gazebo_plugins/src/walk_controller.cpp
+++ ros1/src/march_gazebo_plugins/src/walk_controller.cpp
@@ -9,12 +9,12 @@ WalkController::WalkController(physics::ModelPtr model) : ObstacleController(mod
 {
   this->swing_step_size_ = 0.7;  // This estimate will be adjusted every step
 
-  this->p_pitch_ = 120;
-  this->d_pitch_ = 40000;
-  this->p_roll_ = 150;
-  this->d_roll_ = 40000;
-  this->p_yaw_ = 1000;
-  this->d_yaw_ = 40000;
+  this->p_pitch_ = 220;
+  this->d_pitch_ = 70000;
+  this->p_roll_ = 250;
+  this->d_roll_ = 70000;
+  this->p_yaw_ = 1500;
+  this->d_yaw_ = 70000;
 }
 
 }  // namespace gazebo
