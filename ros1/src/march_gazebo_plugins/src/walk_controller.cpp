59e884251d6078ec12d83c7ed5f2fdd89eff028c
wnederpel <wolf.nederpel@gmail.com>
2020-10-22 11:26:33 +0200

    remove unnecesary this->

--- ros1/src/march_gazebo_plugins/src/walk_controller.cpp
+++ ros1/src/march_gazebo_plugins/src/walk_controller.cpp
@@ -7,14 +7,14 @@ namespace gazebo
 {
 WalkController::WalkController(physics::ModelPtr model) : ObstacleController(model)
 {
-  this->swing_step_size_ = 0.7;  // This estimate will be adjusted every step
+  swing_step_size_ = 0.7;  // This estimate will be adjusted every step
 
-  this->p_pitch_ = 220;
-  this->d_pitch_ = 70000;
-  this->p_roll_ = 250;
-  this->d_roll_ = 70000;
-  this->p_yaw_ = 1500;
-  this->d_yaw_ = 70000;
+  p_pitch_ = 220;
+  d_pitch_ = 70000;
+  p_roll_ = 250;
+  d_roll_ = 70000;
+  p_yaw_ = 1500;
+  d_yaw_ = 70000;
 }
 
 }  // namespace gazebo
