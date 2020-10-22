35d04b23b4b9ab6bfcf6e111520cb5162a063ae6
wnederpel <wolf.nederpel@gmail.com>
2020-10-22 10:10:02 +0200

    remove unnecessaru instances of this->

--- ros1/src/march_gazebo_plugins/src/walk_controller.cpp
+++ ros1/src/march_gazebo_plugins/src/walk_controller.cpp
@@ -7,24 +7,24 @@ namespace gazebo
 {
 WalkController::WalkController(physics::ModelPtr model) : ObstacleController(model)
 {
-  this->swing_step_size_ = 0.7;  // This estimate will be adjusted every step
+  swing_step_size_ = 0.7;  // This estimate will be adjusted every step
 
   // The non-off_ values are tune so that the exo stays upright, the _off_ are set so the exo falls over. These _off_
-  // values can be changed if one whishes the plugin to deliver some torque even when turned off.
-  this->p_pitch_ = 120;
-  this->p_pitch_off_ = 0;
-  this->d_pitch_ = 40000;
-  this->d_pitch_off_ = 0;
+  // values can be changed if one wishes the plugin to deliver some torque even when turned off.
+  p_pitch_ = 120;
+  p_pitch_off_ = 0;
+  d_pitch_ = 40000;
+  d_pitch_off_ = 0;
 
-  this->p_roll_ = 150;
-  this->p_roll_off_ = 0;
-  this->d_roll_ = 40000;
-  this->d_roll_off_ = 0;
+  p_roll_ = 150;
+  p_roll_off_ = 0;
+  d_roll_ = 40000;
+  d_roll_off_ = 0;
 
-  this->p_yaw_ = 1000;
-  this->p_yaw_off_ = 0;
-  this->d_yaw_ = 40000;
-  this->d_yaw_off_ = 0;
+  p_yaw_ = 1000;
+  p_yaw_off_ = 0;
+  d_yaw_ = 40000;
+  d_yaw_off_ = 0;
 }
 
 }  // namespace gazebo
