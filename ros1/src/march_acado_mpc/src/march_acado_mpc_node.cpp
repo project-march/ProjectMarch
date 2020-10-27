2b8a35536f4dce99a3570d09b99b284e6f051212
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2020-10-27 09:27:44 +0100

    adding ACADO files

--- ros1/src/march_acado_mpc/src/march_acado_mpc_node.cpp
+++ ros1/src/march_acado_mpc/src/march_acado_mpc_node.cpp
@@ -1,11 +1,33 @@
-#include "march_acado_mpc/mpc.h"
-#include <iostream>
+#include "ros/ros.h"
 
-using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
 
-  cout << "Hello World!" << endl;
+  ros::init(argc, argv, "march_acado_mpc_node")
+  ros::NodeHandle n;
+  ros::Rate rate(10);
+
+  int count = 0;
+  while (!n.hasParam("/march/joint_names"))
+  {
+    ros::Duration(0.5).sleep();
+    count++;
+    if (count > 10)
+    {
+      ROS_ERROR("Failed to read the joint_names from the parameter server.");
+      throw std::runtime_error("Failed to read the joint_names from the parameter server.");
+    }
+  }
+
+  n.getParam("/march/joint_names", sensor_names);
+
+  while (ros::ok())
+  {
+  	// ...
+    
+    ros::spinOnce();
+    rate.sleep();
+  }
 
   return 0;
 }
