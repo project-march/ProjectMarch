b7b233a18c561f50f900c0b7e56f4512fe28faa5
wnederpel <wolf.nederpel@gmail.com>
2020-10-28 17:08:41 +0100

    refactor to pass pep8

--- ros1/src/shared/march_shared_classes/src/march_shared_classes/gait/setpoint.py
+++ ros1/src/shared/march_shared_classes/src/march_shared_classes/gait/setpoint.py
@@ -7,6 +7,7 @@ from urdf_parser_py import urdf
 
 VELOCITY_SCALE = 250
 
+
 class Setpoint(object):
     """Base class to define the setpoints of a subgait."""
 
@@ -63,11 +64,13 @@ class Setpoint(object):
         new_foot_vel = (base_foot_vel * (1 - parameter) + other_foot_vel * parameter)
 
         new_angles_pos = [Setpoint.get_angles_from_pos(new_foot_pos[0], 'left'),
-                      Setpoint.get_angles_from_pos(new_foot_pos[1], 'right')]
+                          Setpoint.get_angles_from_pos(new_foot_pos[1], 'right')]
         # Calculate new velocity by finding the foot position one 250th (one ehtercat cycle) of a second later.
         new_angles_vel = (- np.array(new_angles_pos) +
-                    np.array([Setpoint.get_angles_from_pos(new_foot_pos[0] + new_foot_vel[0] / VELOCITY_SCALE, 'left'),
-                        Setpoint.get_angles_from_pos(new_foot_pos[1] + new_foot_vel[1] / VELOCITY_SCALE, 'right')]))\
+                            np.array([Setpoint.get_angles_from_pos(new_foot_pos[0] + new_foot_vel[0] /
+                                                                   VELOCITY_SCALE, 'left'),
+                                     Setpoint.get_angles_from_pos(new_foot_pos[1] + new_foot_vel[1] /
+                                                                  VELOCITY_SCALE, 'right')]))\
                   * VELOCITY_SCALE
 
         # linearly interpolate the ankle angle, as it cannot be calculated from the inverse kinematics
@@ -99,7 +102,6 @@ class Setpoint(object):
                                'right_ankle': cls(time, new_ankle_pos[1], new_ankle_vel[1])}
 
         return resulting_setpoints
-
 
     def __repr__(self):
         return 'Time: %s, Position: %s, Velocity: %s' % (self.time, self.position, self.velocity)
@@ -151,15 +153,6 @@ class Setpoint(object):
         r_haa = setpoint_dic['right_hip_aa'].position
         r_hfe = setpoint_dic['right_hip_fe'].position
         r_kfe = setpoint_dic['right_knee'].position
-        if velocity:
-            # To calculate the velocity of the foot, find the foot location as it would be one ethercat cycle later.
-            # The velocities need to be scaled as the position one second later could be invalid.
-            l_haa_next = l_haa + setpoint_dic['left_hip_aa'].velocity / VELOCITY_SCALE
-            l_hfe_next = l_hfe + setpoint_dic['left_hip_fe'].velocity / VELOCITY_SCALE
-            l_kfe_next = l_kfe + setpoint_dic['left_knee'].velocity / VELOCITY_SCALE
-            r_haa_next = r_haa + setpoint_dic['right_hip_aa'].velocity / VELOCITY_SCALE
-            r_hfe_next = r_hfe + setpoint_dic['right_hip_fe'].velocity / VELOCITY_SCALE
-            r_kfe_next = r_kfe + setpoint_dic['right_knee'].velocity / VELOCITY_SCALE
 
         robot = urdf.Robot.from_xml_file(rospkg.RosPack().get_path('march_description') + '/urdf/march4.urdf')
         ul_l = robot.link_map['upper_leg_left'].collisions[0].geometry.size[2]
@@ -173,7 +166,8 @@ class Setpoint(object):
         base = robot.link_map['hip_base'].collisions[0].geometry.size[1]
 
         # x is positive in the walking direction, z is in the downward direction, y is directed to the right side
-        # the origin in the middle of the hip structure
+        # the origin in the middle of the hip structure. The calculations are supported by
+        # https://confluence.projectmarch.nl:8443/display/62tech/%28Inverse%29+kinematics
         haa_to_foot_length_left = ul_l * cos(l_hfe) + ll_l * cos(l_hfe - l_kfe)
         left_y = - cos(l_haa) * ph_l - sin(l_haa) * haa_to_foot_length_left - base / 2.0
         left_z = - sin(l_haa) * ph_l + cos(l_haa) * haa_to_foot_length_left
@@ -186,6 +180,14 @@ class Setpoint(object):
 
         if velocity:
             # To calculate the velocity of the foot, find the foot location as it would be one ethercat cycle later.
+            # The velocities need to be scaled as the position one second later could be invalid.
+            l_haa_next = l_haa + setpoint_dic['left_hip_aa'].velocity / VELOCITY_SCALE
+            l_hfe_next = l_hfe + setpoint_dic['left_hip_fe'].velocity / VELOCITY_SCALE
+            l_kfe_next = l_kfe + setpoint_dic['left_knee'].velocity / VELOCITY_SCALE
+            r_haa_next = r_haa + setpoint_dic['right_hip_aa'].velocity / VELOCITY_SCALE
+            r_hfe_next = r_hfe + setpoint_dic['right_hip_fe'].velocity / VELOCITY_SCALE
+            r_kfe_next = r_kfe + setpoint_dic['right_knee'].velocity / VELOCITY_SCALE
+
             haa_to_foot_length_left_next = ul_l * cos(l_hfe_next) + ll_l * cos(l_hfe_next - l_kfe_next)
             left_y_next = - cos(l_haa_next) * ph_l - sin(l_haa_next) * haa_to_foot_length_left_next - base / 2.0
             left_z_next = - sin(l_haa_next) * ph_l + cos(l_haa_next) * haa_to_foot_length_left_next
@@ -265,7 +267,6 @@ class Setpoint(object):
             alpha = pi / 2
             haa = acos(ph / sqrt(pos_z * pos_z + pos_y * pos_y)) - alpha
 
-
         # once the haa angle is known, use https://www.wolframalpha.com/input/?i=solve+%5Bsin%28x%29+%2B+sin%28x+-+y%29
         # *c%2C+cos%28x%29+%2B+cos%28x+-+y%29*c%5D+%3D+%5Ba%2C+b%5D to calculate the angles of the hfe and kfe
 
@@ -285,36 +286,26 @@ class Setpoint(object):
         try:
             big_sqrt_plus = sqrt(-rescaled_x * rescaled_x - rescaled_z * rescaled_z + ll * ll + 2 * ll + 1)
             big_sqrt_min = sqrt(rescaled_x * rescaled_x + rescaled_z * rescaled_z - ll * ll + 2 * ll - 1)
-            denom_hip = (rescaled_x * rescaled_x + rescaled_z * rescaled_z + 2 * rescaled_z - ll * ll + 1) \
-                        * big_sqrt_min
-            numer_op_one = - rescaled_x * rescaled_x * big_sqrt_plus + 2 * rescaled_x * big_sqrt_min - rescaled_z \
-                           * rescaled_z \
-                           * big_sqrt_plus + ll * ll * big_sqrt_plus - 2 * ll * big_sqrt_plus + big_sqrt_plus
-            numer_op_two = rescaled_x * rescaled_x * big_sqrt_plus + 2 * rescaled_x * big_sqrt_min + rescaled_z \
-                           * rescaled_z \
-                           * big_sqrt_plus - ll * ll * big_sqrt_plus + 2 * ll * big_sqrt_plus - big_sqrt_plus
-            safety_check_large_op_one = ll * (rescaled_x * rescaled_x * rescaled_z * big_sqrt_min + 2 * rescaled_x
-                                              * rescaled_x
-                                              * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus + rescaled_x
-                                              * ll * ll
-                                              * big_sqrt_plus - 2 * rescaled_x * ll * big_sqrt_plus + rescaled_x *
-                                              big_sqrt_plus +
-                                              2 * rescaled_z * rescaled_z * big_sqrt_min - rescaled_z * ll * ll
-                                              * big_sqrt_min
-                                              + rescaled_z * big_sqrt_min + rescaled_z * rescaled_z * rescaled_z
-                                              * big_sqrt_min
-                                              - rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
-            safety_check_large_op_two = - ll * (rescaled_x * rescaled_x * rescaled_z * big_sqrt_min - 2 * rescaled_x
-                                                * rescaled_x
-                                                * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus + rescaled_x
-                                                * ll * ll
-                                                * big_sqrt_plus - 2 * rescaled_x * ll * big_sqrt_plus + rescaled_x *
-                                                big_sqrt_plus -
-                                                2 * rescaled_z * rescaled_z * big_sqrt_min + rescaled_z * ll * ll
-                                                * big_sqrt_min
-                                                - rescaled_z * big_sqrt_min - rescaled_z * rescaled_z * rescaled_z
-                                                * big_sqrt_min
-                                                - rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
+            denom_hip = (rescaled_x * rescaled_x + rescaled_z * rescaled_z + 2 * rescaled_z - ll * ll + 1) * \
+                        big_sqrt_min
+            numer_op_one = - rescaled_x * rescaled_x * big_sqrt_plus + 2 * rescaled_x * big_sqrt_min - rescaled_z * \
+                           rescaled_z * big_sqrt_plus + ll * ll * big_sqrt_plus - 2 * ll * big_sqrt_plus + big_sqrt_plus
+            numer_op_two = rescaled_x * rescaled_x * big_sqrt_plus + 2 * rescaled_x * big_sqrt_min + rescaled_z * \
+                           rescaled_z * big_sqrt_plus - ll * ll * big_sqrt_plus + 2 * ll * big_sqrt_plus - big_sqrt_plus
+            safety_check_large_op_one = ll * (rescaled_x * rescaled_x * rescaled_z * big_sqrt_min + 2 * rescaled_x *
+                                              rescaled_x * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus +
+                                              rescaled_x * ll * ll * big_sqrt_plus - 2 * rescaled_x * ll *
+                                              big_sqrt_plus + rescaled_x * big_sqrt_plus + 2 * rescaled_z * rescaled_z *
+                                              big_sqrt_min - rescaled_z * ll * ll * big_sqrt_min + rescaled_z *
+                                              big_sqrt_min + rescaled_z * rescaled_z * rescaled_z * big_sqrt_min -
+                                              rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
+            safety_check_large_op_two = - ll * (rescaled_x * rescaled_x * rescaled_z * big_sqrt_min - 2 * rescaled_x *
+                                                rescaled_x * big_sqrt_min - rescaled_x * rescaled_z * big_sqrt_plus +
+                                                rescaled_x * ll * ll * big_sqrt_plus - 2 * rescaled_x * ll *
+                                                big_sqrt_plus + rescaled_x * big_sqrt_plus - 2 * rescaled_z *
+                                                rescaled_z * big_sqrt_min + rescaled_z * ll * ll * big_sqrt_min -
+                                                rescaled_z * big_sqrt_min - rescaled_z * rescaled_z * rescaled_z *
+                                                big_sqrt_min - rescaled_x * rescaled_x * rescaled_x * big_sqrt_plus)
         except ValueError:
             raise SubgaitInterpolationError("The calculation method cannot find the angles corresponding to the desired"
                                             " foot position, ({0}, {1}, {2}).".
@@ -364,4 +355,3 @@ class Setpoint(object):
             hfe = hfe_two
 
         return [haa, hfe, kfe]
-
