cc44b4e02fe6da1b3420b2bb7d0870dcb269698c
Katja Schmahl <katjaschmahl@hotmail.com>
2020-11-23 11:18:03 +0100

    Update launch files for ros2

---
+++ ros2/src/visualization/march_rqt_gait_selection/launch/gait_selection.launch.py
@@ -0,0 +1,24 @@
+import launch
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description() -> launch.LaunchDescription:
+    """
+    Launch file to launch rqt note taker.
+    """
+
+    return launch.LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='True',
+            description='Whether to use simulation time'),
+        Node(
+            package='march_rqt_gait_selection',
+            executable='rqt_gait_selection',
+            output='screen',
+            name='rqt_gait_selection',
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        )
+    ])
