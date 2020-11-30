c6736e11be95abe1aa35f1a60f5722a7b81ce128
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2020-11-30 16:24:05 +0100

    renaming source files

---
+++ ros1/src/control/march_acado_mpc/src/joint_trajectory_mpc.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I'm alive!" << std::endl;
+    return 0;
+}
--- ros1/src/control/march_acado_mpc/src/march_acado_mpc_node.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "march_acado_mpc_node has been launched!" << std::endl;
-    return 0;
-}
