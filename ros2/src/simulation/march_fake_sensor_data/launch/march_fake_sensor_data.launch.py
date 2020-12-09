9d9649d5073fa3ea3fcd2351580408b7cea0f0ad
Thijs Raymakers <thijs@raymakers.nl>
2020-12-09 10:20:30 +0100

    Add launch file for march_fake_sensor_data

---
+++ ros2/src/simulation/march_fake_sensor_data/launch/march_fake_sensor_data.launch.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    ld = launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='march_fake_sensor_data',
+            executable='march_fake_temperature_data_node',
+            name='fake_sensor_data',
+            output='screen'
+        )
+    ])
+    return ld
+
+
+if __name__ == '__main__':
+    generate_launch_description()
