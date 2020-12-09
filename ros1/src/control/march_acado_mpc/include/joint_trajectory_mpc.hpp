b1f395fb90a3719df6dea0f43161b22ff084017f
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2020-12-09 17:26:31 +0100

    Moving functional joint_trajectory_mpc code from header to source

--- ros1/src/control/march_acado_mpc/include/joint_trajectory_mpc.hpp
+++ ros1/src/control/march_acado_mpc/include/joint_trajectory_mpc.hpp
@@ -31,28 +31,13 @@ public:
   /**
    * \brief Initialize the controller by establishing the pointer to the joints
    */
-  bool init(std::vector<hardware_interface::JointHandle>& joint_handles, ros::NodeHandle& nh)
-  {
-    joint_handles_ptr_ = &joint_handles;
-    num_joints_ = joint_handles_ptr_->size();
-
-    return true;
-  }
+  bool init(std::vector<hardware_interface::JointHandle>& joint_handles, ros::NodeHandle& nh);
 
   /**
   * \brief Starts the controller by checking if the joint handle pointer is filled and sets
   * the initial command to zero so that the joint doesn't start moving without a desired trajectory
   */
-  void starting(const ros::Time& /*time*/)
-  {
-    if (!joint_handles_ptr_) {return;}
-
-    // zero commands
-    for (unsigned int i = 0; i < num_joints_; ++i)
-    {
-      (*joint_handles_ptr_)[i].setCommand(0.0);
-    }
-  }
+  void starting(const ros::Time& /*time*/);
 
   /**
    * \brief Updates the commanded effort for each individual joint and estimates the inertia on each joint and publishes
@@ -60,32 +45,12 @@ public:
    */
   void updateCommand(const ros::Time& /*time*/, const ros::Duration& period,
                      const joint_trajectory_controller::State& /*desired state*/,
-                     const joint_trajectory_controller::State& state_error)
-  {
-    num_joints_ = joint_handles_ptr_->size();
-
-    // Preconditions
-    if (!joint_handles_ptr_)
-    {
-      return;
-    }
-    assert(num_joints_ == state_error.position.size());
-    assert(num_joints_ == state_error.velocity.size());
-
-    // Update effort command
-    for (unsigned int i = 0; i < num_joints_; ++i)
-    {
-      // simple P controller to test setCommand() and the trajectory controller
-      const double command = state_error.position[i]*1000;
-      (*joint_handles_ptr_)[i].setCommand(command);
-    }
-
-  }
+                     const joint_trajectory_controller::State& state_error);
 
   /**
    * \brief Procedure, if required, for stopping the controller
    */
-  void stopping(const ros::Time& /*time*/) { }
+  void stopping(const ros::Time& /*time*/);
 
 private:
   std::vector<hardware_interface::JointHandle>* joint_handles_ptr_;
--- ros1/src/control/march_acado_mpc/src/joint_trajectory_mpc.cpp
+++ ros1/src/control/march_acado_mpc/src/joint_trajectory_mpc.cpp
@@ -1,6 +1,58 @@
 #include "joint_trajectory_mpc.hpp"
 #include <pluginlib/class_list_macros.hpp>
 
+typedef HardwareInterfaceAdapter<hardware_interface::EffortJointInterface, joint_trajectory_controller::State> HWIA;
+
+bool HWIA::init(std::vector<hardware_interface::JointHandle>& joint_handles, ros::NodeHandle& nh)
+{
+  joint_handles_ptr_ = &joint_handles;
+  num_joints_ = joint_handles_ptr_->size();
+
+  return true;
+}
+
+void HWIA::starting(const ros::Time& /*time*/)
+{
+  if (!joint_handles_ptr_) {return;}
+
+  // zero commands
+  for (unsigned int i = 0; i < num_joints_; ++i)
+  {
+    (*joint_handles_ptr_)[i].setCommand(0.0);
+  }
+}
+
+void HWIA::updateCommand(const ros::Time& /*time*/, const ros::Duration& period,
+                   const joint_trajectory_controller::State& /*desired state*/,
+                   const joint_trajectory_controller::State& state_error)
+{
+  num_joints_ = joint_handles_ptr_->size();
+
+  // Preconditions
+  if (!joint_handles_ptr_)
+  {
+    return;
+  }
+  assert(num_joints_ == state_error.position.size());
+  assert(num_joints_ == state_error.velocity.size());
+
+  // Update effort command
+  for (unsigned int i = 0; i < num_joints_; ++i)
+  {
+    // simple P controller to test setCommand() and the trajectory controller
+    const double command = state_error.position[i]*1000;
+    (*joint_handles_ptr_)[i].setCommand(command);
+  }
+
+}
+
+void HWIA::stopping(const ros::Time& /*time*/)
+{
+
+}
+
+
+// Exporting the controller plugin
 namespace model_predictive_trajectory_controller
 {
 typedef joint_trajectory_controller::JointTrajectoryController<trajectory_interface::QuinticSplineSegment<double>,
