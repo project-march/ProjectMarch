1d1413cd74abb02e92f73ff617d0253192bc8110
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2020-12-11 17:13:21 +0100

    Adding initSolver() code and skeleton controller() function

--- ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
+++ ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
@@ -5,7 +5,29 @@
 using namespace std;
 
 void ModelPredictiveController::initSolver() {
-  cout << "ModelPredictiveController" << endl;
+  // Reset all solver memory
+  memset(&acadoWorkspace, 0, sizeof(acadoWorkspace));
+  memset(&acadoVariables, 0, sizeof(acadoVariables));
+
+  // Initialize the solver
+  acado_initializeSolver();
+
+  // Prepare a consistent initial guess
+  for (int i = 0; i < N + 1; ++i) {
+    acadoVariables.x[i * NX + 0] = 0;
+    acadoVariables.x[i * NX + 1] = 0;
+  }
+
+  // Prepare references
+  for (int i = 0; i < N; ++i) {
+    acadoVariables.y[i * NY + 0] = 1; // theta
+    acadoVariables.y[i * NY + 1] = 0; // dtheta
+    acadoVariables.y[i * NY + 2] = 0; // T
+  }
+
+  acadoVariables.yN[0] = 1; // theta
+  acadoVariables.yN[1] = 0; // dtheta
+  acadoVariables.yN[2] = 0; // T
 }
 
 void ModelPredictiveController::setInitialState(vector<double> x0) {
@@ -13,3 +35,8 @@ void ModelPredictiveController::setInitialState(vector<double> x0) {
     acadoVariables.x0[i] = x0[i];
   }
 }
+
+void ModelPredictiveController::controller(std::vector<double> x0) {
+
+}
+
