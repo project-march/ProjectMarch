ebcf27fb937e0436ce2042b35a27bb7dd09b899e
Thijs Raymakers <thijs@raymakers.nl>
2021-01-11 13:55:55 +0100

    Fix flake8 warning about unused variable and imports

--- ros2/src/march_launch/launch/march_ros2_simulation.launch.py
+++ ros2/src/march_launch/launch/march_ros2_simulation.launch.py
@@ -4,17 +4,11 @@ from ament_index_python import get_package_share_directory
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import (
-    EnvironmentVariable,
-    LaunchConfiguration,
-    PathJoinSubstitution,
-)
 from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     # General arguments
-    node_prefix = LaunchConfiguration("node_prefix")
     use_sim_time = LaunchConfiguration("use_sim_time")
     robot = LaunchConfiguration("robot")
     # Input device arguments
