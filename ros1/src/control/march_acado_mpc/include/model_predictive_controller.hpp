a79e1b6976f672ba065972bff3864929e8846044
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-01-11 10:31:49 +0100

    Adding controller function to the mpc class

--- ros1/src/control/march_acado_mpc/include/model_predictive_controller.hpp
+++ ros1/src/control/march_acado_mpc/include/model_predictive_controller.hpp
@@ -1,48 +1,38 @@
-#ifndef MARCH_MODELPREDICTIVECONTROLLER_H
-#define MARCH_MODELPREDICTIVECONTROLLER_H
+#ifndef MARCH_MODEL_PREDICTIVE_CONTROLLER_H
+#define MARCH_MODEL_PREDICTIVE_CONTROLLER_H
 
 #include <vector>
-#include "acado_common.h"
+
+using namespace std;
 
 class ModelPredictiveController {
-
 public:
+    // Public variables
+    vector<double> x0;  // Current state
+    double u;           // Calculated control input
 
-/**
- * \brief Initalise the solver
- */
-  void initSolver();
+    /**
+     * \brief Initialise the model predictive controller
+     */
+    void init();
 
-  /**
-   * \brief Set the initial state
-   * @param x0 - initial state
-   */
-  void setInitialState(std::vector<double> x0);
+    /**
+    * \brief Set the initial state
+    * @param x0 - initial state
+    */
+    void setInitialState(vector<double> x0);
 
-  /**
-   * \brief Run a single feedback iteration given the initial state
-   * @param x0 - initial state
-   */
-  void controller(std::vector<double> x0);
-
+    /**
+     * \brief Calculate the control input
+     * @param x0 - initial state
+     * @return u - control input
+     */
+    void controller();
 
 private:
 
-  // Some convenient definitions
-  #define NX          ACADO_NX  // Number of differential state variables
-  #define NXA         ACADO_NXA // Number of algebraic variables
-  #define NU          ACADO_NU  // Number of control inputs
-  #define NOD         ACADO_NOD // Number of online data values
 
-  #define NY          ACADO_NY  // Number of measurements/references on nodes 0..N - 1
-  #define NYN         ACADO_NYN // Number of measurements/references on node N
-
-  #define N           ACADO_N   // Number of intervals in the horizon
-
-  // global variables used by the solver
-  ACADOvariables acadoVariables;
-  ACADOworkspace acadoWorkspace;
 
 };
 
-#endif  // MARCH_MODELPREDICTIVECONTROLLER_H
+#endif
