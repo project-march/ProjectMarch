f45390f8945aa3d4889782f9c378e6d347b7eaba
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-01-14 15:36:05 +0100

    Adding functions to read reference files

--- ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
+++ ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -13,7 +16,50 @@ using namespace std;
 ACADOvariables acadoVariables = {};
 ACADOworkspace acadoWorkspace = {};
 
+bool ModelPredictiveController::readReferenceFromFile(const char* filename, vector<vector<double>>& data) {
+    ifstream file(filename);
+    string line;
+
+    if (file.is_open()) {
+        while (getline(file,line))
+        {
+            stringstream linestream(line);
+            vector<double> linedata;
+            double number;
+
+            while (linestream >> number)
+            {
+                linedata.push_back(number);
+            }
+            data.push_back(linedata);
+        }
+        file.close();
+    }
+    else
+        return false;
+
+    return true;
+}
+
+void ModelPredictiveController::setReference(vector<vector<double>>& reference, int iter) {
+    for (int i = 0; i < ACADO_N; ++i) {
+        acadoVariables.y[i * ACADO_NY + 0] = reference[iter + i][0]; // theta
+        acadoVariables.y[i * ACADO_NY + 1] = reference[iter + i][1]; // dtheta
+        acadoVariables.y[i * ACADO_NY + 2] = reference[iter + i][2]; // T
+    }
+
+    acadoVariables.yN[0] = reference[iter + ACADO_N][0]; // theta
+    acadoVariables.yN[1] = reference[iter + ACADO_N][1]; // dtheta
+    acadoVariables.yN[2] = reference[iter + ACADO_N][2]; // T
+}
+
+
 void ModelPredictiveController::init() {
+
+  // Read reference data
+  if (ModelPredictiveController::readReferenceFromFile("../references/sin.txt", reference) == false) {
+      cout << "Cannot read reference" << endl;
+  }
 
   // Initialize the solver
   acado_initializeSolver();
@@ -33,15 +79,17 @@ void ModelPredictiveController::init() {
   double theta_ref = 90*(M_PI/180);
 
   // Prepare references (step reference)
-  for (int i = 0; i < ACADO_N; ++i) {
-    acadoVariables.y[i * ACADO_NY] = theta_ref; // theta
-    acadoVariables.y[i * ACADO_NY + 1] = 0;         // dtheta
-    acadoVariables.y[i * ACADO_NY + 2] = 0;         // T
-  }
+//  ModelPredictiveController::setReference(reference, iter);
 
-  acadoVariables.yN[0] = theta_ref; // theta
-  acadoVariables.yN[1] = 0;         // dtheta
-  acadoVariables.yN[2] = 0;         // T
+//  for (int i = 0; i < ACADO_N; ++i) {
+//    acadoVariables.y[i * ACADO_NY + 0] = theta_ref; // theta
+//    acadoVariables.y[i * ACADO_NY + 1] = 0;         // dtheta
+//    acadoVariables.y[i * ACADO_NY + 2] = 0;         // T
+//  }
+//
+//  acadoVariables.yN[0] = theta_ref; // theta
+//  acadoVariables.yN[1] = 0;         // dtheta
+//  acadoVariables.yN[2] = 0;         // T
 
   // Current state feedback
   setInitialState(x0);
@@ -59,8 +107,11 @@ void ModelPredictiveController::setInitialState(vector<double> x0) {
 
 void ModelPredictiveController::calculateControlInput() {
 
-  // Set initial speed
+  // Set initial state
   setInitialState(x0);
+
+  // Set reference
+  ModelPredictiveController::setReference(reference, iter);
 
   // preparation step
   acado_preparationStep();
@@ -72,6 +123,8 @@ void ModelPredictiveController::calculateControlInput() {
   // Shift states and control and prepare for the next iteration
   acado_shiftStates(2, 0, 0);
   acado_shiftControls(0);
+
+  iter++;
 
 }
 
