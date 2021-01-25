7d5455c3a254a46239bd28b13fb1c017ca41c867
Katja Schmahl <katjaschmahl@hotmail.com>
2021-01-25 14:25:30 +0100

    First version of moveit interface, trajectory is still failing

--- ros2/src/shared/march_description/launch/march_description.launch.py
+++ ros2/src/shared/march_description/launch/march_description.launch.py
@@ -2,6 +2,7 @@
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch_ros.actions import Node
 
@@ -12,7 +13,8 @@ def generate_launch_description():
     xacro_path = PathJoinSubstitution(
         [get_package_share_directory("march_description"), "urdf", robot_description]
     )
-
+    balance = LaunchConfiguration("balance")
+    
     return LaunchDescription(
         [
             DeclareLaunchArgument(
@@ -25,6 +27,11 @@ def generate_launch_description():
                 default_value="march4",
                 description="Which <robot_description>.xacro file to use. "
                 "This file must be available in the march_desrciption/urdf/ folder",
+            ),
+            DeclareLaunchArgument(
+                "balance",
+                default_value="False",
+                description="Whether balance is being used.",
             ),
             Node(
                 package="robot_state_publisher",
@@ -40,6 +47,7 @@ def generate_launch_description():
                         ),
                     }
                 ],
+                # condition=UnlessCondition(balance)
             ),
         ]
     )
