894a6a83dc43c536ff6149aecba3c7a0bd0cf905
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-01-28 10:58:15 +0100

    WIP reference system

--- ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
+++ ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
@@ -16,6 +16,50 @@ using namespace std;
 ACADOvariables acadoVariables = {};
 ACADOworkspace acadoWorkspace = {};
 
+//void sinRef(vector<vector<double>>& ref, double freq, double amp, int N, double dt) {
+//
+//    // calculate the amount of time steps required for one period
+//    double T_N = ceil(1/(freq*dt));
+//
+//    // multiply the required time steps with the amount of required periods to have at least N reference values
+//    T_N = T_N*ceil(N/T_N)+1;
+//
+//    vector<double> line;
+//    for (int j = 0; j < T_N; j++) {
+//        // clear the line vector
+//        line.clear();
+//
+//        // fill the line vector with the three required references values
+//        line.push_back(amp*sin((freq*2*M_PI)*(j*dt))); // THETA
+//        line.push_back(amp*cos((freq*2*M_PI)*(j*dt))); // DTHETA
+//        line.push_back(0.0); // T
+//
+//        // add the temp vector to the reference
+//        ref.push_back(line);
+//    }
+//    // [Temporary] Has the function been executed?
+//    std::cout << "\033[4;32m" << __FUNCTION__ << "()\033[0m" << " has executed\n";
+//
+//}
+
+//void stepRef(vector<vector<double>>& ref, double amp, int N, double dt) {
+//
+//    vector<double> line;
+//    for (int j = 0; j < N; j++) {
+//        // clear the line vector
+//        line.clear();
+//
+//        // fill the line vector with the three required references values
+//        line.push_back(amp); // THETA
+//        line.push_back(0.0); // DTHETA
+//        line.push_back(0.0); // T
+//
+//        // add the temp vector to the reference
+//        ref.push_back(line);
+//    }
+//
+//}
+
 void ModelPredictiveController::setReference(vector<vector<double>> reference) {
     for(int i = 0; i < ACADO_N; i++) {
         for(int j = 0; j < ACADO_NY; j++) {
@@ -25,20 +69,18 @@ void ModelPredictiveController::setReference(vector<vector<double>> reference) {
     for(int j = 0; j < ACADO_NYN; j++) {
         acadoVariables.yN[j] = reference[ACADO_N][j];
     }
+    // [Temporary] Has the function been executed?
+    std::cout << "\033[4;32m" << __FUNCTION__ << "()\033[0m" << " has executed\n";
 }
 
 void ModelPredictiveController::scrollReference(vector<vector<double>>& reference) {
     reference.erase(reference.begin());
     reference.push_back(reference[0]);
+    // [Temporary] Has the function been executed?
+    std::cout << "\033[4;32m" << __FUNCTION__ << "()\033[0m" << " has executed\n";
 }
 
 void ModelPredictiveController::init() {
-
-  // Read reference data
-  vector<vector<double>> reference;
-  if (ModelPredictiveController::readReferenceFromFile("references/sin.csv", reference) == false) {
-      cout << "Cannot read reference" << endl;
-  }
 
   // Initialize the solver
   acado_initializeSolver();
@@ -53,9 +95,27 @@ void ModelPredictiveController::init() {
       acadoVariables.u[i * ACADO_NU] = 0; // T
   }
 
-  // Set reference and scroll one step up
-  ModelPredictiveController::setReference(reference);
-  ModelPredictiveController::scrollReference(reference);
+  // Setup and fill a reference vector
+//  vector<vector<double>> reference;
+//  ModelPredictiveController::sinRef(reference, 0.5, 1.0, ACADO_N, 0.04);
+//  bool repeat_reference = true;
+
+//  // Set reference and scroll one step up
+//  ModelPredictiveController::setReference(reference);
+//  if (repeat_reference) {
+//    ModelPredictiveController::scrollReference(reference);
+//  }
+
+    // Prepare references (step reference)
+    for (int i = 0; i < ACADO_N; ++i) {
+        acadoVariables.y[i * ACADO_NY] = 1.0; // theta
+        acadoVariables.y[i * ACADO_NY + 1] = 0;         // dtheta
+        acadoVariables.y[i * ACADO_NY + 2] = 0;         // T
+    }
+
+    acadoVariables.yN[0] = 1.0; // theta
+    acadoVariables.yN[1] = 0;         // dtheta
+    acadoVariables.yN[2] = 0;         // T
 
   // Current state feedback
   setInitialState(x0);
@@ -63,6 +123,8 @@ void ModelPredictiveController::init() {
   // Warm-up the solver
   acado_preparationStep();
 
+    // [Temporary] Has the function been executed?
+    std::cout << "\033[4;32m" << __FUNCTION__ << "()\033[0m" << " has executed\n";
 }
 
 void ModelPredictiveController::setInitialState(vector<double> x0) {
@@ -78,7 +140,7 @@ void ModelPredictiveController::calculateControlInput() {
 
   // Set reference
   ModelPredictiveController::setReference(reference);
-  ModelPredictiveController::scrollReference(reference);
+//  ModelPredictiveController::scrollReference(reference);
 
   // preparation step
   acado_preparationStep();
