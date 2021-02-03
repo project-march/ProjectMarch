54be1442744c9f2dec39568831d74e514451a8de
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-02-03 11:01:06 +0100

    Creating seperate source files for reference functions

--- ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
+++ ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
@@ -117,53 +117,6 @@ void ModelPredictiveController::scrollReference(vector<vector<double>>& referenc
     std::cout << "\033[4;32m" << __FUNCTION__ << "()\033[0m" << " has executed\n";
 }
 
-void ModelPredictiveController::sinRef(vector<vector<double>>& reference, double freq, double amp, int N, double dt) {
-
-  // calculate the amount of time steps required for one period
-  double T_N = ceil(1/(freq*dt));
-
-  // multiply the required time steps with the amount of required periods to have at least N reference values
-  T_N = T_N*ceil(N/T_N)+1;
-
-  vector<double> line;
-  for (int j = 0; j < T_N; j++) {
-    // clear the line vector
-    line.clear();
-
-    // fill the line vector with the three required references values
-    line.push_back(amp*sin((freq*2*M_PI)*(j*dt))); // THETA
-    line.push_back(amp*cos((freq*2*M_PI)*(j*dt))); // DTHETA
-    line.push_back(0.0); // T
-
-    // add the temp vector to the reference
-    reference.push_back(line);
-  }
-
-}
-
-void ModelPredictiveController::stepRef(vector<vector<double>>& ref, double amp, int N, double dt) {
-
-    vector<double> line;
-    for (int j = 0; j < N+1; j++) {
-        // clear the line vector
-        line.clear();
-
-        // fill the line vector with the three required references values
-        line.push_back(amp); // THETA
-        line.push_back(0.0); // DTHETA
-        line.push_back(0.0); // T
-
-        // add the temp vector to the reference
-        ref.push_back(line);
-    }
-
-}
-
-void ModelPredictiveController::scrollReference(vector<vector<double>>& reference) {
-    reference.erase(reference.begin());
-    reference.push_back(reference[0]);
-}
-
 void ModelPredictiveController::setReference(vector<vector<double>> reference) {
     for(int i = 0; i < ACADO_N; i++) {
         for(int j = 0; j < ACADO_NY; j++) {
@@ -174,7 +127,6 @@ void ModelPredictiveController::setReference(vector<vector<double>> reference) {
         acadoVariables.yN[j] = reference[ACADO_N][j];
     }
 }
-
 
 void ModelPredictiveController::init() {
 
