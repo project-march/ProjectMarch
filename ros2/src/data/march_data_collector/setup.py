f75d3ef7fdfac87f69ee2ca503b9214bd8f681ce
Katja Schmahl <katjaschmahl@hotmail.com>
2021-02-09 10:51:35 +0100

    Fix package setup to be ros2 compatible

--- ros2/src/data/march_data_collector/setup.py
+++ ros2/src/data/march_data_collector/setup.py
@@ -1,11 +1,25 @@
-# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
 
-setup_args = generate_distutils_setup(
-    packages=["march_data_collector"],
-    package_dir={"": "src"},
-    scripts=["scripts/march_data_collector"],
-)
+package_name = "march_data_collector"
 
-setup(**setup_args)
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=[
+        package_name
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Project March",
+    maintainer_email="software@projectmarch.nl",
+    description="Package that is responsible for computing the CoM using the IMU data "
+                "and computing the capture point for balance purposes",
+    license="TODO: License declaration",
+    tests_require=[],
+    entry_points={
+        "console_scripts": [
+            "data_collector = march_data_collector.data_collector_node:main"
+        ],
+    },
+)
