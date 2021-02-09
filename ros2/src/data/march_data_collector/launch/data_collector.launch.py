c616853bd7204704ad83e3152f9c65df436366fb
Katja Schmahl <katjaschmahl@hotmail.com>
2021-02-09 11:33:44 +0100

    Add launch file and add required messages to march_shared_msgs

---
+++ ros2/src/data/march_data_collector/launch/data_collector.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+# <arg name="moticon_ip" default="192.168.8.105" doc="The ip-adress with Moticon software running on it, defaults to
+# EMS switch laptop on standard router"/>
+# <arg name="pressure_soles" default="false" doc="Whether pressure soles will be connected"/>
+# <arg name="logfile" default="false" doc="Whether the data input is from a log file" />
+# <arg name="robot" default="march4" doc="The robot to run. Can be: march3, march4, test_joint_linear, test_joint_rotational."/>
+
+
+def generate_launch_description():
+    """ Basic launch file to launch the gait selection node """
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="true",
+                description="Whether to use simulation time as published on the "
+                            "/clock topic by gazebo instead of system time.",
+            ),
+            DeclareLaunchArgument(
+                "moticon_ip",
+                default_value="192.168.8.105",
+                description="The ip-adress with Moticon software running on it, "
+                            "defaults to EMS switch laptop on standard router",
+            ),
+            DeclareLaunchArgument(
+                "pressure_soles",
+                default_value="false",
+                description="Whether the pressure soles will be connected"
+            ),
+            DeclareLaunchArgument(
+                "logfile",
+                default_value="false",
+                description="Whether the data input is from a log file."
+            ),
+            Node(
+                package="march_data_collector",
+                executable="data_collector",
+                output="screen",
+                name="data_collector",
+                parameters=[
+                    {"use_sim_time": LaunchConfiguration("use_sim_time")},
+                    {"moticon_ip": LaunchConfiguration("moticon_ip")},
+                    {"pressure_soles": LaunchConfiguration("pressure_soles")},
+                    {"logfile": LaunchConfiguration("logfile")},
+                ],
+            ),
+        ]
+    )
--- ros2/src/data/march_data_collector/setup.py
+++ ros2/src/data/march_data_collector/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import setup
 
 package_name = "march_data_collector"
@@ -6,15 +9,21 @@ package_name = "march_data_collector"
 setup(
     name=package_name,
     version="0.0.0",
-    packages=[
-        package_name
+    packages=[package_name],
+    data_files=[
+        (
+            os.path.join("share", "ament_index", "resource_index", "packages"),
+            [os.path.join("resource", package_name)],
+        ),
+        (os.path.join("share", package_name), ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="Project March",
     maintainer_email="software@projectmarch.nl",
     description="Package that is responsible for computing the CoM using the IMU data "
-                "and computing the capture point for balance purposes",
+    "and computing the capture point for balance purposes",
     license="TODO: License declaration",
     tests_require=[],
     entry_points={
