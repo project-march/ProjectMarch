60e6a8e27a648a892b77e02b10aa6524e552274e
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-02-10 15:48:02 +0100

    Switch traceback and the showing of the warning message

--- ros1/src/gaits/march_rqt_gait_generator/src/march_rqt_gait_generator/gait_generator_controller.py
+++ ros1/src/gaits/march_rqt_gait_generator/src/march_rqt_gait_generator/gait_generator_controller.py
@@ -430,17 +430,17 @@ class GaitGeneratorController(object):
             setpoint_dictionary = self.get_setpoints_from_inverse_kinematics_input()
             self.add_setpoints_from_dictionary(setpoint_dictionary)
         except SubgaitInterpolationError:
+            traceback.print_exc()
             self.view.message(
                 "The inverse kinematics setpoints feature as failed.",
                 "A subgait interpolation error occured, see the terminal for more information.",
             )
-            traceback.print_exc()
         except ValueError:
+            traceback.print_exc()
             self.view.message(
                 "The inverse kinematics setpoints feature has failed.",
                 "A ValueError occured, see the terminal for more information",
             )
-            traceback.print_exc()
 
     def transform_inverse_kinematics_setpoints_inputs(self) -> None:
         """Transform the input coordinates (relative to a default foot location) to coordinates relative to the exo."""
