cf80525a6625ffcc2a284089f2557d28b52438f5
Bas <basvolkers99@gmail.com>
2021-02-11 15:43:42 +0100

    Finalize MotorControllerState class

--- ros1/src/hardware/interface/march_hardware/include/march_hardware/motor_controller/motor_controller_state.h
+++ ros1/src/hardware/interface/march_hardware/include/march_hardware/motor_controller/motor_controller_state.h
@@ -6,14 +6,6 @@
 
 namespace march
 {
-class MotorControllerStateOfOperation
-{
-public:
-  MotorControllerStateOfOperation() = default;
-  virtual std::string toString();
-};
-
-
 class MotorControllerState
 {
 public:
@@ -22,8 +14,7 @@ public:
                        double absolute_angle_iu, double incremental_angle_iu,
                        double absolute_velocity_iu, double incremental_velocity_iu,
                        double absolute_angle_rad, double incremental_angle_rad,
-                       double absolute_velocity_rad, double incremental_velocity_rad,
-                       MotorControllerStateOfOperation state_of_operation)
+                       double absolute_velocity_rad, double incremental_velocity_rad)
     : motor_current_(motor_current)
     , motor_voltage_(motor_voltage)
     , absolute_angle_iu_(absolute_angle_iu)
@@ -34,7 +25,6 @@ public:
     , incremental_angle_rad_(incremental_angle_rad)
     , absolute_velocity_rad_(absolute_velocity_rad)
     , incremental_velocity_rad_(incremental_velocity_rad)
-    , state_of_operation_(state_of_operation)
   {}
 
   float motor_current_;
@@ -47,18 +37,23 @@ public:
   double incremental_angle_rad_;
   double absolute_velocity_rad_;
   double incremental_velocity_rad_;
-  MotorControllerStateOfOperation state_of_operation_;
 
   /**
    * Check whether the motor controller is in an error state
    * @return false if the motor controller is in error state, otherwise true
    */
-  virtual bool checkState() = 0;
+  virtual bool isOk() = 0;
   /**
    * Get a string description of the state and error states of the motor controller
    * @return string describing the current state as well as the error state(s) of the motor controller
    */
   virtual std::string getErrorStatus() = 0;
+
+  /**
+   * Get a string description of the operational state
+   * @return string describing the operational state
+   */
+  virtual std::string getOperationalState() = 0;
 };
 
 }  // namespace march
