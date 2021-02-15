cb5c270552578396f1eb945370252b3e06f1cc8e
Katja Schmahl <katjaschmahl@hotmail.com>
2021-02-15 15:01:45 +0100

    Small bugfix

--- ros1/src/data/march_realsense_reader/src/realsense_reader.cpp
+++ ros1/src/data/march_realsense_reader/src/realsense_reader.cpp
@@ -28,9 +28,9 @@ void RealSenseReader::pointcloud_callback(const PointCloud::ConstPtr& input_clou
   if (reading_)
   {
     // All logic to execute with a pointcloud will be executed here.
-    ROS_INFO_STREAM("Processing point cloud at time " << msg->header.stamp);
+    ROS_INFO_STREAM("Processing point cloud at time " << input_cloud->header.stamp);
     reading_ = false;
-    PointCloud::Ptr pointcloud = boost::make_shared<PointCloud>(*msg);
+    PointCloud::Ptr pointcloud = boost::make_shared<PointCloud>(*input_cloud);
     Normals::Ptr normals = boost::make_shared<Normals>();
     std::unique_ptr<SimplePreprocessor> preprocessor =
         std::make_unique<SimplePreprocessor>(config_file_, pointcloud, normals);
