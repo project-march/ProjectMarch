fd0701437d3d1f0804566ce3d92c8e80d38723ed
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-02-16 10:58:47 +0100

    Test downsampling.

--- ros1/src/data/march_realsense_reader/src/preprocessor.cpp
+++ ros1/src/data/march_realsense_reader/src/preprocessor.cpp
@@ -2,6 +2,9 @@
 #include "yaml-cpp/yaml.h"
 #include <ros/ros.h>
 
+#include <pcl/filters/voxel_grid.h>
+
+
 using PointCloud = pcl::PointCloud<pcl::PointXYZ>;
 using Normals = pcl::PointCloud<pcl::Normal>;
 
@@ -36,16 +39,23 @@ void SimplePreprocessor::preprocess()
 void NormalPreprocessor::preprocess()
 {
   ROS_INFO_STREAM("Preprocessing with normal filtering. Number of points BEFORE downsampling " << pointcloud_->points.size());
-  DownSample();
+//  DownSample();
+
+  pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
+  voxel_grid.setInputCloud(pointcloud_);
+  double leaf_size = config_tree_["leaf_size"].as<double>(); // How is the type of leaf_size determined?
+  voxel_grid.setLeafSize(leaf_size, leaf_size, leaf_size);
+  voxel_grid.filter(*pointcloud_);
+
   ROS_INFO_STREAM("Number of points AFTER downsampling " << pointcloud_->points.size());
 
 }
 
-void DownSample()
-{
-  pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
-  voxel_grid.setInputCloud(pointcloud_);
-  double leaf_size = config_tree_["leaf_size"];
-  voxel_grid.setLeafSize(leaf_size, leaf_size, leaf_size);
-  voxel_grid.filter(*pointcloud_);
-}
+//void DownSample()
+//{
+//  pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
+//  voxel_grid.setInputCloud(pointcloud_);
+//  double leaf_size = config_tree_["leaf_size"];
+//  voxel_grid.setLeafSize(leaf_size, leaf_size, leaf_size);
+//  voxel_grid.filter(*pointcloud_);
+//}
