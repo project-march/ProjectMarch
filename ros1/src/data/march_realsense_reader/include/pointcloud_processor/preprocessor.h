3f819fe7244600634cca0e8d2d0021d0f4071f74
Katja Schmahl <katjaschmahl@hotmail.com>
2021-02-20 11:19:44 +0100

    Move comments

--- ros1/src/data/march_realsense_reader/include/pointcloud_processor/preprocessor.h
+++ ros1/src/data/march_realsense_reader/include/pointcloud_processor/preprocessor.h
@@ -18,8 +18,8 @@ class Preprocessor {
     Preprocessor(std::string file_name,
                  PointCloud::Ptr pointcloud,
                  Normals::Ptr normal_pointcloud);
-    virtual void preprocess()=0; // This function is required to be implemented by
-    // any preprocessor
+    // This function is required to be implemented by any preprocessor
+    virtual void preprocess()=0;
     virtual ~Preprocessor() {};
 
     PointCloud::Ptr pointcloud_;
@@ -29,7 +29,8 @@ class Preprocessor {
 
 class SimplePreprocessor : Preprocessor {
   public:
-    using Preprocessor::Preprocessor; //Use the constructors defined in the super class
+    //Use the constructors defined in the super class
+    using Preprocessor::Preprocessor;
     void preprocess();
 };
 
