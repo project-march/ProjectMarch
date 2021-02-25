c8846afb8cbb3b9e677ee30b0405e7b34aeeda71
Katja Schmahl <katjaschmahl@hotmail.com>
2021-02-25 09:55:01 +0100

    Fix transform from /tf topic

--- ros1/src/data/march_realsense_reader/src/preprocessor.cpp
+++ ros1/src/data/march_realsense_reader/src/preprocessor.cpp
@@ -1,6 +1,8 @@
 #include <pointcloud_processor/preprocessor.h>
 #include "yaml-cpp/yaml.h"
 #include <ros/ros.h>
+#include <tf2_ros/transform_listener.h>
+#include <pcl_ros/transforms.h>
 
 using PointCloud = pcl::PointCloud<pcl::PointXYZ>;
 using Normals = pcl::PointCloud<pcl::Normal>;
@@ -27,8 +29,27 @@ Preprocessor::Preprocessor(
   config_tree_ = YAML::LoadFile(path)["preprocessor"];
 }
 
-void SimplePreprocessor::preprocess()
-{
+void SimplePreprocessor::preprocess() {
   ROS_INFO_STREAM("Preprocessing, test_parameter is " <<
   config_tree_["test_parameter"]);
+
+}
+
+void SimplePreprocessor::transformPointCloudFromUrdf() {
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+
+  geometry_msgs::TransformStamped transformStamped;
+  try
+  {
+    transformStamped = tfBuffer.lookupTransform("camera_link", "foot_left",
+                                                ros::Time::now(), ros::Duration(0.5));
+    pcl_ros::transformPointCloud(*pointcloud_, *pointcloud_,
+                                 transformStamped.transform);
+  }
+  catch (tf2::TransformException &ex)
+  {
+    ROS_WARN("%s", ex.what());
+    return;
+  }
 }
