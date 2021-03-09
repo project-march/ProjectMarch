8dd48723d0d5643d4b8ea5bb8ef8869dcbcac282
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-03-09 10:23:56 +0000

    Increase controllerUpdateSoftBound value

--- ros1/src/control/march_acado_mpc/include/model_predictive_controller.hpp
+++ ros1/src/control/march_acado_mpc/include/model_predictive_controller.hpp
@@ -22,7 +22,7 @@ public:
     // Timing variables
     acado_timer t;
     double t_preparation, t_feedback;
-    double controllerUpdateSoftBound = 1e-3; // 1 ms
+    double controllerUpdateSoftBound = 1e-2; // 1 ms
 
     // status variables
     int preparationStepStatus;
