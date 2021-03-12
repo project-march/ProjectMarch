4b44ffc57b0537615f03352b1850683041da7153
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-03-12 14:31:18 +0100

    Change default verbosity to debug.

--- ros1/src/data/march_realsense_reader/src/realsense_reader_node.cpp
+++ ros1/src/data/march_realsense_reader/src/realsense_reader_node.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
-
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <march_realsense_reader/realsense_reader.h>
 
 
@@ -11,7 +11,9 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
 
   RealSenseReader reader = RealSenseReader(&n);
-
+  if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+    ros::console::notifyLoggerLevelsChanged();
+  }
   ros::spin();
   ros::shutdown();
   return 0;
