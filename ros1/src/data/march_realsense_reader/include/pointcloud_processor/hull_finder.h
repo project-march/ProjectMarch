577e63cd064e3441ef65821b80ce921b4bd33127
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-03-12 17:36:17 +0100

    Add code structure.

--- ros1/src/data/march_realsense_reader/include/pointcloud_processor/hull_finder.h
+++ ros1/src/data/march_realsense_reader/include/pointcloud_processor/hull_finder.h
@@ -9,10 +9,14 @@
 
 using PointCloud = pcl::PointCloud<pcl::PointXYZ>;
 using Normals = pcl::PointCloud<pcl::Normal>;
-using RegionVector = std::vector<pcl::PointIndices>;
-using PlaneParameterVector = std::vector<pcl::ModelCoefficients::Ptr>;
-using HullVector = std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr>;
-using PolygonVector = std::vector<std::vector<pcl::Vertices>>;
+using Region = pcl::PointIndices;
+using PlaneCoefficients = pcl::ModelCoefficients::Ptr;
+using Hull = pcl::PointCloud<pcl::PointXYZ>::Ptr;
+using Polygon = std::vector<pcl::Vertices>;
+using RegionVector = std::vector<Region>;
+using PlaneCoefficientsVector = std::vector<PlaneCoefficients>;
+using HullVector = std::vector<Hull>;
+using PolygonVector = std::vector<Polygon>;
 
 class HullFinder
 {
@@ -40,7 +44,7 @@ protected:
     bool debugging_;
 };
 
-class SimpleHullFinder : HullFinder
+class CHullFinder : HullFinder
 {
 public:
     //Use the constructors defined in the super class
@@ -53,6 +57,34 @@ public:
                      boost::shared_ptr<PlaneParameterVector> plane_parameter_vector,
                      boost::shared_ptr<HullVector> hull_vector,
                      boost::shared_ptr<PolygonVector> polygon_vector) override;
+
+protected:
+    // Converts a region into a convex or concave hull
+    bool getCHullFromRegion();
+
+    // Get the plane coefficients of the region using average point and normal
+    bool getPlaneCoefficientsRegion();
+
+    // project the region to its plane so a convex or concave hull can be create
+    bool projectRegionToPlane();
+
+    // Create the convex or concave hull from a projected region
+    bool getCHullFromProjectedPlane();
+
+    // Add the hull to a vector together with its plane coefficients and polygons
+    bool addCHullToVector();
+
+    // Reads all the relevant parameters from the yaml file
+    bool readYaml();
+
+    // Plane coefficients given as [0]*x + [1]*y + [2]*z + [3] = 0
+    pcl::ModelCoefficients::Ptr  plane_coefficients;
+
+    int region_index;
+    int region_vector_length;
+    PointCloud::Ptr region_points_;
+    PonitCloud::Ptr region_normals_;
+
 };
 
 #endif //MARCH_HULL_FINDER_H
