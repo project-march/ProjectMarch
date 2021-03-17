1a41ad11ced40a829571e177fd5dc557eedef8c0
Herbert Van Even <herbertve.za@gmail.com>
2021-03-17 11:07:03 +0100

    Added a gait for the linear test joint and restructured the test joint gait directories

--- ros2/src/visualization/march_rqt_input_device/march_rqt_input_device/input_device_view.py
+++ ros2/src/visualization/march_rqt_input_device/march_rqt_input_device/input_device_view.py
@@ -403,9 +403,14 @@ class InputDeviceView(QWidget):
         home_test_gait_button = self.create_button(
             "home_setup", callback=lambda: self._controller.publish_gait("home_setup")
         )
-        testjoint_gait_button = self.create_button(
-            "test_joint_gait",
-            callback=lambda: self._controller.publish_gait("test_joint_gait"),
+        rot_testjoint_gait_button = self.create_button(
+            "rot_test_joint_gait",
+            callback=lambda: self._controller.publish_gait("rot_test_joint_gait"),
+        )
+
+        lin_testjoint_gait_button = self.create_button(
+            "lin_test_joint_gait",
+            callback=lambda: self._controller.publish_gait("lin_test_joint_gait"),
         )
 
         # The button layout.
@@ -486,7 +491,7 @@ class InputDeviceView(QWidget):
                 gait_tilted_path_second_end,
             ],
             [ik_test, gait_curb],
-            [home_test_gait_button, testjoint_gait_button],
+            [home_test_gait_button, rot_testjoint_gait_button, lin_testjoint_gait_button],
         ]
 
         # Create the qt_layout from the button layout.
