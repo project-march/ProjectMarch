bb95230979cc45b6778ce24c3bee54ca6ebaab6d
wnederpel <wolf.nederpel@gmail.com>
2021-04-02 15:50:43 +0200

    Start with changing the linear algebra utilities to a source file.

--- ros1/src/data/march_realsense_reader/include/utilities/linear_algebra_utilities.h
+++ ros1/src/data/march_realsense_reader/include/utilities/linear_algebra_utilities.h
@@ -1,24 +1,15 @@
 #ifndef MARCH_LINEAR_ALGEBRA_UTILITIES_H
 #define MARCH_LINEAR_ALGEBRA_UTILITIES_H
 
+#include <vector>
+
 namespace linear_algebra_utilities
 {
-  // Calculate a dot product of two vectors
   template<typename T>
-  T dotProductVector(std::vector<T> vector1, std::vector<T> vector2) {
-    return vector1[0] * vector2[0] +
-           vector1[1] * vector2[1] +
-           vector1[2] * vector2[2];
-  }
+  T dotProductVector(std::vector<T> vector1, std::vector<T> vector2);
 
-  // Calculate a dot product of two objects with x, y and z attributes
   template<typename T>
-  double dotProductPoint(T point1, T point2)
-  {
-    return point1.z * point2.x +
-           point1.y * point2.y +
-           point1.z * point2.z;
-  }
+  T dotProductPoint(T point1, T point2);
 }
 
 #endif //MARCH_LINEAR_ALGEBRA_UTILITIES_H
