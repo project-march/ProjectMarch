19ae28a397d81f33cc5c87fb9a790afa8ba68d39
Thijs Raymakers <thijs@raymakers.nl>
2021-04-09 14:27:40 +0200

    Reformat march_realsense_reader

--- ros1/src/data/march_realsense_reader/include/utilities/yaml_utilities.h
+++ ros1/src/data/march_realsense_reader/include/utilities/yaml_utilities.h
@@ -5,26 +5,26 @@
 #include <string>
 #include <typeinfo>
 
-namespace yaml_utilities
+namespace yaml_utilities {
+// Grab a parameter from a YAML::Node and throws a clear warning if the
+// requested parameter does not exist
+template <typename T>
+T grabParameter(YAML::Node const yaml_node, std::string const parameter_name,
+    bool success = true)
 {
-  // Grab a parameter from a YAML::Node and throws a clear warning if the requested parameter does not exist
-  template<typename T>
-  T grabParameter(YAML::Node const yaml_node, std::string const parameter_name, bool success = true)
-  {
     T parameter;
-    if (YAML::Node raw_parameter = yaml_node[parameter_name])
-    {
-      parameter = raw_parameter.as<T>();
-      success &= true;
-      return parameter;
+    if (YAML::Node raw_parameter = yaml_node[parameter_name]) {
+        parameter = raw_parameter.as<T>();
+        success &= true;
+        return parameter;
+    } else {
+        ROS_ERROR_STREAM(
+            "Parameter not found in the given YAML::node. Parameter name is "
+            << parameter_name);
+        success &= false;
+        return parameter;
     }
-    else
-    {
-      ROS_ERROR_STREAM("Parameter not found in the given YAML::node. Parameter name is " << parameter_name);
-      success &= false;
-      return parameter;
-    }
-  }
 }
+} // namespace yaml_utilities
 
-#endif //MARCH_YAML_UTILITIES_H
+#endif // MARCH_YAML_UTILITIES_H
