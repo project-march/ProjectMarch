7d78b20f90cfd73d3fc18f97df5c3a55d127ec67
Thijs Raymakers <thijs@raymakers.nl>
2021-04-09 17:03:25 +0200

    Refactor march_gazebo_plugins

--- ros1/src/simulation/march_gazebo_plugins/include/contact/contact_plugin.h
+++ ros1/src/simulation/march_gazebo_plugins/include/contact/contact_plugin.h
@@ -7,36 +7,34 @@
 #include <std_msgs/Bool.h>
 #include <string>
 
-namespace gazebo
-{
+namespace gazebo {
 /// \brief An example plugin for a contact sensor.
-class ContactPlugin : public SensorPlugin
-{
+class ContactPlugin : public SensorPlugin {
 
-  /// \brief Load the sensor plugin.
-  /// \param[in] _sensor Pointer to the sensor that loaded this plugin.
-  /// \param[in] _sdf SDF element that describes the plugin.
+    /// \brief Load the sensor plugin.
+    /// \param[in] _sensor Pointer to the sensor that loaded this plugin.
+    /// \param[in] _sdf SDF element that describes the plugin.
 public:
-  void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf) override;
+    void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf) override;
 
-  /// \brief Callback that receives the contact sensor's update signal.
+    /// \brief Callback that receives the contact sensor's update signal.
 private:
-  void OnUpdate();
+    void OnUpdate();
 
-  /// \brief Pointer to the contact sensor
-  sensors::ContactSensorPtr parentSensor;
+    /// \brief Pointer to the contact sensor
+    sensors::ContactSensorPtr parentSensor;
 
-  /// \brief Connection that maintains a link between the contact sensor's
-  /// updated signal and the OnUpdate callback.
-  event::ConnectionPtr updateConnection;
+    /// \brief Connection that maintains a link between the contact sensor's
+    /// updated signal and the OnUpdate callback.
+    event::ConnectionPtr updateConnection;
 
-  /// \brief The name of the contact sensor
-  std::string name;
-  /// \brief A node use for ROS transport
-  std::unique_ptr<ros::NodeHandle> ros_node_;
+    /// \brief The name of the contact sensor
+    std::string name;
+    /// \brief A node use for ROS transport
+    std::unique_ptr<ros::NodeHandle> ros_node_;
 
-  /// \brief A ROS subscriber
-  ros::Publisher ros_pub_;
+    /// \brief A ROS subscriber
+    ros::Publisher ros_pub_;
 };
-}  // namespace gazebo
+} // namespace gazebo
 #endif
