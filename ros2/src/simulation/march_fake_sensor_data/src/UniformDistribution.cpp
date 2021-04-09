ce40c0c57dab9ee6d2c38570b1f15be544641547
Thijs Raymakers <thijs@raymakers.nl>
2021-04-09 17:06:03 +0200

    Reformat march_fake_sensor_data

--- ros2/src/simulation/march_fake_sensor_data/src/UniformDistribution.cpp
+++ ros2/src/simulation/march_fake_sensor_data/src/UniformDistribution.cpp
@@ -5,10 +5,10 @@
 /**
  * @file UniformDistribution.cpp
  * @brief Provide a wrapper around the random functions in the standard library.
- * @details The wrapper makes sure that the same distribution will be used, unless
- * the range of the distribution is changed. In that case, a new distribution is
- * created. This is necessary to ensure that the generated pseudo random distribution
- * has pseude random properties.
+ * @details The wrapper makes sure that the same distribution will be used,
+ * unless the range of the distribution is changed. In that case, a new
+ * distribution is created. This is necessary to ensure that the generated
+ * pseudo random distribution has pseude random properties.
  */
 
 /**
@@ -17,8 +17,8 @@
  * @param end The upper bound (inclusive)
  * @return An object that can generate random distributions.
  */
-UniformDistribution::UniformDistribution(const int start, const int end):
-    generator(random_device())
+UniformDistribution::UniformDistribution(const int start, const int end)
+    : generator(random_device())
 {
     set_range(start, end);
 }
--- ros2/src/simulation/march_fake_sensor_data/test/UniformDistributionTest.cpp
+++ ros2/src/simulation/march_fake_sensor_data/test/UniformDistributionTest.cpp
@@ -1,14 +1,14 @@
 // Copyright 2020 Project MARCH
 
-#include <gtest/gtest.h>
 #include "march_fake_sensor_data/UniformDistribution.hpp"
+#include <gtest/gtest.h>
 
 TEST(UniformDistributionTest, generate_within_bounds)
 {
     int lower_bound = -5;
-    int upper_bound = 5; 
+    int upper_bound = 5;
     UniformDistribution distribution { lower_bound, upper_bound };
-    for (int i {0}; i < 30; ++i) {
+    for (int i { 0 }; i < 30; ++i) {
         auto number = distribution.get_random_number();
         ASSERT_LE(lower_bound, number);
         ASSERT_GE(upper_bound, number);
@@ -19,7 +19,7 @@ TEST(UniformDistributionTest, valid_change_range)
 {
     UniformDistribution distribution { -100, 100 };
     distribution.set_range(-1, 1);
-    for (int i {0}; i < 10; ++i) {
+    for (int i { 0 }; i < 10; ++i) {
         auto number = distribution.get_random_number();
         ASSERT_LE(-1, number);
         ASSERT_GE(1, number);
@@ -31,7 +31,7 @@ TEST(UniformDistributionTest, inverted_range)
     int lower_bound = -3;
     int upper_bound = 3;
     UniformDistribution distribution { upper_bound, lower_bound };
-    for (int i {0}; i < 30; ++i) {
+    for (int i { 0 }; i < 30; ++i) {
         auto number = distribution.get_random_number();
         ASSERT_LE(lower_bound, number);
         ASSERT_GE(upper_bound, number);
