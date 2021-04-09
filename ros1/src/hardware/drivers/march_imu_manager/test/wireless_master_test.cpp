53ffad65a7fd512200be63e9bb28a748e8fdc3be
Thijs Raymakers <thijs@raymakers.nl>
2021-04-09 14:28:44 +0200

    Reformat march_imu_manager

--- ros1/src/hardware/drivers/march_imu_manager/test/wireless_master_test.cpp
+++ ros1/src/hardware/drivers/march_imu_manager/test/wireless_master_test.cpp
@@ -5,37 +5,38 @@
 
 TEST(WirelessMasterTest, emptyRates)
 {
-  XsIntArray rates(0, nullptr);
-  const int rate = WirelessMaster::findClosestUpdateRate(rates, 0);
-  ASSERT_EQ(rate, 0);
+    XsIntArray rates(0, nullptr);
+    const int rate = WirelessMaster::findClosestUpdateRate(rates, 0);
+    ASSERT_EQ(rate, 0);
 }
 
 TEST(WirelessMasterTest, oneRate)
 {
-  const int supportedRate = 60;
-  XsIntArray rates(1, &supportedRate);
-  const int rate = WirelessMaster::findClosestUpdateRate(rates, 0);
-  ASSERT_EQ(rate, supportedRate);
+    const int supportedRate = 60;
+    XsIntArray rates(1, &supportedRate);
+    const int rate = WirelessMaster::findClosestUpdateRate(rates, 0);
+    ASSERT_EQ(rate, supportedRate);
 }
 
 TEST(WirelessMasterTest, matchingRate)
 {
-  const int supportedRates[3] = { 60, 80, 100 };
-  XsIntArray rates(3, supportedRates);
-  const int rate = WirelessMaster::findClosestUpdateRate(rates, supportedRates[1]);
-  ASSERT_EQ(rate, supportedRates[1]);
+    const int supportedRates[3] = { 60, 80, 100 };
+    XsIntArray rates(3, supportedRates);
+    const int rate
+        = WirelessMaster::findClosestUpdateRate(rates, supportedRates[1]);
+    ASSERT_EQ(rate, supportedRates[1]);
 }
 
 TEST(WirelessMasterTest, twoClosestRates)
 {
-  const int supportedRates[2] = { 10, 20 };
-  XsIntArray rates(2, supportedRates);
-  const int rate = WirelessMaster::findClosestUpdateRate(rates, 15);
-  ASSERT_EQ(rate, supportedRates[0]);
+    const int supportedRates[2] = { 10, 20 };
+    XsIntArray rates(2, supportedRates);
+    const int rate = WirelessMaster::findClosestUpdateRate(rates, 15);
+    ASSERT_EQ(rate, supportedRates[0]);
 }
 
 int main(int argc, char* argv[])
 {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
