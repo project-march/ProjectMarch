c3b7b20b3efb98075f588617e7a36e8b67de23c7
Thijs Raymakers <thijs@raymakers.nl>
2021-04-13 16:59:30 +0200

    Add semicolon after template declaration in linear algebra utility of march_realsense_reader

--- ros1/src/data/march_realsense_reader/include/utilities/linear_algebra_utilities.h
+++ ros1/src/data/march_realsense_reader/include/utilities/linear_algebra_utilities.h
@@ -12,13 +12,13 @@ T dotProductVector(std::vector<T> vector1, std::vector<T> vector2)
 {
     return vector1[0] * vector2[0] + vector1[1] * vector2[1]
         + vector1[2] * vector2[2];
-}
+};
 
 // Calculate a dot product of two objects with x, y and z attributes
 template <typename T> double dotProductPoint(T point1, T point2)
 {
     return point1.x * point2.x + point1.y * point2.y + point1.z * point2.z;
-}
+};
 
 // Calculate the distance between two points
 template <typename T, typename Q>
