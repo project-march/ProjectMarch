359b6279acb27df57691367e0b2a3307bc481ebb
Bas <basvolkers99@gmail.com>
2021-04-15 11:07:20 +0200

    Change some parameters

--- ros2/src/gaits/march_gait_selection/launch/gait_selection.launch.py
+++ ros2/src/gaits/march_gait_selection/launch/gait_selection.launch.py
@@ -32,14 +32,14 @@ def generate_launch_description():
             ),
             DeclareLaunchArgument(
                 name="first_subgait_delay",
-                default_value="0.25",
+                default_value="0.2",
                 description="Duration to wait before starting first subgait."
                 "If 0 then the first subgait is started immediately,"
                 "dropping the first setpoint in the process.",
             ),
             DeclareLaunchArgument(
                 name="early_schedule_duration",
-                default_value="0.1",
+                default_value="0.2",
                 description="Duration to schedule next subgait early. If 0 then the"
                 "next subgait is never scheduled early.",
             ),
