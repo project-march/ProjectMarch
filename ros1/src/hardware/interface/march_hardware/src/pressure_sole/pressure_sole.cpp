fe4f12c68a5655f33cfc8608231468ea716bc152
Thijs Raymakers <thijs@raymakers.nl>
2021-04-21 15:54:47 +0200

    Initialize non-initialize array in march_hardware/pressure_sole

--- ros1/src/hardware/interface/march_hardware/src/pressure_sole/pressure_sole.cpp
+++ ros1/src/hardware/interface/march_hardware/src/pressure_sole/pressure_sole.cpp
@@ -14,7 +14,7 @@ std::string PressureSole::getSide()
 
 PressureSoleData PressureSole::read()
 {
-    std::array<float, PRESSURE_SOLE_DATA_LENGTH> data;
+    std::array<float, PRESSURE_SOLE_DATA_LENGTH> data {};
     for (unsigned int i = 0; i < data.size(); i++) {
         // Increment the offset by 4 bytes each iteration
         data[i] = this->read32(byte_offset_ + i * sizeof(float)).f;
