2058256972f33dee569f4ca6c43820929ecb08b5
Thijs Raymakers <thijs@raymakers.nl>
2021-04-23 13:57:15 +0200

    Remove march_imu_manager from ROS 1

--- ros1/src/hardware/drivers/march_imu_manager/lib/include/xsens/xsbusid.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:533481d3efa73099627073938d0f56c2e43e2bc9e609531e14640ef839473c1a
-size 1791
--- ros1/src/hardware/drivers/march_imu_manager/lib/include/xsens/xstypes_include.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:677f158e7d3ca6829ab490a6df3a273b770af7f36e1415f855fbffc365ebfaf6
-size 1141
