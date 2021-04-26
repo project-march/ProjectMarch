186f8bb5455bc42c345d9b9762707f5529c381a0
ttveen <ttveen@gmail.com>
2021-04-26 17:00:29 +0200

    Add Flask

--- ros2/src/visualization/march_mpc_visualization/march_mpc_visualization/mpc_visualization_listener.py
+++ ros2/src/visualization/march_mpc_visualization/march_mpc_visualization/mpc_visualization_listener.py
@@ -2,15 +2,45 @@ import rclpy
 from rclpy.node import Node
 from march_shared_msgs.msg import MpcMsg
 
+from flask import jsonify, make_response
+from march_mpc_visualization import app
+
+STREAM_PERIOD = 0.02
+
 
 class MpcListener(Node):
     def __init__(self):
         super().__init__('mpc_listener')
         self.subscription = self.create_subscription(MpcMsg, '/march/mpc', self.listener_callback, 1)
-        self.number_of_joints = 8
-        self.new_data_position = 0.0
+
+        #self.number_of_joints = 8
+
+        #self.new_data_position = 0.0
+
+
+        # 0 - 100
+        # Current actual -> append
+        # Desired N -> replace
+        # Calculated input N -> replace
+
+        #self.create_timer(STREAM_PERIOD, self.stream)
 
     # Set all data
     def listener_callback(self, msg):
-        self.new_data_position = msg.joint[0].estimation.states[0].array[0]
-        self.get_logger().info('I heard: "%f"' % self.new_data_position)
+        pass
+        #self.new_data_position = msg.joint[0].estimation.states[0].array[0]
+        #self.get_logger().info('I heard: "%f"' % self.new_data_position)
+
+    def init_bokeh(self):
+        pass
+
+    # callback, of op timer
+    @app.route("/json")
+    def stream(self):
+        response_body = {
+            "x": 5,
+            "y": 10
+        }
+        response = make_response(jsonify(response_body), 200)
+        return "Thanks"
+
--- ros2/src/visualization/march_mpc_visualization/march_mpc_visualization/mpc_visualization_node.py
+++ ros2/src/visualization/march_mpc_visualization/march_mpc_visualization/mpc_visualization_node.py
@@ -1,17 +1,14 @@
 import rclpy
-from rclpy.node import Node
 from march_mpc_visualization.mpc_visualization_listener import MpcListener
-from march_mpc_visualization.mpc_visualization_streamer import MpcStreamer
-
+from march_mpc_visualization import app
 
 
 def main(args=None):
     """The main function used to start up the rqt note taker."""
     rclpy.init(args=args)
 
-    #node = Node('march_mpc_visualization_node')
     mpc_listener = MpcListener()
-    mpc_streamer = MpcStreamer(mpc_listener)
+    app.run()
 
     try:
         rclpy.spin(mpc_listener)
