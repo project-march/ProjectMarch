392664141318cf048364e66e6a378a328b623770
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-05-04 11:49:01 +0200

    Fix launch argument, add result message, change names to english

--- ros1/src/data/march_realsense_test_publisher/src/realsense_test_publisher.cpp
+++ ros1/src/data/march_realsense_test_publisher/src/realsense_test_publisher.cpp
@@ -248,6 +248,7 @@ void RealsenseTestPublisher::updatePublishLoop(
             makeProcessPointcloudCall();
         }
         res.success = success;
+        res.message = "Now publishing pointcloud with name " + pointcloud_file_name;
     } else {
         ROS_ERROR_STREAM(
             "No .ply files can be found by the test publisher under path "
