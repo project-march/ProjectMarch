f491e6589f9089b903f5a0921bbc6fdde8695868
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2021-05-05 12:19:00 +0200

    Constructor parameter 'W' is now getting moved to avoid unnecessary copies in model_predictive_controller.cpp

--- ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
+++ ros1/src/control/march_acado_mpc/src/model_predictive_controller.cpp
@@ -21,7 +21,7 @@ ACADOvariables acadoVariables = {};
 ACADOworkspace acadoWorkspace = {};
 
 ModelPredictiveController::ModelPredictiveController(std::vector<float> W)
-    : W_(W)
+    : W_(std::move(W))
 {
 }
 
