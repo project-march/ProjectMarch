5960794c41e80f8b44e81f6b3983745e8d72debc
Thijs Raymakers <thijs@raymakers.nl>
2021-05-11 15:51:43 +0200

    Add basic python package for the smartglasses bridge

---
+++ ros2/src/hardware/interface/march_smartglasses_bridge/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'march_smartglasses_bridge'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Project MARCH',
+    maintainer_email='software@projectmarch.nl',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
