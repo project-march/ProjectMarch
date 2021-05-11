724f99a9f2f17c2ed9a8e40c46e2b656eb530717
Katja Schmahl <katjaschmahl@hotmail.com>
2021-05-11 10:33:35 +0200

    Change bug with old parameter name

--- ros1/src/data/march_realsense_reader/src/pointcloud_processor/parameter_determiner.cpp
+++ ros1/src/data/march_realsense_reader/src/pointcloud_processor/parameter_determiner.cpp
@@ -101,8 +101,8 @@ bool HullParameterDeterminer::determineParameters(
     // Initialize the optimal foot location at the origin and the gait
     // parmaeters at -1 in case the calculation fails
     optimal_foot_location = pcl::PointNormal();
-    gait_parameters_->step_size_parameter = -1;
-    gait_parameters_->step_height_parameter = -1;
+    gait_parameters_->first_parameter = -1;
+    gait_parameters_->second_parameter = -1;
     gait_parameters_->side_step_parameter = -1;
 
     bool success = true;
