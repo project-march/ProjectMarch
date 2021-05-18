b95c0b7821ab358aa2eecce0092a54642ba81aff
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-05-18 09:58:23 +0200

    Potentially fix strange clang tindy error

--- ros1/src/data/march_realsense_reader/src/pointcloud_processor/parameter_determiner.cpp
+++ ros1/src/data/march_realsense_reader/src/pointcloud_processor/parameter_determiner.cpp
@@ -373,6 +373,7 @@ bool HullParameterDeterminer::getDistanceToObject(
             ROS_ERROR_STREAM("getDistanceToObject method is not implemented "
                              "for selected obstacle "
                 << realsense_category_.value());
+            double = std::numeric_limits<double>::max();
             return false;
         }
     }
