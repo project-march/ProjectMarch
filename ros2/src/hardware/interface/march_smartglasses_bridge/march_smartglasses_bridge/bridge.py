03829794ae7ed4ef5217e8a497a979fbc7e1cc1b
Thijs Raymakers <thijs@raymakers.nl>
2021-05-20 15:45:51 +0200

    Make smartglass hostname and port adjustable via parameters

--- ros2/src/hardware/interface/march_smartglasses_bridge/march_smartglasses_bridge/bridge.py
+++ ros2/src/hardware/interface/march_smartglasses_bridge/march_smartglasses_bridge/bridge.py
@@ -3,9 +3,6 @@ import socket
 from rclpy.node import Node
 from march_shared_msgs.msg import CurrentIPDState
 
-HOST = "localhost"
-PORT = 53003
-
 class SmartglassBridge(Node):
     def __init__(self):
         super().__init__("march_smartglass_bridge")
@@ -18,18 +15,21 @@ class SmartglassBridge(Node):
             qos_profile=1,
         )
 
+        self.host = self.declare_parameter("hud_host").get_parameter_value().string_value
+        self.port = self.declare_parameter("hud_port").get_parameter_value().integer_value
+
     def current_state_callback(self, msg: CurrentIPDState) -> None:
-        self.get_logger().info(f"Received IPD state message: {msg.menu_name}")
+        self.get_logger().debug(f"Received IPD state message: {msg.menu_name}")
         self.send_to_smartglasses(msg.menu_name)
 
     def send_to_smartglasses(self, state: str):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
-                host = socket.gethostbyname(HOST)
-                s.connect((host, PORT))
-                s.sendall(bytes(f"Received: {state}\n", "utf-8"))
+                host = socket.gethostbyname(self.host)
+                s.connect((host, self.port))
+                s.sendall(bytes(f"{state}\n", "utf-8"))
             except ConnectionRefusedError:
-                self.get_logger().warn(f"Smart glasses can not be found on {HOST}:{PORT}")
+                self.get_logger().warn(f"Smart glasses can not be found on {self.host}:{self.port}")
 
 def main(args=None):
     rclpy.init(args=args)
