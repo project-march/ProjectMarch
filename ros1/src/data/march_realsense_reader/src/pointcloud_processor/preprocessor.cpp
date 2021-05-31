0bee1ae27e9d9dd9a41a37741e896592087e5876
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-05-31 14:38:00 +0200

    Format cpp

--- ros1/src/data/march_realsense_reader/src/pointcloud_processor/preprocessor.cpp
+++ ros1/src/data/march_realsense_reader/src/pointcloud_processor/preprocessor.cpp
@@ -145,7 +145,8 @@ void NormalsPreprocessor::readParameters(
     YAML::Node robot_properties
         = YAML::LoadFile(ros::package::getPath("march_description")
             + "/urdf/properties/march6.yaml");
-    foot_height = robot_properties["dimensions"]["general"]["width"].as<double>();
+    foot_height
+        = robot_properties["dimensions"]["general"]["width"].as<double>();
     debugging_ = config.debug;
 }
 
