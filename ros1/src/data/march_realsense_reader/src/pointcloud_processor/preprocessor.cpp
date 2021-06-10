b1e469254d04e666cc9f2399b92ed551048338cf
ttveen <ttveen@gmail.com>
2021-06-10 14:22:31 +0200

    Set rotation to z

--- ros1/src/data/march_realsense_reader/src/pointcloud_processor/preprocessor.cpp
+++ ros1/src/data/march_realsense_reader/src/pointcloud_processor/preprocessor.cpp
@@ -173,7 +173,9 @@ bool NormalsPreprocessor::downsample()
 // necessary to know the height and distance to the wanted step from the foot.
 bool NormalsPreprocessor::transformPointCloudFromUrdf(
     geometry_msgs::TransformStamped& transform_stamped)
+
 {
+    //geometry_msgs::TransformStamped transform_stamped_rotation;
     try {
         pointcloud_frame_id = pointcloud_->header.frame_id.c_str();
         if (tfBuffer->canTransform(frame_id_to_transform_to_,
@@ -181,6 +183,10 @@ bool NormalsPreprocessor::transformPointCloudFromUrdf(
             transform_stamped
                 = tfBuffer->lookupTransform(frame_id_to_transform_to_,
                     pointcloud_frame_id, ros::Time(/*t=*/0));
+            //transform_stamped_rotation
+              //    = tfBuffer->lookupTransform("world",
+//                                              pointcloud_frame_id, ros::Time(/*t=*/0));
+//            transform_stamped.transform.rotation.z = transform_stamped_rotation.transform.rotation.z;
         }
         pcl_ros::transformPointCloud(
             *pointcloud_, *pointcloud_, transform_stamped.transform);
