c82d291ec5d3e9b8828d52020f5b224a1f845491
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-06-17 16:04:41 +0200

    Fix method names

--- ros1/src/data/march_realsense_reader/src/pointcloud_processor/parameter_determiner.cpp
+++ ros1/src/data/march_realsense_reader/src/pointcloud_processor/parameter_determiner.cpp
@@ -92,9 +92,8 @@ void HullParameterDeterminer::readParameters(
     max_x_search_sit = (float)config.parameter_determiner_max_x_search_sit;
     search_y_deviation_sit
         = (float)config.parameter_determiner_search_y_deviation_sit;
-    sit_grid_size = (float)config.parameter_determiner_sit_grid_size
-
-                        max_allowed_z_deviation_foot
+    sit_grid_size = (float)config.parameter_determiner_sit_grid_size;
+    max_allowed_z_deviation_foot
         = (float)config.parameter_determiner_max_allowed_z_deviation_foot;
     max_distance_to_line
         = (float)config.parameter_determiner_ramp_max_distance_to_line;
@@ -153,7 +152,7 @@ bool HullParameterDeterminer::determineParameters(
     }
 
     if (success) {
-        succes &= getGaitParametersFromLocation();
+        success &= getGaitParametersFromLocation();
     }
 
     if (debugging_) {
@@ -358,7 +357,7 @@ bool HullParameterDeterminer::getGaitParametersFromLocation()
             break;
         }
         case RealSenseCategory::sit: {
-            success &= getGaitParametersFromFootLocationSit();
+            success &= getGaitParametersFromSitHeight();
             break;
         }
         default: {
@@ -372,7 +371,8 @@ bool HullParameterDeterminer::getGaitParametersFromLocation()
     return success;
 }
 
-bool HullParameterDeterminer::getGaitParametersFromFootLocationSit()
+// Find the sit parameter from the sit height
+bool HullParameterDeterminer::getGaitParametersFromSitHeight()
 {
     gait_parameters_->first_parameter
         = (sit_height - min_sit_height) / (max_sit_height - min_sit_height);
