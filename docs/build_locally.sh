7078154d74e88772ce633e4d56e99069678e8773
Thijs Raymakers <thijs@raymakers.nl>
2021-06-25 13:32:04 +0200

    Remove ROS specific files that are not actually needed

--- docs/build_locally.sh
+++ docs/build_locally.sh
@@ -11,7 +11,7 @@ rm -rf native_build
 sphinx-build -W -b html . native_build
 
 # Build
-rosdoc_lite -o build .
+#rosdoc_lite -o build .
 
 # Validate if the HTML is valid (i.e. no missing links, images, etc.)
 htmlproofer ./build --only-4xx --http-status-ignore 429 --check-html --file-ignore ./build/html/genindex.html,./build/html/search.html,./build/html/index-msg.html --alt-ignore '/.*/' --url-ignore '#'
