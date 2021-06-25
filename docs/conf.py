6db5d0414c5623ab0e0f0af31cbb199eb22b1c65
Thijs Raymakers <thijs@raymakers.nl>
2021-06-25 17:45:27 +0200

    Address flake8 violations in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,64 +1,67 @@
 import os
 
 # Retrieve branch name
-if os.getenv('CI'):
-    branch_name = os.environ['TAG']
+if os.getenv("CI"):
+    branch_name = os.environ["TAG"]
 else:
-    import subprocess
-    command = subprocess.run(['git', 'branch', '--show-current'], capture_output=True)
-    branch_name = str(command.stdout, 'utf-8').strip()
+    import subprocess  # noqa: S404
 
-extensions = ['sphinx.ext.extlinks',
-              'sphinx.ext.todo',
-              'sphinx_rtd_theme',
-              ]
+    g = ["git", "branch", "--show-current"]
+    command = subprocess.run(g, capture_output=True)  # noqa: S603, S607
+    branch_name = str(command.stdout, "utf-8").strip()
+
+extensions = [
+    "sphinx.ext.extlinks",
+    "sphinx.ext.todo",
+    "sphinx_rtd_theme",
+]
 
 todo_include_todos = True
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
-project = 'Project MARCH'
-copyright = '2021, Project MARCH'
+project = "Project MARCH"
+copyright = "2021, Project MARCH"  # noqa: A001
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = 'Noetic'
+version = "Noetic"
 # The full version, including alpha/beta/rc tags.
-release = 'Noetic'
+release = "Noetic"
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
+pygments_style = "default"
 
 # Name of the style used to generate the html documentation
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-    'style_external_links': True,
+    "logo_only": True,
+    "display_version": False,
+    "style_external_links": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+html_static_path = ["static"]
 
-html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = "%b %d, %Y"
 html_show_copyright = True
 
 html_context = {
-    'display_gitlab': True,
-    'gitlab_user': 'project-march',
-    'gitlab_repo': 'march',
-    'gitlab_version': branch_name,
-    'gitlab_url': 'https://gitlab.com/',
+    "display_gitlab": True,
+    "gitlab_user": "project-march",
+    "gitlab_repo": "march",
+    "gitlab_version": branch_name,
+    "gitlab_url": "https://gitlab.com/",
     "conf_py_path": "/docs",
-    "css_files": ['_static/css/override.css'],
+    "css_files": ["_static/css/override.css"],
     "favicon": "favicon.ico",
-    "logo": "logo.png"
+    "logo": "logo.png",
 }
 
 # Global substitutions
@@ -66,21 +69,29 @@ rst_prolog = """
 .. |march| replace:: MARCH exoskeleton
 """
 
-gitlab_root = html_context['gitlab_url'] + html_context['gitlab_user'] + '/'
-ros1_src = 'march/-/blob/main/ros1/src/'
+gitlab_root = html_context["gitlab_url"] + html_context["gitlab_user"] + "/"
+ros1_src = "march/-/blob/main/ros1/src/"
 
 # Links
-extlinks = {'codedir': (gitlab_root + 'march/tree/' + html_context['gitlab_version'] + '/doc/%s', ''),
-            'rootdir': (gitlab_root + 'march/tree/' + html_context['gitlab_version'] + '/%s', ''),
-            'hardware-interface': (gitlab_root + ros1_src + 'march_hardware_interface/%s', ''),
-            'input-device': (gitlab_root + 'input-device/-/tree/main/%s', ''),
-            'march': (gitlab_root + ros1_src + '%s', ''),
-            'monitor': (gitlab_root + ros1_src + 'march_monitor/%s', ''),
-            'simulation': (gitlab_root + ros1_src + 'march_simulation/%s', ''),
-            'gait-files': (gitlab_root + ros1_src + 'march_gait_files/%s', ''),
-            'ethercat-slaves': (gitlab_root + 'ethercat-slaves/-/tree/main/%s', ''),
-            'gait-generation': (gitlab_root + ros1_src + 'march_rqt_gait_generator/%s', ''),
-            'march_website': ('httpps://projectmarch.nl', '')}
+extlinks = {
+    "codedir": (
+        gitlab_root + "march/tree/" + html_context["gitlab_version"] + "/doc/%s",
+        "",
+    ),
+    "rootdir": (
+        gitlab_root + "march/tree/" + html_context["gitlab_version"] + "/%s",
+        "",
+    ),
+    "hardware-interface": (gitlab_root + ros1_src + "march_hardware_interface/%s", ""),
+    "input-device": (gitlab_root + "input-device/-/tree/main/%s", ""),
+    "march": (gitlab_root + ros1_src + "%s", ""),
+    "monitor": (gitlab_root + ros1_src + "march_monitor/%s", ""),
+    "simulation": (gitlab_root + ros1_src + "march_simulation/%s", ""),
+    "gait-files": (gitlab_root + ros1_src + "march_gait_files/%s", ""),
+    "ethercat-slaves": (gitlab_root + "ethercat-slaves/-/tree/main/%s", ""),
+    "gait-generation": (gitlab_root + ros1_src + "march_rqt_gait_generator/%s", ""),
+    "march_website": ("httpps://projectmarch.nl", ""),
+}
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'MarchDocumentation'
+htmlhelp_basename = "MarchDocumentation"
