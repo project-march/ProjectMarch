185f3882c6657717576fda9eca8caa04c93f4fca
Thijs Raymakers <thijs@raymakers.nl>
2021-06-25 14:36:17 +0200

    Remove rosdoc_lite dependency from build_locally.sh

--- docs/build_locally.sh
+++ docs/build_locally.sh
@@ -5,13 +5,9 @@ set -e
 
 # Setup Environment
 rm -rf build
-rm -rf native_build
 
 # Build without ros wrapper to catch warnings and errors
-sphinx-build -W -b html . native_build
-
-# Build
-#rosdoc_lite -o build .
+sphinx-build -W -b html . build
 
 # Validate if the HTML is valid (i.e. no missing links, images, etc.)
 htmlproofer ./build --only-4xx --http-status-ignore 429 --check-html --file-ignore ./build/html/genindex.html,./build/html/search.html,./build/html/index-msg.html --alt-ignore '/.*/' --url-ignore '#'
