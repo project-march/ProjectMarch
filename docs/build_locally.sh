5a8be5395a7581c2ca51ab43dfde0ae72b10c82c
Thijs Raymakers <thijs@raymakers.nl>
2021-06-30 16:04:13 +0200

    Correctly generate reStructuredText from README.md files when multiple READMEs are involved

--- docs/build_locally.sh
+++ docs/build_locally.sh
@@ -10,38 +10,44 @@ function readmes_to_sphinx ()
 {
     echo "Converting READMEs to reStructuredText..."
     # Create the given directory if it does not yet exist
+    rm -rf $2
     mkdir -p $2
 
     # List all README.md files
-    READMES=$(mktemp)
-    find $1 -name "README.md" | grep -v "libraries" | grep '^' > $READMES
+    INPUT=$(mktemp)
+    find $1 -name "README.md" | grep '^' > $INPUT
 
     # Extract the package name by removing the "README.md" part and by removing
     # everything before "/march"
     # Add the second parameter as before the package name
     # Append `.rst` to the end of the name
+    # Prepend "-o" as the pandoc output parameter to the name
 
     # For example, if called with "foxy" as a parameter then
     # ../ros2/src/hardware/interface/march_smartglasses_bridge/README.md
     # becomes
     # foxy/march_smartglasses_bridge.rst
-    PACKAGES=$(mktemp)
-    cat $READMES | sed "s#/README.md##" | sed "s#.*/march#$2/march#" | sed "s/$/\.rst/" > $PACKAGES
+    OUTPUT=$(mktemp)
+    cat $INPUT | sed "s#/README.md##" \
+                 | sed "s#.*/##" \
+                 | sed "s#^#$2#" \
+                 | sed "s/$/\.rst/" \
+                 | sed "s/^/-o /" > $OUTPUT
 
     # Transform each markdown file to a reStructuredText file with pandoc
     # This command will become something like
     # pandoc ../ros2/src/hardware/interface/march_smartglasses_bridge/README.md
     #        -o march_smartglasses_bridge.rst
     # for every found README.md file
-    echo "-o" | paste -d ' ' $READMES - | paste -d ' ' - $PACKAGES | xargs pandoc
+    paste -d ' ' $INPUT $OUTPUT | xargs -I{} -- sh -c "pandoc {}"
 
     # Remove the temporary files
-    rm $READMES
-    rm $PACKAGES
+    rm $INPUT
+    rm $OUTPUT
     echo "Succesfully converted READMEs to reStructuredText!"
 }
 
-readmes_to_sphinx "../ros1/src ../ros2/src" "doc/march_packages/readme"
+readmes_to_sphinx "../ros1/src ../ros2/src" "doc/march_packages/readme/"
 
 # Build the sphinx documentation while catching warnings and errors
 sphinx-build -W -b html . build
