a453f86dae4ce80c2b7df8e29d0735fd7d2f9a28
Katja Schmahl <katjaschmahl@hotmail.com>
2021-07-23 11:00:38 +0200

    Update march robot and create PowerDistributionBoard class in march_hardware

---
+++ ros1/src/hardware/interface/march_hardware/src/power_distribution_board/power_distribution_board.cpp
@@ -0,0 +1,23 @@
+#include "march_hardware/error/hardware_exception.h"
+#include <march_hardware/power_distribution_board/power_distribution_board.h>
+
+#include <ros/ros.h>
+
+namespace march {
+PowerDistributionBoard::PowerDistributionBoard(
+    const Slave& slave, uint8_t byte_offset)
+    : Slave(slave)
+    , byte_offset_(byte_offset) {};
+
+PowerDistributionBoardData PowerDistributionBoard::read()
+{
+    std::array<float, POWER_DISTRIBUTION_BOARD_DATA_LENGTH> data {};
+    for (unsigned int i = 0; i < data.size(); i++) {
+        data[i] = this->read32(byte_offset_ + i * sizeof(float)).f;
+    }
+    static_assert(POWER_DISTRIBUTION_BOARD_DATA_LENGTH == 14);
+    return { data[0], data[1], data[2], data[3], data[4], data[5], data[6],
+        data[7], data[8], data[9], data[10], data[11], data[12], data[13] };
+}
+
+} // namespace march
