83447855adfbcbf1aa1802a00f828cb666279a06
Katja Schmahl <katjaschmahl@hotmail.com>
2021-07-23 14:26:36 +0200

    Fix pipeline

--- ros2/src/gaits/march_gait_selection/march_gait_selection/gaits/realsense_gait.py
+++ ros2/src/gaits/march_gait_selection/march_gait_selection/gaits/realsense_gait.py
@@ -304,7 +304,6 @@ class RealsenseGait(SetpointsGait):
         self._start_time = current_time + self.INITIAL_START_DELAY_TIME
         self._current_time = current_time
 
-
         # If a gait is dependent on some other gait its subgaits are already
         # interpolated from parameters so we can skip the realsense call
         if not self._dependent_on:
