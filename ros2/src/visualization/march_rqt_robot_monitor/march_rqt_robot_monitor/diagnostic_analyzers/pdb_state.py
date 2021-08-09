7522ca62a3f70c7a0e19018c4a31f8b0400ff7b3
Katja Schmahl <katjaschmahl@hotmail.com>
2021-08-09 12:58:35 +0200

    Reduce duplication'

--- ros2/src/visualization/march_rqt_robot_monitor/march_rqt_robot_monitor/diagnostic_analyzers/pdb_state.py
+++ ros2/src/visualization/march_rqt_robot_monitor/march_rqt_robot_monitor/diagnostic_analyzers/pdb_state.py
@@ -33,12 +33,25 @@ class CheckPDBStatus:
         )
         self._pdb_data = None
 
-        updater.add("PowerDistributionBoard stop state", self._stop_diagnostic())
-        updater.add("PowerDistributionBoard lv", self._lv_diagnostic())
-        updater.add("PowerDistributionBoard hv", self._hv_diagnostic())
-        updater.add("Battery percentage", self._battery_percentage_diagnostic())
-        updater.add("Battery voltage", self._battery_voltage_diagnostic())
-        updater.add("Battery temperature", self._battery_temperature_diagnostic())
+        updater.add(pdb
+            "PowerDistributionBoard stop state",
+            self._pdb_diagnostic(self._stop_diagnostic),
+        )
+        updater.add(
+            "PowerDistributionBoard lv", self._pdb_diagnostic(self._lv_diagnostic)
+        )
+        updater.add(
+            "PowerDistributionBoard hv", self._pdb_diagnostic(self._hv_diagnostic)
+        )
+        updater.add(
+            "Battery percentage", self._pdb_diagnostic(self._battery_percentage_check)
+        )
+        updater.add(
+            "Battery voltage", self._pdb_diagnostic(self._battery_voltage_check)
+        )
+        updater.add(
+            "Battery temperature", self._pdb_diagnostic(self._battery_temperature_check)
+        )
 
     def _cb(self, msg: PowerDistributionBoardData):
         """Set the motor_controller_states.
@@ -47,149 +60,136 @@ class CheckPDBStatus:
         """
         self._pdb_data = msg
 
-    def _battery_temperature_diagnostic(self) -> Callable:  # noqa: D202
-        def d(stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
-            if self._pdb_data is None:
-                stat.summary(DiagnosticStatus.STALE, "No battery data")
-                return stat
-
-            battery_temperature = self._pdb_data.battery_state.temperature
-            if battery_temperature >= self.BATTERY_TEMPERATURE_ERROR_THRESHOLD:
-                stat.summary(
-                    DiagnosticStatus.ERROR,
-                    f"Battery temperature too high: {battery_temperature}",
-                )
-            elif battery_temperature >= self.BATTERY_TEMPERATURE_WARNING_THRESHOLD:
-                stat.summary(
-                    DiagnosticStatus.WARN,
-                    f"Battery temperature high: {battery_temperature}",
-                )
-            else:
-                stat.summary(DiagnosticStatus.OK, f"OK: {battery_temperature}")
+    @staticmethod
+    def _threshold_check(
+        stat: DiagnosticStatusWrapper,
+        value: float,
+        warning_threshold: float,
+        error_threshold: float,
+        is_upper_limit: bool,
+        value_name: str,
+    ):
+        if value == -1:
+            stat.summary(DiagnosticStatus.STALE, "No PDB data")
             return stat
 
-        return d
-
-    def _battery_percentage_diagnostic(self) -> Callable:  # noqa: D202
-        def d(stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
-            if self._pdb_data is None:
-                stat.summary(DiagnosticStatus.STALE, "No battery data")
-                return stat
-
-            battery_percentage = self._pdb_data.battery_state.percentage
-            if battery_percentage <= self.BATTERY_PERCENTAGE_ERROR_THRESHOLD:
-                stat.summary(
-                    DiagnosticStatus.ERROR,
-                    f"Battery percentage too low: {battery_percentage}",
-                )
-            elif battery_percentage <= self.BATTERY_PERCENTAGE_WARNING_THRESHOLD:
-                stat.summary(
-                    DiagnosticStatus.WARN,
-                    f"Battery percentage low: {battery_percentage}",
-                )
-            else:
-                stat.summary(DiagnosticStatus.OK, f"OK: {battery_percentage}")
-            return stat
-
-        return d
-
-    def _battery_voltage_diagnostic(self) -> Callable:  # noqa: D202:
-        def d(stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
-            if self._pdb_data is None:
-                stat.summary(DiagnosticStatus.STALE, "No battery data")
-                return stat
-
-            battery_voltage = self._pdb_data.battery_state.voltage
-            if battery_voltage <= self.BATTERY_VOLTAGE_ERROR_THRESHOLD:
-                stat.summary(
-                    DiagnosticStatus.ERROR,
-                    f"Battery voltage too low: {battery_voltage}",
-                )
-            elif battery_voltage <= self.BATTERY_PERCENTAGE_WARNING_THRESHOLD:
-                stat.summary(
-                    DiagnosticStatus.WARN, f"Battery voltage low: {battery_voltage}"
-                )
-            else:
-                stat.summary(DiagnosticStatus.OK, f"OK: {battery_voltage}")
-
-            return stat
-
-        return d
-
-    def _lv_diagnostic(self) -> Callable:  # noqa: D202
-        def d(stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
-            if self._pdb_data is None:
-                stat.summary(DiagnosticStatus.STALE, "No pdb data")
-                return stat
-
+        if (is_upper_limit and value >= warning_threshold) or (
+            not is_upper_limit and value <= warning_threshold
+        ):
             stat.summary(
-                DiagnosticStatus.OK,
-                f"LV OK\n"
-                f"Current lv1: "
-                f"{self._pdb_data.lv_state.lv1_current}\n"
-                f"Current lv2: "
-                f"{self._pdb_data.lv_state.lv2_current}",
+                DiagnosticStatus.WARN,
+                f"{value_name} {'high' if is_upper_limit else 'low'}: {value}",
             )
-            return stat
-
-        return d
-
-    def _hv_diagnostic(self) -> Callable:  # noqa: D202
-        def d(stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
-            if self._pdb_data is None:
-                stat.summary(DiagnosticStatus.STALE, "No pdb data")
-                return stat
-
-            hv_state = self._pdb_data.hv_state
-
+        elif (is_upper_limit and value >= error_threshold) or (
+            not is_upper_limit and value <= error_threshold
+        ):
             stat.summary(
-                DiagnosticStatus.OK,
-                f"HV OK\n"
-                f"Total current: "
-                f"{hv_state.total_current}\n"
-                f"Current hv1: "
-                f"{hv_state.hv1_current}\n"
-                f"Current hv2: "
-                f"{hv_state.hv2_current}\n"
-                f"Current hv3: "
-                f"{hv_state.hv3_current}\n"
-                f"Current hv4: "
-                f"{hv_state.hv4_current}",
+                DiagnosticStatus.WARN,
+                f"{value_name} too {'high' if is_upper_limit else 'low'}: {value}",
             )
-            return stat
+        else:
+            stat.summary(DiagnosticStatus.OK, f"OK: {value}")
 
-        return d
+        return stat
 
-    def _stop_diagnostic(self) -> Callable:  # noqa: D202
-        """Create a diagnostic function for an MotorController.
-
-        :type index: int
-        :param index: index of the joint
-
-        :return Curried diagnostic function that updates the diagnostic status
-                according to the given index.
-        """
-
+    def _pdb_diagnostic(self, check_function) -> Callable:  # noqa: D202
         def d(stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
             if self._pdb_data is None:
-                stat.summary(DiagnosticStatus.STALE, "No pdb data")
+                stat.summary(DiagnosticStatus.STALE, "No PDB data")
                 return stat
 
-            emergency_button_status = self._pdb_data.emergency_button_state
-            stop_button_state = self._pdb_data.stop_button_state
-
-            stat.add("Emergency button status", str(emergency_button_status))
-            stat.add("Stop button status", str(stop_button_state))
-
-            if emergency_button_status != 1:
-                stat.summary(DiagnosticStatus.ERROR, "Emergency button pressed")
-            elif stop_button_state != 1:
-                stat.summary(DiagnosticStatus.ERROR, "Stop button pressed")
-            else:
-                stat.summary(
-                    DiagnosticStatus.OK, "OK, no stop or emergency button " "pressed"
-                )
+            check_function(stat)
 
             return stat
 
         return d
+
+    def _battery_temperature_check(
+        self, stat: DiagnosticStatusWrapper
+    ) -> DiagnosticStatusWrapper:
+        battery_temperature = self._pdb_data.battery_state.temperature
+
+        return CheckPDBStatus._threshold_check(
+            stat=stat,
+            value=battery_temperature,
+            warning_threshold=self.BATTERY_TEMPERATURE_WARNING_THRESHOLD,
+            error_threshold=self.BATTERY_VOLTAGE_ERROR_THRESHOLD,
+            is_upper_limit=True,
+            value_name="Battery temperature",
+        )
+
+    def _battery_percentage_check(
+        self, stat: DiagnosticStatusWrapper
+    ) -> DiagnosticStatusWrapper:
+        battery_percentage = self._pdb_data.battery_state.percentage
+
+        return CheckPDBStatus._threshold_check(
+            stat=stat,
+            value=battery_percentage,
+            warning_threshold=self.BATTERY_PERCENTAGE_WARNING_THRESHOLD,
+            error_threshold=self.BATTERY_PERCENTAGE_ERROR_THRESHOLD,
+            is_upper_limit=False,
+            value_name="Battery percentage",
+        )
+
+    def _battery_voltage_check(
+        self, stat: DiagnosticStatusWrapper
+    ) -> DiagnosticStatusWrapper:
+        battery_voltage = self._pdb_data.battery_state.voltage
+
+        return CheckPDBStatus._threshold_check(
+            stat=stat,
+            value=battery_voltage,
+            warning_threshold=self.BATTERY_VOLTAGE_WARNING_THRESHOLD,
+            error_threshold=self.BATTERY_VOLTAGE_ERROR_THRESHOLD,
+            is_upper_limit=False,
+            value_name="Battery voltage",
+        )
+
+    def _lv_diagnostic(self, stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
+        stat.summary(
+            DiagnosticStatus.OK,
+            f"LV OK\n"
+            f"Current lv1: "
+            f"{self._pdb_data.lv_state.lv1_current}\n"
+            f"Current lv2: "
+            f"{self._pdb_data.lv_state.lv2_current}",
+        )
+        return stat
+
+    def _hv_diagnostic(self, stat: DiagnosticStatusWrapper) -> DiagnosticStatusWrapper:
+        hv_state = self._pdb_data.hv_state
+
+        stat.summary(
+            DiagnosticStatus.OK,
+            f"HV OK\n"
+            f"Total current: "
+            f"{hv_state.total_current}\n"
+            f"Current hv1: "
+            f"{hv_state.hv1_current}\n"
+            f"Current hv2: "
+            f"{hv_state.hv2_current}\n"
+            f"Current hv3: "
+            f"{hv_state.hv3_current}\n"
+            f"Current hv4: "
+            f"{hv_state.hv4_current}",
+        )
+        return stat
+
+    def _stop_diagnostic(
+        self, stat: DiagnosticStatusWrapper
+    ) -> DiagnosticStatusWrapper:
+        emergency_button_status = self._pdb_data.emergency_button_state
+        stop_button_state = self._pdb_data.stop_button_state
+
+        stat.add("Emergency button status", str(emergency_button_status))
+        stat.add("Stop button status", str(stop_button_state))
+
+        if emergency_button_status != 1:
+            stat.summary(DiagnosticStatus.ERROR, "Emergency button pressed")
+        elif stop_button_state != 1:
+            stat.summary(DiagnosticStatus.ERROR, "Stop button pressed")
+        else:
+            stat.summary(DiagnosticStatus.OK, "OK, no stop or emergency button pressed")
+
+        return stat
