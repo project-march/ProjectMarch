aeca89f056a2b0de7d9223ef8a4e4188d4f707f4
wnederpel <wolf.nederpel@gmail.com>
2021-08-15 18:51:54 +0200

    Let black do its thing

--- ros2/src/gaits/utility_scripts/speed_up_gait.py
+++ ros2/src/gaits/utility_scripts/speed_up_gait.py
@@ -35,10 +35,12 @@ for subgait_name, version in versions_to_speed_up.items():
                         setpoint["velocity"] * 1.1, 4
                     )
         if new_version_extension != "":
-            new_version_name = version[:version.rfind("_") + 1] + new_version_extension
+            new_version_name = version[: version.rfind("_") + 1] + new_version_extension
         if new_description != "":
             content["description"] = new_description
-        write_path = os.path.join(gait_to_speed_up, subgait_name, new_version_name + subgait_suffix)
+        write_path = os.path.join(
+            gait_to_speed_up, subgait_name, new_version_name + subgait_suffix
+        )
         with open(write_path, "w") as subgait_file:
             yaml.dump(content, subgait_file)
     except Exception as e:  # noqa: B902 PIE786
