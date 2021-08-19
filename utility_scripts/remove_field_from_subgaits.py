10788a1a10a4ba3c1a38837e59fb1cf2817304b3
Wolf Nederpel <wolf.nederpel@gmail.com>
2021-08-19 12:11:16 +0200

    Let black make changes

--- utility_scripts/remove_field_from_subgaits.py
+++ utility_scripts/remove_field_from_subgaits.py
@@ -6,7 +6,7 @@ field_to_remove = "version"
 
 paths_that_failed = []
 for path in Path(
-        "../ros2/src/gaits/march_gait_files/airgait_vi/",
+    "../ros2/src/gaits/march_gait_files/airgait_vi/",
 ).rglob("*.subgait"):
     try:
         with open(path, "r") as subgait_file:
