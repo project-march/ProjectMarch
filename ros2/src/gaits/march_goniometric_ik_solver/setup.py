a58d166aea058c9c588ea055b5b83c1e2ac86aa0
Jelmer <jsdewolde@gmail.com>
2021-11-29 16:42:36 +0100

    Solve black issues for flake8 check in setup py

--- ros2/src/gaits/march_goniometric_ik_solver/setup.py
+++ ros2/src/gaits/march_goniometric_ik_solver/setup.py
@@ -1,25 +1,23 @@
 from setuptools import setup
 
-package_name = 'march_goniometric_ik_solver'
+package_name = "march_goniometric_ik_solver"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='jelmer',
-    maintainer_email='jsdewolde@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="Project March",
+    maintainer_email="software@projectmarch.nl",
+    description="Performs geometric inverse kinematic solving to get pose for desired foot location",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
