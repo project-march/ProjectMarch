33a46053299af851a03b2b8d213f303f153e3d3b
George <georgevegelien@gmail.com>
2021-11-30 17:03:53 +0100

    Change 'if' to 'while' for waiting until finished

--- .docker_local/start_scripts/build/ros2.bash
+++ .docker_local/start_scripts/build/ros2.bash
@@ -6,10 +6,10 @@ cd "${HOME}"/march/ros2 || exit
 rm "${DONE_FILE}"
 source /opt/ros/foxy/setup.bash
 
-if ! [ -f "${HOME}"/march/ros1/"${DONE_FILE}" ]; then
+while ! [ -f "${HOME}"/march/ros1/"${DONE_FILE}" ]; do
   echo "Ros1 is not done yet, sleeping for ${SLEEP_TIME}. (do 'touch ~/march/ros1/${DONE_FILE}' if it should be done)";
   sleep $SLEEP_TIME;
-fi
+done;
 
 colcon build
 touch "${DONE_FILE}"
