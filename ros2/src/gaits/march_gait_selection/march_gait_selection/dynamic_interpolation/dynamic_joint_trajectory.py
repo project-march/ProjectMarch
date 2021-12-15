b06ced36a80651779fa5834b5e33e1182fbe0da8
Marten Haitjema <martenh99@gmail.com>
2021-12-15 11:09:05 +0100

    Rename dynamic_joint_trajectory_setpoint class to dynamic_joint_trajectory

--- ros2/src/gaits/march_gait_selection/march_gait_selection/dynamic_interpolation/dynamic_joint_trajectory.py
+++ ros2/src/gaits/march_gait_selection/march_gait_selection/dynamic_interpolation/dynamic_joint_trajectory.py
@@ -3,7 +3,12 @@ from march_utility.gait.setpoint import Setpoint
 from march_utility.utilities.duration import Duration
 
 
-class DynamicJointTrajectorySetpoint:
+class DynamicJointTrajectory:
+    """Class that performs interpolation between given setpoints.
+
+    :param setpoints: A list containing setpoints for a given joint.
+    :type setpoints: list
+    """
 
     setpoint_class = Setpoint
 
@@ -11,6 +16,7 @@ class DynamicJointTrajectorySetpoint:
         self.setpoints = setpoints
 
     def get_setpoints_unzipped(self):
+        """Returns a list of time, position and velocity."""
         time = []
         position = []
         velocity = []
@@ -23,6 +29,7 @@ class DynamicJointTrajectorySetpoint:
         return time, position, velocity
 
     def interpolate_setpoints(self):
+        """Uses a CubicSpline with velocity boundary conditions to return an interpolated position and velocity"""
         duration, position, velocity = self.get_setpoints_unzipped()
         boundary_condition = ((1, velocity[0]), (1, velocity[-1]))
         yi = []
@@ -36,6 +43,14 @@ class DynamicJointTrajectorySetpoint:
         self.interpolated_velocity = self.interpolated_position.derivative()
 
     def get_interpolated_setpoint(self, time):
+        """Computes a Setpoint instance with the given time and the position and velocity at this time.
+
+        :param time: Time at which the setpoint will be set.
+        :type time: float
+
+        :returns: A setpoint with the given time and the position and velocity at this time.
+        :rtype: Setpoint class instance
+        """
         return self.setpoint_class(
             Duration(time),
             float(self.interpolated_position(time)),
