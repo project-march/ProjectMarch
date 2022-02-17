406272707faf29849cb798943fe1db3abaf1d72c
Tuhin <2hin.das@gmail.com>
2022-02-17 11:23:59 +0100

    Make IP address dynamic

--- ros2/src/wireless/march_wireless_ipd/march_wireless_ipd/march_wireless_ipd_node.py
+++ ros2/src/wireless/march_wireless_ipd/march_wireless_ipd/march_wireless_ipd_node.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 import threading
+import socket
 
 
 class WirelessIPDNode(Node):
@@ -12,12 +13,18 @@ class WirelessIPDNode(Node):
         super().__init__('wireless_ipd_node')
 
 def main():
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+
     rclpy.init()
 
     node = WirelessIPDNode()
     executor = MultiThreadedExecutor()
     controller = InputDeviceController(node)
-    manager = ConnectionManager('192.168.148.240', 4000, controller, node)
+    manager = ConnectionManager(ip, 4000, controller, node)
     thr = threading.Thread(target=manager.establish_connection)
     thr.start()
 
