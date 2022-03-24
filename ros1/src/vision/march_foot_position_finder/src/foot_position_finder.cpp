413d51efe1dac7f91b9ac6146b8b94de811f9bf3
Tuhin Das <2hin.das@gmail.com>
2022-03-24 10:44:06 +0100

    Add comments to covid code

--- ros1/src/vision/march_foot_position_finder/src/foot_position_finder.cpp
+++ ros1/src/vision/march_foot_position_finder/src/foot_position_finder.cpp
@@ -116,22 +116,34 @@ void FootPositionFinder::readParameters(
             &FootPositionFinder::processSimulatedDepthFrames, this);
     }
 
-    last_chosen_point_current_ = start_point_current_ = last_displacement_
+    // Initialize position of other foot in current frame
+    // This position is equal to the last displacement in current frame
+    start_point_current_ = last_displacement_
         = transformPoint(ORIGIN, other_frame_id_, current_frame_id_);
-    last_chosen_point_world_ = transformPoint(
-        last_chosen_point_current_, current_frame_id_, base_frame_);
 
+    // Initialize position of other foot in base frame
+    last_chosen_point_world_
+        = transformPoint(start_point_current_, current_frame_id_, base_frame_);
+
+    // The last height is used to remember how high the previous step was of the
+    // other foot (relative to the hip base). Here is it initialized to the zero
+    // point in the base frame
     Point height_init = transformPoint(ORIGIN, base_frame_, "hip_base_aligned");
     last_height_ = height_init.z;
 
+    // Current start point in world frame (for visualization)
     start_point_world_
         = transformPoint(start_point_current_, current_frame_id_, base_frame_);
+    // The previous point of the current foot (for visualization)
     previous_start_point_world_
         = transformPoint(ORIGIN, current_frame_id_, base_frame_);
 
+    // Desired point = (current start point) + (usual displacement)
+    // The displacement is the vector (-step_distance_, +-foot_gap_, 0)
     desired_point_ = addPoints(start_point_current_,
         Point(-(float)step_distance_, (float)(switch_factor_ * foot_gap_),
             /*_z=*/0));
+    // Rotation necessary for base_frame computation
     desired_point_ = rotateRight(
         transformPoint(desired_point_, current_frame_id_, base_frame_));
 
@@ -148,6 +160,9 @@ void FootPositionFinder::readParameters(
 void FootPositionFinder::chosenCurrentPointCallback(
     const march_shared_msgs::FootPosition msg) // NOLINT
 {
+    // Allow last height of other foot to be updated at most 10 times
+    // This simulates that the pressure soles have confirmed that the other foot
+    // has touched the ground, so that the height can be reset
     last_height_ = FLT_MAX;
     refresh_last_height_ = 10;
 }
@@ -159,19 +174,26 @@ void FootPositionFinder::chosenCurrentPointCallback(
 void FootPositionFinder::chosenOtherPointCallback(
     const march_shared_msgs::FootPosition msg) // NOLINT
 {
+    // Start point in current frame is equal to the previous displacement
     last_displacement_ = start_point_current_
         = Point(msg.displacement.x, msg.displacement.y, msg.displacement.z);
+    // Store chosen point of other foot in world frame
     last_chosen_point_world_
         = Point(msg.point_world.x, msg.point_world.y, msg.point_world.z);
 
+    // Update points to visualize the previous displacement
+    // previous_start_point_world_ is the previous start point
+    // start_point_world_ is the previous end point!
     start_point_world_
         = transformPoint(start_point_current_, current_frame_id_, base_frame_);
     previous_start_point_world_
         = transformPoint(ORIGIN, current_frame_id_, base_frame_);
 
+    // Compute desired point in base_frame_
     desired_point_ = addPoints(start_point_current_,
         Point(-(float)step_distance_, (float)(switch_factor_ * foot_gap_),
             /*_z=*/0));
+    // Rotation is necessary for visualization and computation in base frame
     desired_point_ = rotateRight(
         transformPoint(desired_point_, current_frame_id_, base_frame_));
 }
@@ -220,7 +242,8 @@ void FootPositionFinder::processSimulatedDepthFrames(
  */
 void FootPositionFinder::processPointCloud(const PointCloud::Ptr& pointcloud)
 {
-    // Reset last height when other leg lands on the ground
+    // Reset last height when other leg lands on the ground; the height is
+    // determined relative to the hip base
     while (refresh_last_height_ > 0) {
         Point reset_zero
             = transformPoint(ORIGIN, other_frame_id_, "hip_base_aligned");
@@ -236,7 +259,7 @@ void FootPositionFinder::processPointCloud(const PointCloud::Ptr& pointcloud)
     // Publish cloud for visualization
     publishCloud(preprocessed_pointcloud_publisher_, *pointcloud);
 
-    // Find possible points
+    // Find possible points around the desired point determined earlier
     PointFinder pointFinder(n_, pointcloud, left_or_right_, desired_point_);
     std::vector<Point> position_queue;
     pointFinder.findPoints(&position_queue);
@@ -249,42 +272,45 @@ void FootPositionFinder::processPointCloud(const PointCloud::Ptr& pointcloud)
         rotateRight(last_chosen_point_world_)); // Purple
 
     if (position_queue.size() > 0) {
+
+        // Take the first point of the point queue returned by the point finder
         Point found_covid_point_world
             = computeTemporalAveragePoint(position_queue[0]); // Red
 
-        // Retrieve 3D points between current and new foot position
+        // Retrieve 3D points between current and new determined foot position
         Point start = rotateRight(
             transformPoint(ORIGIN, current_frame_id_, base_frame_));
         std::vector<Point> track_points
             = pointFinder.retrieveTrackPoints(start, found_covid_point_world);
 
-        // Publish for visualization
+        // Visualization
         publishTrackMarkerPoints(point_marker_publisher_, track_points);
         publishMarkerPoint(point_marker_publisher_, found_covid_point_world);
         publishPossiblePoints(point_marker_publisher_, position_queue);
 
-        // Transform found covid point to current frame and hip frame
+        // Transform point found with cameras to current frame and to hip frame
         found_covid_point_world = rotateLeft(found_covid_point_world);
         Point found_covid_point_current_ = transformPoint(
             found_covid_point_world, base_frame_, current_frame_id_);
         Point found_covid_point_hip_ = transformPoint(
             found_covid_point_world, base_frame_, "hip_base_aligned");
 
-        // Reset the heights of points to the z-values in hip frame (found with
-        // cameras)
+        // Reset the heights of points in current frame with z-values in the
+        // gravity aligned hip frame
         found_covid_point_current_.z = found_covid_point_hip_.z;
         start_point_current_.z = last_height_;
 
-        // Compute new foot displacement
+        // Compute new foot displacement for gait computation
         Point new_displacement
             = subtractPoints(found_covid_point_current_, start_point_current_);
 
-        // Add a threshold for the height of points to be different from 0
+        // Apply a threshold for the height of points to be different from 0
         if (std::abs(new_displacement.z) < height_zero_threshold_) {
             new_displacement.z = 0;
         }
 
-        // Compute height of points to final position
+        // Transform the height points between start and end position to current
+        // frame
         std::vector<Point> relative_track_points;
         for (Point& p : track_points) {
             Point point = rotateLeft(p);
@@ -298,11 +324,7 @@ void FootPositionFinder::processPointCloud(const PointCloud::Ptr& pointcloud)
         publishArrow2(point_marker_publisher_, start_point_world_,
             found_covid_point_world);
 
-        // Reset the z-values to points in hip frame
-        found_covid_point_current_.z = found_covid_point_current_.z;
-        found_covid_point_world.z = found_covid_point_world.z;
-
-        // Publish final point
+        // Publish final point for gait computation
         publishPoint(point_publisher_, found_covid_point_current_,
             found_covid_point_world, new_displacement, relative_track_points);
     }
