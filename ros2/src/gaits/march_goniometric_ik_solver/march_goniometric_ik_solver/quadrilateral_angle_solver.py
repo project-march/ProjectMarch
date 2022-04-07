179b147d0d4da1518c8f8fd1dc2606b450169306
Jelmer <jsdewolde@gmail.com>
2022-04-07 16:16:02 +0200

    Add docstring google style to march goniometric ik solver package

--- ros2/src/gaits/march_goniometric_ik_solver/march_goniometric_ik_solver/quadrilateral_angle_solver.py
+++ ros2/src/gaits/march_goniometric_ik_solver/march_goniometric_ik_solver/quadrilateral_angle_solver.py
@@ -5,10 +5,16 @@ RIGHT_ANGLE = np.pi / 2
 
 
 def get_angle_between_points(points: List[np.array]) -> float:
-    """
-    Calculates the angle between three points,
-    where the angle is calculated for the middle point.
+    """Calculates the angle between three points.
+
+    The angle is calculated for the middle point.
     Based on the dot product cosine rule.
+
+    Args:
+        points (List[np.array]): a list of 3 points, where each point is a 2D array containing the location of the point.
+
+    Returns:
+        float: the angle between the three points.
     """
 
     a = points[0]
@@ -28,10 +34,20 @@ def find_fourth_point(
     cd: float,
     convex: bool,
 ) -> np.array:
-    """
-    Finds the fourth point (d) of a quadrilateral when 3 points (a, b, c) and the
-    distances da and cd are given, based on
-    http://paulbourke.net/geometry/circlesphere/#:~:text=Intersection%20of%20two%20circles
+    """Finds the fourth point (d) of a quadrilateral when 3 points (a, b, c) and the distances da and cd are given.
+
+    Based on http://paulbourke.net/geometry/circlesphere/#:~:text=Intersection%20of%20two%20circles.
+
+    Args:
+        a (np.array): a 2D array of the position of the first point of the quadrilateral.
+        b (np.array): a 2D array of the position of the second point of the quadrilateral.
+        c (np.array): a 2D array of the position of the third point of the quadrilateral.
+        da (float): the distance between point a and the fourth point (d) we try to find.
+        cd (float): the distance between point c and the fourth point (d) we try to find.
+        convex (bool): whether the quadrilateral is convex or not.
+
+    Returns:
+        np.array: the location of the fourth point of the quadrilateral.
     """
 
     r0, r1 = da, cd
@@ -48,9 +64,13 @@ def find_fourth_point(
 
 
 def get_angles(points: List[np.array]) -> List[float]:
-    """
-    Calculates the angles of a quadrilateral by giving the four points of it.
-    The order of returned angles is equal to the order of points given.
+    """Calculates the angles of a quadrilateral by giving the four points of it.
+
+    Args:
+        points (List[np.array]): A list containing the positions (2D numpy arrays) of the four points of a quadrilateral.
+
+    Returns:
+        List[float]: the four angles of the quadrilateral in the same order as the four points are given.
     """
 
     angles = []
@@ -67,11 +87,19 @@ def get_angles(points: List[np.array]) -> List[float]:
 
 
 def solve_quadritlateral(lengths: List[float], angle_b: float, convex: bool = True, debug: bool = False) -> List[float]:
-    """
-    Calculates the angles of a quadrilateral given all side lengths and the angle of point b.
-    Expects lengths to be given as [da, ab, bc, cd]
+    """Calculates the angles of a quadrilateral given all side lengths and the angle of point b.
+
     It first determines the location of all points where point a is at (0,0).
     Next it calculates and returns all angles.
+
+    Args:
+        lengths (List[float]): the lengths of the four sides of a quadrilateral, with expected order as [da, ab, bc, cd].
+        angle_b (float): the angle in the quadrilateral at point b.
+        convex (bool): whether the quadrilateral is convex or not.
+        debug (bool): whether debug mode is enabled or not.
+
+    Returns:
+        List[float]: the four angles of the quadrilateral.
     """
 
     da, ab, bc, cd = lengths
@@ -92,10 +120,14 @@ def solve_quadritlateral(lengths: List[float], angle_b: float, convex: bool = Tr
 
 
 def check_lengths(points: List[float], real_lengths: List[float]):
-    """
-    Checks whether the lengths between points are equal to the real lengths.
+    """Checks whether the calculated lengths between points are equal to the real lengths.
+
     Expects points as [a, b, c, d] and real_lengths as [da, ab, bc, cd].
     This method is only used for debugging.
+
+    Args:
+        points (List[np.array]): A list containing the positions (2D numpy arrays) of the four points [a, b, c, d] of a quadrilateral.
+        real_lengths (List[float]): A list containing the real lengths [da, ab, bc, cd] of a quadrilateral.
     """
 
     for i in range(len(points)):
