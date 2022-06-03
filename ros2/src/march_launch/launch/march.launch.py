15faa331ea279f5f91d0e6f6db51bc51294d445a
Tuhin Das <2hin.das@gmail.com>
2022-06-03 16:21:54 +0200

    Uncomment safety node

--- ros2/src/march_launch/launch/march.launch.py
+++ ros2/src/march_launch/launch/march.launch.py
@@ -433,16 +433,16 @@ def generate_launch_description() -> LaunchDescription:
                 ],
             ),
             # Safety
-            # IncludeLaunchDescription(
-            #     PythonLaunchDescriptionSource(
-            #         os.path.join(
-            #             get_package_share_directory("march_safety"),
-            #             "launch",
-            #             "march_safety.launch.py",
-            #         )
-            #     ),
-            #     launch_arguments=[("use_sim_time", use_sim_time)],
-            # ),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory("march_safety"),
+                        "launch",
+                        "march_safety.launch.py",
+                    )
+                ),
+                launch_arguments=[("use_sim_time", use_sim_time)],
+            ),
             # March robot information
             IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
