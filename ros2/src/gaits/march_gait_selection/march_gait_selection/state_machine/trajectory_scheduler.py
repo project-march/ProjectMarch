9e26f0325aa92e100e061c455b2a5b7be06a7401
George Vegelien <georgevegelien@gmail.com>
2022-06-03 17:38:58 +0200

    Add docs to trajectoy scheduler

--- ros2/src/gaits/march_gait_selection/march_gait_selection/state_machine/trajectory_scheduler.py
+++ ros2/src/gaits/march_gait_selection/march_gait_selection/state_machine/trajectory_scheduler.py
@@ -1,13 +1,12 @@
-"""Author: ???."""
+"""Author: George Vegelien, MVII."""
 
 from __future__ import annotations
 
 from queue import LifoQueue
-from typing import List, Set
+from typing import Set
 
 from attr import dataclass
 
-from control_msgs.action._follow_joint_trajectory import FollowJointTrajectory_GetResult_Response
 from rclpy import Future
 from rclpy.action import ActionClient
 from rclpy.action.client import ClientGoalHandle
@@ -21,8 +20,6 @@ from march_shared_msgs.msg import (
     FollowJointTrajectoryResult,
 )
 from trajectory_msgs.msg import JointTrajectory
-
-TRAJECTORY_SCHEDULER_HISTORY_DEPTH = 5
 
 
 @dataclass
@@ -62,79 +59,104 @@ class TrajectoryCommand:
 
 
 class TrajectoryScheduler:
-    """Scheduler that sends the wanted trajectories to the topic listened to by the exoskeleton/simulation.
+    """Scheduler that handles the trajectory communication with the controller controlling the exoskeleton/simulation.
+
+    Made and used only in the statemachine. Trajectories can be scheduled by calling the `schedule(...)` method.
 
     Args:
-        node (Node): node that is used to create subscribers/publishers
-    Attributes:
-        _logger (Logger): used to log to the terminal
-        _failed (bool): ???
-        _node (Node): node that is used to create subscribers/publishers
-        _goals (List[TrajectoryCommand]): list containing trajectory commands
-        _trajectory_goal_pub (Publisher): used to publish FollowJointTrajectoryActionGoal on
-            /march/controller/trajectory/follow_joint_trajectory_goal
-        _cancel_pub (Publisher): publishes a GoalID message on /march/controller/trajectory/follow_joint...
-            ..._trajectory/goal
-        _trajectory_goal_result_sub (Subscriber): Listens for FollowJointTrajectoryActionResult on
-            "march/controller/trajectory/follow_joint_trajectory/result. Receives the errors
-        _trajectory_command_pub (Publisher): publishes JointTrajectory messages on
-            /march/controller/trajectory/command
+        node (Node): Node that is used to create subscribers/publishers.
 
-        For more information on the Action client see these pages:
-            * https://docs.ros.org/en/foxy/Tutorials/Understanding-ROS2-Actions.html?highlight=action
-            * https://docs.ros.org/en/foxy/Tutorials/Actions/Creating-an-Action.html
-            * https://docs.ros.org/en/foxy/Tutorials/Actions/Writing-a-Py-Action-Server-Client.html
+    Attributes:
+        _logger (Logger): Used to log to the terminal.
+        _failed (bool): Checks if the last gait was successfully executed.
+        _node (Node): Node that is used to create an action client.
+        _active_goals (LifoQueue[Future[ClientGoalHandle]]): An Last in First out queue (aka 'stack') containing
+            the already finished future objects containing as result an ClientGoalHandles. Which handles the
+            active goals. These can be used to cancel the goal.
+        _messages_in_transit (Set[Future]): An set of future object of messages containing a request to start
+            a trajectory. These messages should be canceled if stopped. Otherwise, a message that is in transit can
+            still arrive after all goals are canceled.
+        _schedule_timeout (double): How long (in seconds) this object should wait before it can send a trajectory
+            goal request to the controller. The value is based on the parameter `early_schedule_duration`.
+        _action_client_to_controller (ActionClient): The client that send the initial connection request with the
+            controller server to send goals. Over the Action topic
+            `/joint_trajectory_controller/follow_joint_trajectory`.
+
+    More information about Actions:
+        * https://docs.ros.org/en/foxy/Tutorials/Understanding-ROS2-Actions.html?highlight=action
+        * https://docs.ros.org/en/foxy/Tutorials/Actions/Creating-an-Action.html
+        * https://docs.ros.org/en/foxy/Tutorials/Actions/Writing-a-Py-Action-Server-Client.html
     """
 
     def __init__(self, node: Node):
-        self._failed = False
         self._node = node
+        self._logger = node.get_logger().get_child(__class__.__name__)
+        self._failed = False
         self._active_goals = LifoQueue()
         self._messages_in_transit: Set[Future] = set()
-        self._logger = node.get_logger().get_child(__class__.__name__)
         self._schedule_timeout = node.get_parameter("early_schedule_duration").get_parameter_value().double_value
-        self._action_client = ActionClient(self._node, FollowJointTrajectory,
-                                           "/joint_trajectory_controller/follow_joint_trajectory")
+        self._action_client_to_controller = ActionClient(
+            self._node, FollowJointTrajectory, "/joint_trajectory_controller/follow_joint_trajectory"
+        )
 
     def schedule(self, command: TrajectoryCommand) -> None:
-        """Schedules a new trajectory.
+        """Sends a trajectory to the controller.
+
+        If the `command` does not contain a `start_time` the gait will start as soon as the message arrives.
+
+        This method will run `_gait_executor_start_request_cb(future)` if the requested trajectory is received at the
+        controller.
 
         Args:
-            command (TrajectoryCommand): The trajectory command to schedule
+            command (TrajectoryCommand): The trajectory command to schedule.
         """
         self._failed = False
         goal_msg = FollowJointTrajectory.Goal()
-        command.trajectory.header.stamp = command.start_time.to_msg()  # To set early scheduling
+        command.trajectory.header.stamp = command.start_time.to_msg()  # To set early scheduling.
         goal_msg.trajectory = command.trajectory
-        if not self._action_client.wait_for_server(self._schedule_timeout):
+        if not self._action_client_to_controller.wait_for_server(self._schedule_timeout):
             self._logger.warn(f"Failed to schedule trajectory {command} within {self._schedule_timeout} seconds")
             return
 
-        goal_future: Future = self._action_client.send_goal_async(goal_msg)
-        goal_future.add_done_callback(self._gait_executor_start_request_cb)
+        goal_future: Future = self._action_client_to_controller.send_goal_async(goal_msg)
+        goal_future.add_done_callback(self._controller_starts_trajectory_cb)
         self._messages_in_transit.add(goal_future)
 
-    def _gait_executor_start_request_cb(self, future: Future):
+    def _controller_starts_trajectory_cb(self, future: Future) -> None:
+        """Callback for if the trajectory start_request message has arrived at the controller.
+
+        It will check if the trajectory is accepted:
+            * If accepted it will store the goal_handle and start a call to `_controller_finished_trajectory_cb(future)`
+                when the trajectory is done executing.
+            * If not accepted, it will log a warning and do nothing. The trajectory will not be executed because
+                it is canceled from the controllers side.
+
+        Args:
+            future (Future[ClientGoalHandle]): Finished future object containing as result an ClientGoalHandle.
+        """
         self._messages_in_transit.remove(future)
         gait_executor_response_goal_handle: ClientGoalHandle = future.result()
         if not gait_executor_response_goal_handle.accepted:
-            self._logger.warning("Goal message to execute gait was not accepted by the Action server (e.g. gazebo). "
-                                 f"Goal: {gait_executor_response_goal_handle}")
+            self._logger.warning(
+                "Goal message to execute gait was not accepted by the Action server (e.g. gazebo). "
+                f"Goal: {gait_executor_response_goal_handle}"
+            )
             return
 
         self._active_goals.put(future)
-        self._logger.info(f"Goal message to execute gait is accepted.")
+        self._logger.info("Goal message to execute gait is accepted.")
         gait_execution_result_future: Future = gait_executor_response_goal_handle.get_result_async()
-        gait_execution_result_future.add_done_callback(self._gait_executor_finished_cb)
+        gait_execution_result_future.add_done_callback(self._controller_finished_trajectory_cb)
 
-    def _gait_executor_finished_cb(self, future: Future):
-        """
+    def _controller_finished_trajectory_cb(self, future: Future) -> None:
+        """Callback for if the trajectory is finished.
+
+        If the trajectory did not execute successfully it will set the `_failed` value to True, which can later be
+        retrieved by the statemachine.
 
         Args:
-            future (Future[FollowJointTrajectory_GetResult_Response]):
-
-        Returns:
-
+            future (Future[FollowJointTrajectory_GetResult_Response]): A future object containing the result if
+                the trajectory is successfully executed.
         """
         result: FollowJointTrajectoryResult = future.result().result
         if result.error_code != FollowJointTrajectoryResult.SUCCESSFUL:
@@ -142,7 +164,10 @@ class TrajectoryScheduler:
             self._failed = True
 
     def cancel_active_goals(self) -> None:
-        """Cancels the active goal."""
+        """Cancels the active goal.
+
+        If active goal is canceled the controller will hold its position.
+        """
         for msg in self._messages_in_transit:
             msg.cancel()
         while not self._active_goals.empty():
@@ -153,7 +178,10 @@ class TrajectoryScheduler:
         return self._failed
 
     def reset(self) -> None:
-        """Reset attributes of class."""
+        """Reset attributes of class.
+
+        Called everytime after successfully executing a gait.
+        """
         self._failed = False
         self._active_goals = LifoQueue()
         self._messages_in_transit.clear()
