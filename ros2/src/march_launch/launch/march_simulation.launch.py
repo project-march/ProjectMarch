3a59562e3542b913d0f696d96a7ada868fbe661c
Marten Haitjema <martenh99@gmail.com>
2022-08-01 11:31:44 +0200

    Remove arguments from march and march_simulation.launch and remove capital letter

--- ros2/src/march_launch/launch/march_simulation.launch.py
+++ ros2/src/march_launch/launch/march_simulation.launch.py
@@ -8,7 +8,6 @@ from launch.substitutions import LaunchConfiguration
 from march_utility.utilities.utility_functions import (
     get_lengths_robot_from_urdf_for_inverse_kinematics,
 )
-from march_goniometric_ik_solver.ik_solver_parameters import IKSolverParameters
 
 # Get lengths from urdf:
 LENGTH_HIP_AA, LENGTH_HIP_BASE = get_lengths_robot_from_urdf_for_inverse_kinematics(
@@ -73,12 +72,6 @@ def generate_launch_description() -> launch.LaunchDescription:
     first_subgait_delay = LaunchConfiguration("first_subgait_delay")
     early_schedule_duration = LaunchConfiguration("early_schedule_duration")
     timer_period = LaunchConfiguration("timer_period")
-    ankle_buffer = LaunchConfiguration("ankle_buffer")
-    hip_buffer = LaunchConfiguration("hip_buffer")
-    default_knee_bend = LaunchConfiguration("default_knee_bend")
-    hip_x_fraction = LaunchConfiguration("hip_x_fraction")
-    upper_body_front_rotation = LaunchConfiguration("upper_body_front_rotation")
-    dorsiflexion_at_end_position = LaunchConfiguration("dorsiflexion_at_end_position")
 
     # Fake sensor data
     fake_sensor_data = LaunchConfiguration("fake_sensor_data")
@@ -259,38 +252,6 @@ def generate_launch_description() -> launch.LaunchDescription:
                 default_value="False",
                 description="If true, no joints will be actuated",
             ),
-            # IK solver parameters
-            DeclareLaunchArgument(
-                name="ankle_buffer",
-                default_value=str(IKSolverParameters.ankle_buffer),
-                description="buffer between dorsiflexion soft limit and allowed dorsiflexion in the ik solver, in deg",
-            ),
-            DeclareLaunchArgument(
-                name="hip_buffer",
-                default_value=str(IKSolverParameters.hip_buffer),
-                description="buffer between retroflexion soft limit and allowed retroflexion in the ik solver, in deg",
-            ),
-            DeclareLaunchArgument(
-                name="default_knee_bend",
-                default_value=str(IKSolverParameters.default_knee_bend),
-                description="efault knee flexion angle, in deg",
-            ),
-            DeclareLaunchArgument(
-                name="hip_x_fraction",
-                default_value=str(IKSolverParameters.hip_x_fraction),
-                description="fraction of step at which hip is located",
-            ),
-            DeclareLaunchArgument(
-                name="upper_body_front_rotation",
-                default_value=str(IKSolverParameters.upper_body_front_rotation),
-                description="forward tilt of the backpack, in deg",
-            ),
-            DeclareLaunchArgument(
-                name="dorsiflexion_at_end_position",
-                default_value=str(IKSolverParameters.dorsiflexion_at_end_position),
-                description="Amount of dorsiflexion of swing leg ankle at end position. Takes regular ik solution "
-                "if it is set to zero.",
-            ),
             # FAKE SENSOR DATA ARGUMENTS
             DeclareLaunchArgument(
                 name="fake_sensor_data",
@@ -368,12 +329,6 @@ def generate_launch_description() -> launch.LaunchDescription:
                     ("first_subgait_delay", first_subgait_delay),
                     ("early_schedule_duration", early_schedule_duration),
                     ("timer_period", timer_period),
-                    ("ankle_buffer", ankle_buffer),
-                    ("hip_buffer", hip_buffer),
-                    ("default_knee_bend", default_knee_bend),
-                    ("hip_x_fraction", hip_x_fraction),
-                    ("upper_body_front_rotation", upper_body_front_rotation),
-                    ("dorsiflexion_at_end_position", dorsiflexion_at_end_position),
                     ("fake_sensor_data", fake_sensor_data),
                     ("minimum_fake_temperature", minimum_fake_temperature),
                     ("maximum_fake_temperature", maximum_fake_temperature),
