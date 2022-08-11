be9e765e071b56f77461407918da733275efaaa6
MonitorMarch <monitor@projectmarch.nl>
2022-08-11 20:31:26 +0200

    Set early schedule duration to 0.07 s

--- ros2/src/march_launch/launch/march.launch.py
+++ ros2/src/march_launch/launch/march.launch.py
@@ -298,7 +298,7 @@ def generate_launch_description() -> LaunchDescription:
         ),
         DeclareLaunchArgument(
             name="early_schedule_duration",
-            default_value="0.3",
+            default_value="0.070",
             description="Duration to schedule next subgait early. If 0 then the"
             "next subgait is never scheduled early.",
         ),
