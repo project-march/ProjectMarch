0d320535a5589a1a3ea1630b468869f9a4308991
Marco Bak <Marco2771@gmail.com>
2022-10-20 15:13:26 +0200

    Deleted the ros1 folder, together with aliases for the ros1, and other ros1 related configurations and files

--- .docker_local/start_scripts/bash/ros1.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/noetic/setup.bash
-if [ -f "${HOME}"/march/ros1/install/local_setup.bash ];then
-  source "${HOME}"/march/ros1/install/local_setup.bash
-fi
-exec bash
--- ros1/src/hardware/interface/march_hardware/config/sw_files/left_hip_aa.sw
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aea28e2d2600f4ad6829762992dbef218e9399789414353aa05e596eab17e160
-size 4617
