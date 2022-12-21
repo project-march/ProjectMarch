471e160bc23c68355d4d442abc0a23faba1d580c
Marco <marco2771@gmail.com>
2022-12-21 18:17:18 +0100

    format python launch file

--- ros2/src/march_launch/launch/march8_test.launch.py
+++ ros2/src/march_launch/launch/march8_test.launch.py
@@ -1,8 +1,8 @@
 """Author: MARCH."""
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
