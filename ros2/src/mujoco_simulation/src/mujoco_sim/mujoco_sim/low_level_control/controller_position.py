bcdb7b12930e4d1350df1956da8fa3d0af6def61
Sahand Wagemakers <sahandwagemakers@gmail.com>
2022-12-21 09:37:31 +0000

    Resolve "Refactor the low level controller"

--- ros2/src/mujoco_simulation/src/mujoco_sim/mujoco_sim/low_level_control/controller_position.py
+++ ros2/src/mujoco_simulation/src/mujoco_sim/mujoco_sim/low_level_control/controller_position.py
@@ -4,58 +4,53 @@ from low_level_controller import LowLvlController
 
 
 class PositionController(LowLvlController):
-    """This class implements the low level position control used by the mujoco simulation.
+    """A class which imitates the low-level control of the robot.
 
-    When the simulation receives a position command from the MARCH code, this class is called.
-    For the control a PD controller is used.
+    Functions as a PID right now which directly applies control
+    to the Mujoco simulation
+
+    Args:
+        LowLvlController (Class): Low level controller baseclass
     """
-
-    def __init__(self, origin, model, data, p, d):
+    def __init__(self, node, model, p, d):
         """A class which imitates the low-level control of the robot.
 
         Functions as a PID right now which directly applies control
         to the Mujoco simulation
+
         Args:
-            origin (object ID): the id of the simulation node
-            data (Mujoco Struct): Refers to the data struct from Mujoco
+            node (object ID): the id of the simulation node
+            model (Mujoco Struct): Refers to the simulated body in Mujoco
             p (float): Proportional-value of a PD controller
             d (float): Derivative-value of a PD controller
         """
-        # Define the amount of controllable joints based on
-        # the generalized coordinates generated within Mujoco
-        super().__init__(origin, model, data)
-        # Define the PD
+# Define the amount of controllable joints based on
+# the generalized coordinates generated within Mujoco
+        super().__init__(node, model)
+# Define the PD - values
         self.p = p
         self.d = d
 
-    def low_level_update(self, model, data):
+    def low_level_update(self, model, data) -> None:
         """The low-level control update callback function.
 
         This function is called by Mujoco at the start of every simulation step.
         While model isn't used as a variable, the callback function has
         to have this as an argument by Mujoco standards
+
         Args:
             model (Mujoco struct): Refers to the simulated body in Mujoco
             data (Mujoco struct): Refers to the data struct containing all model data in Mujoco
         """
-        # update the control inputs based on PID
+        dt = self.node.TIME_STEP_MJC
+# update the control inputs based on PID
+        for index in range(self.actuator_amount):
+            joint_val = self.sensor_map[index]
 
-        for count, act_name in enumerate(self.act_names):
-            dt = self.origin.TIME_STEP_MJC
-            index = self.joint_to_qpos[model.actuator_trnid[count, 0]]
+            e = self.joint_desired[index] - joint_val
+            de_prev = (e - self.e_prev[index]) / dt
 
-            joint_val = data.qpos[index]
-            try:
-                ref_dict = self.joint_ref_dict[act_name]
-            except KeyError:
-                ref_dict = 0
+            ctrl_input = self.p[index] * e + self.d[index] * de_prev
+            data.ctrl[index] += ctrl_input
 
-            e = float(ref_dict) - joint_val
-            de_prev = (e - self.e_prev[count]) / dt
-
-            new_ctrl_input = self.p[count] * e + self.d[count] * de_prev
-            self.ctrl[count] = new_ctrl_input
-            self.e_prev[count] = e
-            data.ctrl[count] += self.ctrl[count]
-
-        # NOTE:CHANGE THE WAY WE UPDATE THE MUJOCO UPDATE LATER
+            self.e_prev[index] = e
