e5b3df606a4562691d800b369b3f2845a74d1c9b
sahand_w <sahandwagemakers@gmail.com>
2023-01-02 14:17:43 +0100

    clang-tidy test

--- ros2/src/acados_solver/src/solver_node.cpp
+++ ros2/src/acados_solver/src/solver_node.cpp
@@ -20,14 +20,15 @@ class SolverNode : public rclcpp::Node {
     // TODO The solver node now only passes the given trajectory to without modification.
     // This needs to be changed when the MPC is implemented more.
 public:
+
     SolverNode()
         : Node("solver_node")
     {
-        robot_state_subscriber = this->create_subscription<march_shared_msgs::msg::RobotState>(
-            "robot_state", 10, std::bind(&SolverNode::robot_state_callback, this, _1));
-        gait_subscriber = this->create_subscription<trajectory_msgs::msg::JointTrajectory>(
-            "gait", 10, std::bind(&SolverNode::gait_callback, this, _1));
-        trajectory_publisher = this->create_publisher<trajectory_msgs::msg::JointTrajectory>("joint_trajectory", 10);
+        // robot_state_subscriber = this->create_subscription<march_shared_msgs::msg::RobotState>(
+        //     "robot_state", 10, std::bind(&SolverNode::robot_state_callback, this, _1));
+        // gait_subscriber = this->create_subscription<trajectory_msgs::msg::JointTrajectory>(
+        //     "gait", 10, std::bind(&SolverNode::gait_callback, this, _1));
+        // trajectory_publisher = this->create_publisher<trajectory_msgs::msg::JointTrajectory>("joint_trajectory", 10);
     };
 
 private:
@@ -41,9 +42,12 @@ private:
         trajectory_publisher->publish(*msg);
     };
 
-    rclcpp::Subscription<march_shared_msgs::msg::RobotState>::SharedPtr robot_state_subscriber;
-    rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr gait_subscriber;
-    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr trajectory_publisher;
+    rclcpp::Subscription<march_shared_msgs::msg::RobotState>::SharedPtr robot_state_subscriber = this->create_subscription<march_shared_msgs::msg::RobotState>(
+        "robot_state", 10, std::bind(&SolverNode::robot_state_callback, this, _1));
+    rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr gait_subscriber = this->create_subscription<trajectory_msgs::msg::JointTrajectory>(
+        "gait", 10, std::bind(&SolverNode::gait_callback, this, _1));
+    rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr trajectory_publisher = this->create_publisher<trajectory_msgs::msg::JointTrajectory>("joint_trajectory", 10);
+
 };
 
 int main(int argc, char** argv)
