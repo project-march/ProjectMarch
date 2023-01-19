63d5f178331d3358af67a695a0eab38d84c32415
Marco <marco2771@gmail.com>
2023-01-19 15:05:14 +0100

    Test untested incremental encoder functions

--- ros2/src/hardware/interface/march_hardware/test/encoder/incremental_encoder_test.cpp
+++ ros2/src/hardware/interface/march_hardware/test/encoder/incremental_encoder_test.cpp
@@ -12,7 +12,7 @@ class IncrementalEncoderTest : public testing::Test {
 protected:
     const size_t counts_per_rotation = 1 << 12;
     const double transmission = 100;
-    const march::MotorControllerType motor_controller_type = march::MotorControllerType::IMotionCube;
+    const march::MotorControllerType motor_controller_type = march::MotorControllerType::ODrive;
     march::IncrementalEncoder encoder
         = march::IncrementalEncoder(this->counts_per_rotation, this->motor_controller_type, this->transmission);
 };
@@ -22,11 +22,30 @@ TEST_F(IncrementalEncoderTest, ZeroIUToRad)
     ASSERT_EQ(0.0, this->encoder.positionIUToRadians(0));
 }
 
+TEST_F(IncrementalEncoderTest, GetTransmission)
+{
+    ASSERT_DOUBLE_EQ(this->transmission, this->encoder.getTransmission());
+}
+
 TEST_F(IncrementalEncoderTest, CorrectToRad)
 {
     const int32_t iu = 1000;
     const double expected = iu * 2.0 * M_PI / (this->counts_per_rotation * this->transmission);
     ASSERT_DOUBLE_EQ(expected, this->encoder.positionIUToRadians(iu));
+}
+
+TEST_F(IncrementalEncoderTest, VecolityIUToRadians)
+{
+    const double velocity = 10.0;
+    const double expected = 0.62831853071795862;
+    ASSERT_DOUBLE_EQ(expected, this->encoder.velocityIUToRadians(velocity));
+}
+
+TEST_F(IncrementalEncoderTest, velocityRadiansToIU)
+{
+    const double expected = 10;
+    const double velocity = 0.62831853071795862;
+    ASSERT_DOUBLE_EQ(expected, this->encoder.velocityRadiansToIU(velocity));
 }
 // NOLINTEND
 #endif
