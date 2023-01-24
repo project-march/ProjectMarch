ed73472a987a3f611642f69159f6cb3c1a33c710
Sahand Wagemakers <sahandwagemakers@gmail.com>
2023-01-24 14:06:15 +0000

    Resolve "Integrate IMU"

--- march_aliases.sh
+++ march_aliases.sh
@@ -128,4 +128,9 @@ FASTRTPS_DEFAULT_PROFILES_FILE=~/march/.fastrtps-profile.xml
 export FASTRTPS_DEFAULT_PROFILES_FILE
 
 # Alias for telling a joke when Koen has to wait for a long time
-alias joke='curl -H "Accept: text/plain" https://icanhazdadjoke.com/ && echo' 
+alias joke='curl -H "Accept: text/plain" https://icanhazdadjoke.com/ && echo'
+#
+## Aliases for performing IMU debugs
+alias kernel_devboard="sudo /sbin/modprobe ftdi_sio && echo 2639 0300 | sudo tee /sys/bus/usb-serial/drivers/ftdi_sio/new_id"
+alias dialout_grp="sudo usermod -G dialout -a $USER && newgrp dialout"
+
