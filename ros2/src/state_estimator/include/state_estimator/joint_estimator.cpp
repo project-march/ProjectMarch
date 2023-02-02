8c6eb9750d4306e9e53850f7be21c31b05d6fda1
sahand_w <sahandwagemakers@gmail.com>
2023-02-02 13:58:18 +0100

    Make start on joint estimator & Frame transformations

--- ros2/src/state_estimator/include/state_estimator/joint_estimator.cpp
+++ ros2/src/state_estimator/include/state_estimator/joint_estimator.cpp
@@ -1,18 +1,25 @@
 #include "joint_estimator.hpp"
 
 JointEstimator::JointEstimator(StateEstimator* owner, sensor_msgs::msg::JointState initial_joint_states)
-    :m_owner(owner)
-    ,m_joint_states(initial_joint_states)
+    : m_owner(owner)
+    , m_joint_states(initial_joint_states)
 {
-    
 }
 
 void JointEstimator::set_joint_states(sensor_msgs::msg::JointState& new_joint_states)
 {
-
 }
 
 sensor_msgs::msg::JointState JointEstimator::get_joint_states()
 {
     return m_joint_states;
+}
+
+void JointEstimator::get_joint_frames()
+{
+    int joint_amount = sizeof(m_joint_states.name)/sizeof(m_joint_states.name[0]);
+    std::array<geometry_msgs::msg::TransformStamped, joint_amount+2> transform_trames;
+    for(auto i : current_joint_states.pos){
+        
+    };
 }
