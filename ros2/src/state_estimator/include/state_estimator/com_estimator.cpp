959433e87486079e6fb9cbdf75a6e5c1aa66bca3
sahand_w <sahandwagemakers@gmail.com>
2023-02-14 14:38:41 +0100

    Fix the com estimation

--- ros2/src/state_estimator/include/state_estimator/com_estimator.cpp
+++ ros2/src/state_estimator/include/state_estimator/com_estimator.cpp
@@ -14,9 +14,9 @@ void ComEstimator::set_com_state(std::vector<CenterOfMass> mass_list)
     m_center_of_mass.position.point.z = 0;
     for (auto i : mass_list) {
         m_center_of_mass.mass += i.mass;
-        m_center_of_mass.position.point.x += i.position.point.x;
-        m_center_of_mass.position.point.y += i.position.point.y;
-        m_center_of_mass.position.point.z += i.position.point.z;
+        m_center_of_mass.position.point.x += i.position.point.x * i.mass;
+        m_center_of_mass.position.point.y += i.position.point.y * i.mass;
+        m_center_of_mass.position.point.z += i.position.point.z * i.mass;
     }
 
     m_center_of_mass.position.point.x = m_center_of_mass.position.point.x / m_center_of_mass.mass;
