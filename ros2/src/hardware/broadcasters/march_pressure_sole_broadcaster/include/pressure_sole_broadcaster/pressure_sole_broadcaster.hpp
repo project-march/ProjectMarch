99144675a72b3674b07e7879f003a432f551b968
Marco <marco2771@gmail.com>
2023-02-16 15:10:51 +0100

    start with semantic component

--- ros2/src/hardware/broadcasters/march_pressure_sole_broadcaster/include/pressure_sole_broadcaster/pressure_sole_broadcaster.hpp
+++ ros2/src/hardware/broadcasters/march_pressure_sole_broadcaster/include/pressure_sole_broadcaster/pressure_sole_broadcaster.hpp
@@ -3,6 +3,7 @@
 #define BUILD_MARCH_PRESSURE_SOLE_BROADCASTER_HPP
 
 #include "controller_interface/controller_interface.hpp"
+#include "pressure_sole_broadcaster/pressure_sole_semantic_component.hpp"
 
 namespace march_pressure_sole_broadcaster{
 
@@ -13,6 +14,7 @@ public:
     controller_interface::return_type init(const std::string& controller_name) override;
 
 private:
+    std::unique_ptr<PressureSoleSemanticComponent> pressure_sole_component;
     std::shared_ptr<rclcpp::Logger> logger_;
 
 };
