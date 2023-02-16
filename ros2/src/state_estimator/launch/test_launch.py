f90baa3e754a275628ddb2dacb05ec05512d101a
sahand_w <sahandwagemakers@gmail.com>
2023-02-16 15:07:47 +0100

    clang fix

--- ros2/src/state_estimator/launch/test_launch.py
+++ ros2/src/state_estimator/launch/test_launch.py
@@ -1,3 +1,4 @@
+"""Author: MVIII."""
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -5,15 +6,15 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    """Launch description of the state estimator
+    """Launch description of the state estimator.
 
     This node is started when the state estimator is run
     """
     config = os.path.join(
-    get_package_share_directory('state_estimator'),
-    'config',
-    'state_estimation_setup_params.yaml'
-    )
+        get_package_share_directory('state_estimator'),
+        'config',
+        'state_estimation_setup_params.yaml'
+        )
 
     return LaunchDescription([
         Node(
