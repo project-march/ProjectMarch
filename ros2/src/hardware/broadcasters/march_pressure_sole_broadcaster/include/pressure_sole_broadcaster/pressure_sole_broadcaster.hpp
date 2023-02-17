7c774e76f8e1c8ceab2a5916335d7f08ea427e19
Marco <marco2771@gmail.com>
2023-02-17 16:13:54 +0100

    Finish main code for pressure sole broadcaster

--- ros2/src/hardware/broadcasters/march_pressure_sole_broadcaster/include/pressure_sole_broadcaster/pressure_sole_broadcaster.hpp
+++ ros2/src/hardware/broadcasters/march_pressure_sole_broadcaster/include/pressure_sole_broadcaster/pressure_sole_broadcaster.hpp
@@ -4,18 +4,48 @@
 
 #include "controller_interface/controller_interface.hpp"
 #include "pressure_sole_broadcaster/pressure_sole_semantic_component.hpp"
+#include "pressure_sole_broadcaster/visibilitiy_control.h"
+#include <realtime_tools/realtime_publisher.h>
 
 namespace march_pressure_sole_broadcaster {
 
+using PRESSURE_SOLE_MSG = march_shared_msgs::msg::PressureSolesData;
+using PressureSoleRTPublisher = realtime_tools::RealtimePublisher<PRESSURE_SOLE_MSG>;
+
 class PressureSoleBroadcaster : public controller_interface::ControllerInterface {
 public:
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
     PressureSoleBroadcaster();
 
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
     controller_interface::return_type init(const std::string& controller_name) override;
+
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
+    controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
+    controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
+    controller_interface::return_type update() override;
+
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_configure(
+        const rclcpp_lifecycle::State& previous_state) override;
+
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_activate(
+        const rclcpp_lifecycle::State& previous_state) override;
+
+    PRESSURE_SOLE_BROADCASTER_PUBLIC
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn on_deactivate(
+        const rclcpp_lifecycle::State& previous_state) override;
 
 private:
     std::unique_ptr<PressureSoleSemanticComponent> pressure_sole_component;
     std::shared_ptr<rclcpp::Logger> logger_;
+    rclcpp::Publisher<PRESSURE_SOLE_MSG>::SharedPtr pressure_sole_publisher_;
+    std::unique_ptr<PressureSoleRTPublisher> realtime_pressure_sole_publisher_;
 };
 } // namespace march_pressure_sole_broadcaster
 #endif // BUILD_MARCH_PRESSURE_SOLE_BROADCASTER_HPP
