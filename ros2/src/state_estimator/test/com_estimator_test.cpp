3cdde23768435dbed9b37cd0eb89a89f1a6a2150
Rixt Hellinga <rixt.hellinga@projectmarch.nl>
2023-03-14 11:30:46 +0100

    Added tests for com estimator

--- ros2/src/state_estimator/test/com_estimator_test.cpp
+++ ros2/src/state_estimator/test/com_estimator_test.cpp
@@ -40,5 +40,49 @@ TEST_F(ComEstimatorTest, setComTest)
     ASSERT_EQ(com_estimator->get_com_state(), com);
 }
 
+TEST_F(ComEstimatorTest, setEmptyComTest)
+{
+    std::vector<CenterOfMass> masses;
+    CenterOfMass com;
+    com_estimator->set_com_state(masses);
+    ASSERT_TRUE(std::isnan(com_estimator->get_com_state().position.point.x));
+    ASSERT_TRUE(std::isnan(com_estimator->get_com_state().position.point.y));
+    ASSERT_TRUE(std::isnan(com_estimator->get_com_state().position.point.z));
+    ASSERT_EQ(com_estimator->get_com_state().mass, 0);
+}
+
+TEST_F(ComEstimatorTest, setMultipleComTest)
+{
+    std::vector<CenterOfMass> masses;
+    CenterOfMass com1;
+    com1.mass = 2;
+    com1.position.point.x = 1;
+    com1.position.point.y = 2;
+    com1.position.point.z = 1;
+    masses.push_back(com1);
+    CenterOfMass com2;
+    com2.mass = 2;
+    com2.position.point.x = 2;
+    com2.position.point.y = 2;
+    com2.position.point.z = 1;
+    masses.push_back(com2);
+    CenterOfMass com3;
+    com3.mass = 2;
+    com3.position.point.x = 1;
+    com3.position.point.y = 1;
+    com3.position.point.z = 1;
+    masses.push_back(com3);
+    CenterOfMass total;
+    total.mass = com1.mass + com2.mass + com3.mass;
+    total.position.point.x = (com1.position.point.x*com1.mass+com2.position.point.x*com2.mass+com3.position.point.x*com3.mass)/(com1.mass+com2.mass+com3.mass);
+    total.position.point.y = (com1.position.point.y*com1.mass+com2.position.point.y*com2.mass+com3.position.point.y*com3.mass)/(com1.mass+com2.mass+com3.mass);
+    total.position.point.z = (com1.position.point.z*com1.mass+com2.position.point.z*com2.mass+com3.position.point.z*com3.mass)/(com1.mass+com2.mass+com3.mass);
+    com_estimator->set_com_state(masses);
+    ASSERT_EQ(com_estimator->get_com_state().position.point.x, total.position.point.x);
+    ASSERT_EQ(com_estimator->get_com_state().position.point.y, total.position.point.y);
+    ASSERT_EQ(com_estimator->get_com_state().position.point.z, total.position.point.z);
+    ASSERT_EQ(com_estimator->get_com_state().mass, total.mass);
+}
+
 // NOLINTEND
 #endif
