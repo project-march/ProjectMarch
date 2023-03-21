98ca06f997b5c856720f0b5bf799760210d4417f
sahand_w <sahandwagemakers@gmail.com>
2023-03-21 17:00:15 +0100

    Add the com-jacobian

--- ros2/src/ik_solver/src/ik_solver.cpp
+++ ros2/src/ik_solver/src/ik_solver.cpp
@@ -4,59 +4,53 @@
 #include "ik_solver/ik_solver.hpp"
 
 IkSolver::IkSolver()
-    {
-    
-    }
+{
+}
 
 void IkSolver::load_urdf_model(std::string urdf_filename)
-    {
+{
     pinocchio::urdf::buildModel(urdf_filename, m_model);
     // pinocchio::buildModels::manipulator(m_model);
     m_model_data = pinocchio::Data(m_model);
     Jfoot.resize(6, m_model.nv);
     Jfoot.setZero();
-    }
+
+    Jcom.resize(3, m_model.nv);
+    Jcom.setZero();
+}
 
 void IkSolver::set_joint_configuration()
-    {
+{
     m_joint_pos = pinocchio::randomConfiguration(m_model);
     m_joint_vel = Eigen::VectorXd::Zero(m_model.nv);
     m_joint_acc = Eigen::VectorXd::Zero(m_model.nv);
-
-    }
+}
 
 int IkSolver::set_jacobian()
-    {
-    try
-        {
+{
+    try {
         // First, apply forward kinematics
         // pinocchio::forwardKinematics(m_model,m_model_data,m_joint_pos);
         pinocchio::computeJointJacobian(m_model, m_model_data, m_joint_pos, 4, Jfoot);
-        
+        Jcom = pinocchio::jacobianCenterOfMass(m_model, m_model_data, m_joint_pos);
         return 0;
-        }
-    catch(...)
-        {
+    } catch (...) {
         // Return Error CODE 1::JOINTS NOT INITIALIZED
         return 1;
-        }
-    return 1;
     }
+    return 1;
+}
 
 int IkSolver::get_model_joints()
-    {
-    try
-    {
+{
+    try {
         return m_model.nv;
-    }
-    catch(...)
-    {
+    } catch (...) {
         return -1;
     }
-    
-    }
+}
 
-pinocchio::Data::Matrix6x IkSolver::get_model_jacobian()
-    {
-    return Jfoot;
-    }
+pinocchio::Data::Matrix3x IkSolver::get_model_jacobian()
+{
+    return Jcom;
+}
