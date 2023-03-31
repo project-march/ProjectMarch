6be895cbf8041e0e428b2c036caf9ddf2978370e
sahand_w <sahandwagemakers@gmail.com>
2023-03-31 17:13:44 +0200

    add the ik solver buffer node

--- ros2/src/ik_solver_buffer/include/ik_solver_buffer/ik_solver_buffer_node.hpp
+++ ros2/src/ik_solver_buffer/include/ik_solver_buffer/ik_solver_buffer_node.hpp
@@ -2,13 +2,29 @@
 #define IK_SOLVER_BUFFER_NODE_H
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_array.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "march_shared_msgs/msg/ik_solver_command.hpp"
 
 class BufferNode : public rclcpp::Node {
 public:
     BufferNode();
 
 private:
+    void com_subscriber_callback(geometry_msgs::msg::PoseArray::SharedPtr);
+    void swing_subscriber_callback(geometry_msgs::msg::PoseArray::SharedPtr);
+    void foot_subscriber_callback(geometry_msgs::msg::PointStamped::SharedPtr);
 
+    void publish_ik_trajectory();
+    geometry_msgs::msg::PoseArray m_latest_com_trajectory;
+    geometry_msgs::msg::PoseArray m_latest_swing_trajectory;
+    geometry_msgs::msg::PoseArray m_latest_placed_foot;
+
+    rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr m_com_subscriber;
+    rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr m_swing_subscriber;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr m_foot_subscriber;
+
+    rclcpp::Publisher<march_shared_msgs::msg::IkSolverCommand>::SharedPtr m_buffer_publisher;
 };
 
 
--- ros2/src/ik_solver_buffer/src/ik_solver_buffer_node.cpp
+++ ros2/src/ik_solver_buffer/src/ik_solver_buffer_node.cpp
@@ -1,11 +1,46 @@
 #include <cstdio>
 #include "ik_solver_buffer/ik_solver_buffer_node.hpp"
 
+#include <chrono>
+#include <cstdio>
+#include <string>
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 BufferNode::BufferNode()
   :Node("trajectory_buffer")
   {
+    m_buffer_publisher = this->create_publisher<march_shared_msgs::msg::IkSolverCommand>("ik_solver_input", 10);
+  
+    m_com_subscriber = this->create_subscription<geometry_msgs::msg::PoseArray>(
+      "/robot_com_position", 10, std::bind(&BufferNode::com_subscriber_callback, this, _1));
+    m_swing_subscriber = this->create_subscription<geometry_msgs::msg::PoseArray>(
+      "/bezier_trajectory", 10, std::bind(&BufferNode::swing_subscriber_callback, this, _1));
+    m_foot_subscriber = this->create_subscription<geometry_msgs::msg::PointStamped>(
+      "/est_foot_position", 10, std::bind(&BufferNode::foot_subscriber_callback, this, _1));
 
   }
+
+void BufferNode::com_subscriber_callback(geometry_msgs::msg::PoseArray::SharedPtr msg)
+{
+
+}
+
+void BufferNode::swing_subscriber_callback(geometry_msgs::msg::PoseArray::SharedPtr msg)
+{
+  
+}
+
+void BufferNode::foot_subscriber_callback(geometry_msgs::msg::PointStamped::SharedPtr msg)
+{
+
+}
+
+void BufferNode::publish_ik_trajectory()
+{
+
+}
 
 int main(int argc, char ** argv)
 {
