863005693e2459c343ee9252e8d82f141487a921
Marco <marco2771@gmail.com>
2023-03-31 12:03:40 +0200

    fix input from acrion server

--- ros2/src/mujoco_simulation/src/mujoco_writer/mujoco_writer/mujoco_writer_node.py
+++ ros2/src/mujoco_simulation/src/mujoco_writer/mujoco_writer/mujoco_writer_node.py
@@ -2,7 +2,9 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.action import ActionServer, GoalResponse
 from control_msgs.msg import JointTrajectoryControllerState
+from control_msgs.action import FollowJointTrajectory
 from std_msgs.msg import Bool
 
 from mujoco_interfaces.msg import MujocoInput
@@ -24,17 +26,44 @@ class MujocoWriterNode(Node):
         """
         super().__init__("mujoco_writer")
         self.publisher = self.create_publisher(MujocoInput, "mujoco_input", 10)
-        self.subscription = self.create_subscription(
-            JointTrajectoryControllerState, "joint_trajectory_controller/state", self.listener_callback, 10
-        )
+        # self.subscription = self.create_subscription(
+        #     JointTrajectoryControllerState, "joint_trajectory_controller/state", self.listener_callback, 10
+        # )
 
         # A subscriber that notifies if the queue with trajectory points has to  be reset.
         self.reset_subscription = self.create_subscription(
             Bool, "/march/mujoco_reset_trajectory", self.reset_callback, 10
         )
+
+        self._action_server = ActionServer(
+            self,
+            FollowJointTrajectory,
+            '/joint_trajectory_controller/follow_joint_trajectory',
+            self.execute_callback)
+
         self.reset = False
         # self.subscription  # prevent unused variable warning
 
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+        trajectory = goal_handle.request.trajectory.points
+        for msg in trajectory:
+            msg_tosend = MujocoInput()
+            skip = False
+            for i, x in enumerate(msg.positions):
+                if x != x:
+                    skip = False
+                    break
+                else:
+                    msg.positions[i] *= 1
+            if not skip:
+                msg_tosend.trajectory = JointTrajectoryControllerState()
+                msg_tosend.trajectory.desired = msg
+                if self.reset:
+                    msg_tosend.reset = 1
+                    self.reset = False
+                self.publisher.publish(msg_tosend)
+        return GoalResponse.ACCEPT_AND_EXECUTE;
     def listener_callback(self, msg):
         """This listener callback publishes all the messages from the MARCH code to the topic Mujoco sim subscribes to.
 
