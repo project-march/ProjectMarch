99dba59d382679d3083e4bb5b87fd61397d82616
Marco <marco2771@gmail.com>
2023-04-20 17:37:08 +0200

    update swinglogtool

--- ros2/src/swing_leg_trajectory_generator/include/swing_leg_trajectory_generator/swing_leg_trajectory_generator_node.hpp
+++ ros2/src/swing_leg_trajectory_generator/include/swing_leg_trajectory_generator/swing_leg_trajectory_generator_node.hpp
@@ -5,12 +5,11 @@
 #ifndef BUILD_SWING_LEG_TRAJECTORY_GENERATOR_NODE_HPP
 #define BUILD_SWING_LEG_TRAJECTORY_GENERATOR_NODE_HPP
 #include "geometry_msgs/msg/point_stamped.hpp"
-#include "march_shared_msgs/msg/gait_type.hpp"
-//#include "march_shared_msgs/msg/point_stamped_list.hpp"
-#include "geometry_msgs/msg/point_stamped.hpp"
 #include "geometry_msgs/msg/pose_array.hpp"
+#include "march_shared_msgs/msg/gait_type.hpp"
 #include "march_shared_msgs/srv/gait_command.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
 #include "swing_leg_trajectory_generator/swing_leg_trajectory_generator.hpp"
 #include <chrono>
 #include <cstdio>
@@ -25,8 +24,10 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr m_publish_curve;
     rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr m_points_subscription;
     rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr m_final_feet_subscriber;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr m_stance_feet_subscriber;
 
     void subscriber_callback(geometry_msgs::msg::PoseArray::SharedPtr msg);
+    void stance_feet_callback(std_msgs::msg::Int32::SharedPtr msg);
     void final_feet_callback(geometry_msgs::msg::PoseArray::SharedPtr msg);
 
     SwingLegTrajectoryGenerator m_swing_leg_generator;
