a1f24a40edda8c76a6bbf925da238a04ffa071fc
Marco <marco2771@gmail.com>
2023-05-03 17:30:42 +0200

    update tests

--- ros2/src/state_estimator/test/zmp_estimator_test.cpp
+++ ros2/src/state_estimator/test/zmp_estimator_test.cpp
@@ -105,29 +105,29 @@ TEST_F(ZmpEstimatorTest, accelerateZmpTest)
     zmp_estimator->set_com_states(com, t1);
     zmp_estimator->set_zmp();
 
-//    ASSERT_EQ(0.0, zmp_estimator->get_zmp().point.x);
-//    ASSERT_EQ(0.0, zmp_estimator->get_zmp().point.y);
-//    ASSERT_EQ(zmp_estimator->get_zmp().point.z, 0);
-//
-//    com.position.point.x = 3;
-//    com.position.point.y = 3;
-//    com.position.point.z = 3;
-//    seconds++;
-//    rclcpp::Time t2(static_cast<uint64_t>(seconds * 1e9));
-//
-//    zmp_estimator->set_com_states(com, t2);
-//    zmp_estimator->set_zmp();
-//
-//    /*
-//     * pos [0]: 3, [1]: 1, [2]: 0
-//     * vel [0]: 3-1=2, [1]: 1-0=1
-//     * acc 2-1=1
-//     * 3 - 1/g * 1 = 2.898
-//     */
-//
-//    ASSERT_TRUE(abs(zmp_estimator->get_zmp().point.x - 2.898) < 0.001);
-//    ASSERT_TRUE(abs(zmp_estimator->get_zmp().point.y - 2.898) < 0.001);
-//    ASSERT_EQ(zmp_estimator->get_zmp().point.z, 0);
+    //    ASSERT_EQ(0.0, zmp_estimator->get_zmp().point.x);
+    //    ASSERT_EQ(0.0, zmp_estimator->get_zmp().point.y);
+    //    ASSERT_EQ(zmp_estimator->get_zmp().point.z, 0);
+    //
+    //    com.position.point.x = 3;
+    //    com.position.point.y = 3;
+    //    com.position.point.z = 3;
+    //    seconds++;
+    //    rclcpp::Time t2(static_cast<uint64_t>(seconds * 1e9));
+    //
+    //    zmp_estimator->set_com_states(com, t2);
+    //    zmp_estimator->set_zmp();
+    //
+    //    /*
+    //     * pos [0]: 3, [1]: 1, [2]: 0
+    //     * vel [0]: 3-1=2, [1]: 1-0=1
+    //     * acc 2-1=1
+    //     * 3 - 1/g * 1 = 2.898
+    //     */
+    //
+    //    ASSERT_TRUE(abs(zmp_estimator->get_zmp().point.x - 2.898) < 0.001);
+    //    ASSERT_TRUE(abs(zmp_estimator->get_zmp().point.y - 2.898) < 0.001);
+    //    ASSERT_EQ(zmp_estimator->get_zmp().point.z, 0);
 }
 
 // NOLINTEND
