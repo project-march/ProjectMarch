9a57761a76d90e9978e48ece77b31721e58defdd
Test Setup <testsetup@projectmarch.nl>
2023-05-22 12:25:48 +0200

    Fix position control on linears

--- ros2/src/march_control/launch/control_test_setup.launch.py
+++ ros2/src/march_control/launch/control_test_setup.launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
     control_yaml = LaunchConfiguration("control_yaml")
 
     control_xacro =  "test_linear_control.xacro"
-    if IfCondition(test_rotational):
+    if test_rotational == "true":
         control_xacro =  "test_rotational_control.xacro"
 
     declared_arguments = [
