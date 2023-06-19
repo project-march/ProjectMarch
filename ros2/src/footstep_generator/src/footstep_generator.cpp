445fbb68fbe8f9d6e4bd75a127fd92b2e3818f91
Marco <marco2771@gmail.com>
2023-06-19 16:35:32 +0200

    update footstep_generator tests and add doctrings to code

--- ros2/src/footstep_generator/src/footstep_generator.cpp
+++ ros2/src/footstep_generator/src/footstep_generator.cpp
@@ -3,6 +3,14 @@ using namespace std::chrono_literals;
 using std::placeholders::_1;
 using std::placeholders::_2;
 
+/**
+ * The footstep generator takes the walking commands from the state machine, and calculated the footsteps that the
+ * zmp-mpc will use. For now it just bases the steps on the desired step size, number of steps, and the current feet
+ * positions.
+ *
+ * The requests from the state-machine come in on the footstep_generator service.
+ * THe generated footsteps are published on the /desired_footstep topic.
+ */
 FootstepGenerator::FootstepGenerator()
     : Node("footstep_generator_node")
     , m_steps(20) // Change this so we can interactively edit the amount of footsteps while Koengaiting
@@ -21,6 +29,13 @@ FootstepGenerator::FootstepGenerator()
     m_vx = this->get_parameter("step_length").as_double();
 }
 
+/**
+ * This method processes the service request from the state-machine.
+ * The method also publishes the generated footsteps.
+ *
+ * @param request Request from the state-machine, with either stand, walk or step-and-close
+ * @param response If the request was processed successfully.
+ */
 void FootstepGenerator::publish_foot_placements(
     const std::shared_ptr<march_shared_msgs::srv::RequestFootsteps::Request> request,
     std::shared_ptr<march_shared_msgs::srv::RequestFootsteps::Response> response)
@@ -40,21 +55,22 @@ void FootstepGenerator::publish_foot_placements(
     }
 }
 
+/**
+ * This method generates the desired footsteps for the different possible stances.
+ *
+ * @param stance_leg Our frame is from the right foot, where y=0. 1 is right leg, -1 is left leg.
+ * @param gait_type The gait for which we need footsteps, 1 is STAND, 2 is WALK, 3 = STEP_CLOSE
+ * @return Array with generated footsteps, empty when the request is invalid.
+ */
 geometry_msgs::msg::PoseArray FootstepGenerator::generate_foot_placements(int stance_leg, int gait_type)
 {
     geometry_msgs::msg::PoseArray footstep_array;
     geometry_msgs::msg::Pose footstep;
     footstep_array.header.frame_id = "mpc_frame";
     double x = 0.0;
-    // Our frame is from the right foot, where y=0;
-    // 1 is right leg
-    // -1 is left leg
+
     stance_leg = -1;
     double y = m_l / 2 - stance_leg * m_l / 2;
-    // STAND = 1
-    // WALK = 2
-    // STEP_CLOSE = 3
-
     switch (gait_type) {
         case 1:
             y = 0;
@@ -100,18 +116,6 @@ geometry_msgs::msg::PoseArray FootstepGenerator::generate_foot_placements(int st
             break;
 
         case 3:
-            // for (int i = 0; i < 1; i++) {
-            //     x += m_vx * 1.0;
-            //     y += m_vy * 1.0 - stance_leg * m_l;
-            //     stance_leg = -stance_leg;
-
-            //     footstep.position.x = x;
-            //     footstep.position.y = y;
-            //     footstep.position.z = 0;
-
-            //     footstep_array.poses.push_back(footstep);
-            // }
-
             // starting step
             x = 0.0;
             y = y;
@@ -142,31 +146,46 @@ geometry_msgs::msg::PoseArray FootstepGenerator::generate_foot_placements(int st
                 // printf("stance_leg %i\n", stance_leg);
             }
             break;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "UNABLE TO GENERATE FOOTSTEPS FOR STANCE LEG %d, AND GAIT TYPE %d",
+                stance_leg, gait_type);
+            RCLCPP_ERROR(this->get_logger(), "RETURNED EMPTY FOOTSTEP ARRAY");
     }
 
     return footstep_array;
 }
 
-void FootstepGenerator::publish_footsteps(geometry_msgs::msg::PoseArray footsteps)
-{
-    m_publisher->publish(footsteps);
-}
-
+/**
+ *
+ * @return amount of steps that will be generated for walk
+ */
 int FootstepGenerator::get_steps()
 {
     return m_steps;
 }
 
+/**
+ *
+ * @return velocity of the steps in the x direction
+ */
 double FootstepGenerator::get_velocity_x()
 {
     return m_vx;
 }
 
+/**
+ *
+ * @return velocity of the steps in the y direction
+ */
 double FootstepGenerator::get_velocity_y()
 {
     return m_vy;
 }
 
+/**
+ *
+ * @return the length that the feet are apart
+ */
 double FootstepGenerator::get_feet_spread()
 {
     return m_l;
--- ros2/src/footstep_generator/test/footstep_gen_test.cpp
+++ ros2/src/footstep_generator/test/footstep_gen_test.cpp
@@ -33,6 +33,20 @@ TEST_F(FootstepGeneratorTest, gettersTest)
     ASSERT_EQ(this->footstep_gen->get_feet_spread(), 0.33);
 }
 
+TEST_F(FootstepGeneratorTest, getStanceFootstepsTest)
+{
+    double expected_dist_x = this->footstep_gen->get_velocity_x();
+    double expected_dist_y = this->footstep_gen->get_velocity_y();
+    const double margin = 0.5;
+
+    geometry_msgs::msg::PoseArray footsteps = this->footstep_gen->generate_foot_placements(1, 1);
+    double footstep_x = footsteps.poses[this->footstep_gen->get_steps() - 1].position.x;
+    double footstep_y = footsteps.poses[this->footstep_gen->get_steps() - 1].position.y;
+
+    ASSERT_LT(std::abs(footstep_x - expected_dist_x), margin);
+    ASSERT_LT(std::abs(footstep_y - expected_dist_y), margin);
+}
+
 TEST_F(FootstepGeneratorTest, getWalkedDistanceTest)
 {
     double expected_dist_x = this->footstep_gen->get_steps() * this->footstep_gen->get_velocity_x();
