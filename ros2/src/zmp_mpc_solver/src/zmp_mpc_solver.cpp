6b2bd4b2e8f0f9d25fb9e080e5a5f37416a9f897
Monitor Laptop <monitor@projectmarch.nl>
2023-06-22 09:47:08 +0200

    uncommented the weight shift checkpoint in the zmp mpc

--- ros2/src/zmp_mpc_solver/src/zmp_mpc_solver.cpp
+++ ros2/src/zmp_mpc_solver/src/zmp_mpc_solver.cpp
@@ -440,16 +440,16 @@ inline int ZmpSolver::solve_zmp_mpc(
     // only change the initial count when a new footstep has to be set and check if the weight shift is done by checking
     // the current stance foot and ZMP location based on a margin. (The ZMP has to be on the new stance foot)
 
-    // if (m_current_shooting_node == 0 && m_current_stance_foot == -1 && m_current_count == 1 && check_zmp_on_foot()
+    if (m_current_shooting_node == 0 && m_current_stance_foot == -1 && m_current_count == 1 && check_zmp_on_foot()
 
-    //     || m_current_shooting_node == 0 && m_current_stance_foot == 1 && m_current_count == -1 && check_zmp_on_foot()) {
-    //     m_current_count = m_current_stance_foot;
-    //     m_step_counter++;
-    //     RCLCPP_INFO(rclcpp::get_logger("Jack stinkt"), "weight shift is complete \n");
-    //     printf("now going into current_shooting node %i\n", m_current_shooting_node);
-    // } else if (m_current_shooting_node == 0) {
-    //     m_current_shooting_node--;
-    // }
+        || m_current_shooting_node == 0 && m_current_stance_foot == 1 && m_current_count == -1 && check_zmp_on_foot()) {
+        m_current_count = m_current_stance_foot;
+        m_step_counter++;
+        RCLCPP_INFO(rclcpp::get_logger("Jack stinkt"), "weight shift is complete \n");
+        printf("now going into current_shooting node %i\n", m_current_shooting_node);
+    } else if (m_current_shooting_node == 0) {
+        m_current_shooting_node--;
+    }
 
     printf("step_counter %i\n", m_step_counter);
     printf("current stance foot is %i\n", m_current_stance_foot);
