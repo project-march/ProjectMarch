459572fca4fee5c2e24978fb4564d935a61e7733
Monitor March <monitor@projectmarch.nl>
2023-06-22 15:33:46 +0200

    new ik tunings

--- ros2/src/ik_solver/src/ik_solver.cpp
+++ ros2/src/ik_solver/src/ik_solver.cpp
@@ -122,13 +122,13 @@ Eigen::VectorXd IkSolver::solve_for_velocity(state state_current, state state_de
 {
     // We put the weights here, but of course we can remove them later
     // WEIGHTS
-    double left_weight = 0.1;
-    double right_weight = 0.1;
-    double CoM_weight = 10.0;
+    double left_weight = 1.0;
+    double right_weight = 1.0;
+    double CoM_weight = 2.0;
     double qdot_weight = 1e-6;
     // double base_weight = 1;
 
-    double CoM_gains = 25.0; // / dt;
+    double CoM_gains = 1.0; // / dt;
     double left_gains = 1.0; // / dt;
     double right_gains = 1.0; // / dt;
 
