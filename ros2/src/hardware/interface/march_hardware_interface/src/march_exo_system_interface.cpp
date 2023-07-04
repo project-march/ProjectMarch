a0d82fb3b6bfd84d1927ab604d8ec7109f1a2796
Monitor March <monitor@projectmarch.nl>
2023-07-04 10:08:07 +0200

    added if clauses for setting joint specific fuzzy values in write method

--- ros2/src/hardware/interface/march_hardware_interface/src/march_exo_system_interface.cpp
+++ ros2/src/hardware/interface/march_hardware_interface/src/march_exo_system_interface.cpp
@@ -532,18 +532,18 @@ hardware_interface::return_type MarchExoSystemInterface::write()
         RCLCPP_INFO_ONCE((*logger_), "The fuzzy target values are as follows: \n target position: %f \n measured position: %f \n position weight: %f \n target torque: %f \n measured torque: %f \n torque weight: %f",
         jointInfo.target_position, jointInfo.position, jointInfo.position_weight, jointInfo.target_torque, jointInfo.torque, jointInfo.torque_weight);
         #endif
-        if(jointInfo.name.compare("left_hip_aa") == 0){
-            // RCLCPP_INFO((*logger_), "left hip aa measured torque: %f", jointInfo.torque);
-            jointInfo.joint.actuate((float)jointInfo.target_position, -0.003f, 0.6f, 0.4f);
-        }
-        else if(jointInfo.name.compare("right_hip_aa") == 0){
-            // RCLCPP_INFO((*logger_), "right hip aa measured torque: %f", jointInfo.torque);
-            jointInfo.joint.actuate((float)jointInfo.target_position, -0.07f, 0.6f, 0.4f);
-        }
-        else{
+        // if(jointInfo.name.compare("left_hip_aa") == 0){
+        //     // RCLCPP_INFO((*logger_), "left hip aa measured torque: %f", jointInfo.torque);
+        //     jointInfo.joint.actuate((float)jointInfo.target_position, -0.003f, 0.6f, 0.4f);
+        // }
+        // else if(jointInfo.name.compare("right_hip_aa") == 0){
+        //     // RCLCPP_INFO((*logger_), "right hip aa measured torque: %f", jointInfo.torque);
+        //     jointInfo.joint.actuate((float)jointInfo.target_position, -0.07f, 0.6f, 0.4f);
+        // }
+        // else{
             // ACTUAL TORQUE LINE
             jointInfo.joint.actuate((float)jointInfo.target_position, (float)jointInfo.target_torque, (float)jointInfo.position_weight, (float)jointInfo.torque_weight);
-        }
+        // }
 
     }
 
