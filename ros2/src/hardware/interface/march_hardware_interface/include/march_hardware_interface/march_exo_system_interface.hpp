5990b22b39f45473fa900ffa2537dee1b7309ad5
TestSetupMARCH <testsetup@projectmarch.nl>
2023-07-07 12:13:12 +0200

    hardcoded ankles into fuzzy

--- ros2/src/hardware/interface/march_hardware_interface/include/march_hardware_interface/march_exo_system_interface.hpp
+++ ros2/src/hardware/interface/march_hardware_interface/include/march_hardware_interface/march_exo_system_interface.hpp
@@ -99,7 +99,7 @@ struct JointInfo {
         void weight_callback(march_shared_msgs::msg::WeightStamped::SharedPtr msg)
         {
             #ifdef TORQUEDEBUG
-            RCLCPP_INFO_STREAM(this->get_logger(), "Ignoring weights are in from fuzzy node: joint : " << msg->joint_name << " position " << msg->position_weight << ", torque " << msg->torque_weight);
+            RCLCPP_INFO_STREAM_ONCE(this->get_logger(), "Ignoring weights are in from fuzzy node: joint : " << msg->joint_name << " position " << msg->position_weight << ", torque " << msg->torque_weight);
             // return;
             #endif
             // FIXME: BEUNFIX
@@ -196,11 +196,11 @@ struct JointInfo {
                 float avg_torque = std::accumulate(total.begin(), total.end(), 0.0) / total.size();
                 RCLCPP_INFO_STREAM(this->get_logger(), "joint " << jointInfo.name << " has average torque " << avg_torque << " measured over " << total.size() << " values");
                 // FIXME: BEUNFIX
-                if(jointInfo.name.compare("left_hip_fe") == 0 || jointInfo.name.compare("right_hip_fe") == 0){
+                if(jointInfo.name.compare("left_ankle") == 0 || jointInfo.name.compare("right_ankle") == 0){
                     RCLCPP_INFO_STREAM(this->get_logger(), "putting the values into fuzzy!");
                     jointInfo.target_torque = avg_torque;
-                    jointInfo.torque_weight = 0.1;
-                    jointInfo.position_weight = 0.9;
+                    jointInfo.torque_weight = 0.6;
+                    jointInfo.position_weight = 0.4;
                 }
                 // Either this or target_torque = jointInfo.joint.torque_sensor.getAverageTorque(); in the cpp if we want to hardcode it
             }
