fb69fd8747ae022aeb0a331f8c938e61622665da
Test Setup <testsetup@projectmarch.nl>
2023-07-07 19:10:10 +0200

    upp max torque for koengait

--- ros2/src/hardware/interface/march_hardware_interface/include/march_hardware_interface/march_exo_system_interface.hpp
+++ ros2/src/hardware/interface/march_hardware_interface/include/march_hardware_interface/march_exo_system_interface.hpp
@@ -196,12 +196,12 @@ struct JointInfo {
                 float avg_torque = std::accumulate(total.begin(), total.end(), 0.0) / total.size();
                 RCLCPP_INFO_STREAM(this->get_logger(), "joint " << jointInfo.name << " has average torque " << avg_torque << " measured over " << total.size() << " values");
                 // FIXME: BEUNFIX
-                if(jointInfo.name.compare("left_ankle") == 0 || jointInfo.name.compare("right_ankle") == 0){
-                    RCLCPP_INFO_STREAM(this->get_logger(), "putting the values into fuzzy!");
-                    jointInfo.target_torque = avg_torque;
-                    jointInfo.torque_weight = 0.6;
-                    jointInfo.position_weight = 0.4;
-                }
+                // if(jointInfo.name.compare("left_ankle") == 0 || jointInfo.name.compare("right_ankle") == 0){
+                //     RCLCPP_INFO_STREAM(this->get_logger(), "putting the values into fuzzy!");
+                //     jointInfo.target_torque = avg_torque;
+                //     jointInfo.torque_weight = 0.6;
+                //     jointInfo.position_weight = 0.4;
+                // }
                 // Either this or target_torque = jointInfo.joint.torque_sensor.getAverageTorque(); in the cpp if we want to hardcode it
             }
         }
