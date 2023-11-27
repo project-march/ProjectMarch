cb57d5092e97526df1b81515e4e180516f660bb4
Andrew <ahutani@gmail.com>
2023-11-27 15:20:39 +0100

    Move exoState to its own file
    This allows for a central place where the exoState are defined

--- ros2/src/march_input_device/include/march_input_device/input_device.hpp
+++ ros2/src/march_input_device/include/march_input_device/input_device.hpp
@@ -5,29 +5,8 @@
 #pragma once
 #include <set>
 #include <string>
+#include "../../state_machine/include/state_machine/exo_state.hpp"
 
-
-//TODO: Move this class to its own file structure in state machine
-enum class exoState { Sit = 0, Stand = 1, Walk = 2, StepClose = 3, ForceUnknown = 4, Error = 5 };
-
-// Function to convert exoState enum to string
-inline std::string toString(exoState state) {
-    switch (state) {
-        case exoState::Sit: return "Sit";
-        case exoState::Stand: return "Stand";
-        case exoState::Walk: return "Walk";
-        case exoState::StepClose: return "StepClose";
-        case exoState::ForceUnknown: return "ForceUnknown";
-        case exoState::Error: return "Error";
-        default: return "Unknown State";
-    }
-}
-
-// Overload operator<< for exoState
-inline std::ostream& operator<<(std::ostream& os, exoState state) {
-    os << toString(state);
-    return os;
-}
 
 class IPD {
 public:
