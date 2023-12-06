0e172a901cf5bb5eb14104250a6a5a4c1ebf4f69
Pleuntje Brons <pleuntjebrons@gmail.com>
2023-12-06 14:18:04 +0100

    Add argument for opening a new terminal
    Fix the state prints as string

--- ros2/src/march_input_device/launch/input_device.launch.py
+++ ros2/src/march_input_device/launch/input_device.launch.py
@@ -1,7 +1,7 @@
 """Author: MVIII."""
 import os
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
+from launch import LaunchDescription, conditions
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess, DeclareLaunchArgument
@@ -21,23 +21,19 @@ def generate_launch_description():
         ),
     ]
     node = Node(
-                package="march_input_device",
-                namespace="",
-                executable="input_device_node",
-                name="input_device",
-                parameters=[{"IPD_new_terminal": IPD_new_terminal}]
-            ),
-# # 
-#     if IPD_new_terminal == 'true':
-#         node = ExecuteProcess(
-#             cmd=['gnome-terminal', '--', 'ros2', 'run', 'march_input_device', 'input_device_node'],
-#             output='screen'
-#         )    
-
+        package="march_input_device",
+        namespace="",
+        executable="input_device_node",
+        name="input_device",
+        parameters=[{"IPD_new_terminal": IPD_new_terminal}]
+    )  
+    
+    open_new_terminal = ExecuteProcess(
+        cmd=['gnome-terminal', '--', 'ros2', 'run', 'march_input_device', 'input_device_node'],
+        output='screen',
+        condition=conditions.IfCondition(IPD_new_terminal)
+    )
 
     return LaunchDescription(
-        [
-            arguments,
-            node
-        ]
+        arguments + [node, open_new_terminal]
     )
