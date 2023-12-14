3cece0e30c5a29dbb77603c4f429331692e91a81
Andrew <ahutani@gmail.com>
2023-12-14 12:22:19 +0100

    Clean up GaitPlanning class

--- ros2/src/march_gait_planning/include/march_gait_planning/gait_planning.hpp
+++ ros2/src/march_gait_planning/include/march_gait_planning/gait_planning.hpp
@@ -9,13 +9,13 @@
 
 class GaitPlanning {
     
-    public:
-    explicit GaitPlanning(); 
+public:
+    explicit GaitPlanning();
 
     // Setters
     void setStanceFoot(const int &new_stance_foot);
-    void setFootPositions(const std::array<double, 3> &new_left_foot_position, const std::array<double, 3> &new_right_foot_position); // set foot positions (l+r) and current stance leg 
-    void setGaitType(const exoState &new_gait_type); 
+    void setFootPositions(const std::array<double, 3> &new_left_foot_position, const std::array<double, 3> &new_right_foot_position);
+    void setGaitType(const exoState &new_gait_type);  
     void setBezierGait();
 
     // Getters
@@ -26,7 +26,7 @@ class GaitPlanning {
     exoState getGaitType() const; 
     std::vector<std::array<double, 3>> getFootEndPositions() const; 
 
-    private: 
+private: 
     exoState m_gait_type; 
     int m_current_stance_foot; 
     double m_step_size; 
@@ -34,5 +34,7 @@ class GaitPlanning {
     std::array<double, 3> m_current_right_foot_position; 
     std::vector<std::array<double, 4>> m_bezier_trajectory; 
     std::vector<std::array<double, 4>> m_first_step_trajectory; 
+
+    std::vector<std::array<double, 4>> processCSV(const std::string& filename);
     
 }; 
