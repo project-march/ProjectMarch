46d1c8b1749a781cb256c4424be2806b7dc62e7c
Femke Buiks <femke.buiks@icloud.com>
2023-12-20 10:02:22 +0100

    Update tests gait planning

--- ros2/src/march_gait_planning/test/gait_planning_test.cpp
+++ ros2/src/march_gait_planning/test/gait_planning_test.cpp
@@ -12,16 +12,20 @@ TEST_F(GaitPlanningTest, AssertObjectCreated){
 
 TEST_F(GaitPlanningTest, SetDoubleStanceCorrect){
     test_gait_planning.setStanceFoot(0);
+    ASSERT_NE(test_gait_planning.getCurrentStanceFoot(), 1) << "Current stance foot is wrong"; 
+    ASSERT_NE(test_gait_planning.getCurrentStanceFoot(), -1) << "Current stance foot is wrong"; 
     ASSERT_EQ(test_gait_planning.getCurrentStanceFoot(), 0) << "Current stance foot is not initialized correctly";
 }
 
 TEST_F(GaitPlanningTest, SetLeftStanceCorrect){
     test_gait_planning.setStanceFoot(-1);
+    ASSERT_NE(test_gait_planning.getCurrentStanceFoot(), 1) << "Current stance foot is wrong"; 
+    ASSERT_NE(test_gait_planning.getCurrentStanceFoot(), 0) << "Current stance foot is wrong"; 
     ASSERT_EQ(test_gait_planning.getCurrentStanceFoot(), -1) << "Current stance foot is not initialized correctly"; 
 }
 
 TEST_F(GaitPlanningTest, AssertErrorWrongInputSetStanceFoot){
-    // assert an error is caught when doubles, floats or strings are used as input
+    // assert an error is caught when wrong ints are used as input
 }
 
 TEST_F(GaitPlanningTest, CorrectSetFootPositions){
@@ -46,21 +50,17 @@ TEST_F(GaitPlanningTest, AssertErrorWrongInputSetGaitType){
     // assert error is caught when input is not an exoState
 }
 
-// TEST_F(GaitPlanningTest, SetBezierTest){
-//     ASSERT_FALSE(test_gait_planning.getTrajectory().empty()); 
-// }
+TEST_F(GaitPlanningTest, FirstStepTrajectoryGetterTest){
+    // test that correct member trajectory is got when double stance foot 
+    test_gait_planning.setStanceFoot(0); 
+    ASSERT_EQ(test_gait_planning.getTrajectory().size(), 20) << "First step trajectory was not returned whilst double stance"; 
+}
 
-// TEST_F(GaitPlanningTest, FirstStepTrajectoryGetterTest){
-//     // test that correct member trajectory is got when double stance foot 
-//     test_gait_planning.setStanceFoot(0); 
-//     ASSERT_EQ(test_gait_planning.getTrajectory().size(), 20) << "First step trajectory was not returned whilst double stance"; 
-// }
-
-// TEST_F(GaitPlanningTest, FullStepTrajectoryGetterTest){
-//     // test that correct member trajectory is got when double stance foot 
-//     test_gait_planning.setStanceFoot(1); 
-//     ASSERT_EQ(test_gait_planning.getTrajectory().size(), 40) << "Full step trajectory was not returned whilst no double stance"; 
-// }
+TEST_F(GaitPlanningTest, FullStepTrajectoryGetterTest){
+    // test that correct member trajectory is got when double stance foot 
+    test_gait_planning.setStanceFoot(1); 
+    ASSERT_EQ(test_gait_planning.getTrajectory().size(), 40) << "Full step trajectory was not returned whilst no double stance"; 
+}
 
 int main(int argc, char** argv){
     testing::InitGoogleTest(&argc, argv); 
