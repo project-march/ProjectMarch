825b9c5bb19a8022a0f68eab0678d065eb21f9fc
Andrew <ahutani@gmail.com>
2023-12-20 16:57:41 +0100

    Start implementing unit tests for the test setup

--- ros2/src/march_gait_planning/test/test_setup_gait_planning_test.cpp
+++ ros2/src/march_gait_planning/test/test_setup_gait_planning_test.cpp
@@ -3,9 +3,27 @@
 
 class TestSetupGaitPlanningTest : public testing::Test {
     protected: 
-    TestSetupGaitPlanning test_gait_planning = TestSetupGaitPlanning();
+    TestSetupGaitPlanning test_setup_gait_planning = TestSetupGaitPlanning();
 };
 
 TEST_F(TestSetupGaitPlanningTest, AssertObjectCreated){
     // assert creation of a gait planning object contains all necessary member variables
+}
+
+TEST_F(TestSetupGaitPlanningTest, LoadTrajectoryCorrectly) {
+    // Act
+    test_setup_gait_planning.loadTrajectoryFromCSV();
+    std::vector<double> trajectory = test_setup_gait_planning.getTrajectory();
+
+    // Assert
+    EXPECT_NEAR(trajectory[50], 0, 1e-6);  // Check that the middle value is close to 0 (since the trajectory is a sine wave)
+}
+
+TEST_F(TestSetupGaitPlanningTest, setGaitTypeCorrectly){
+    // Act
+    exoState state = exoState::Walk;
+    test_setup_gait_planning.setGaitType(state);
+
+    // Assert
+    ASSERT_EQ(state, test_setup_gait_planning.getGaitType());
 }
