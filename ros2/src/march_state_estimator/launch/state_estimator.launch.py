0f206a13ba4fb580169b11d872ff1d92b53fc603
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-01-08 22:11:08 +0100

    Create launch files for robot_description_node with hennie, and for state_estimator_node

---
+++ ros2/src/march_state_estimator/launch/state_estimator.launch.py
@@ -0,0 +1,18 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('march_state_estimator'), 'launch', 'robot_description_hennie.launch.py')]),
+        ),
+        Node(
+            package='march_state_estimator',
+            executable='state_estimator_node',
+            name='state_estimator',
+        ),
+    ])
