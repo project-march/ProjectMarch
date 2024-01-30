0208f017fdfb6e462c444c28e5f588a6cb557c26
Femke Buiks <femke.buiks@icloud.com>
2024-01-30 15:03:23 +0100

    Add test file

---
+++ ros2/src/march_footstep_planner/test/footstep_planner_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "march_footstep_planner/include/footstep_planner.hpp"
+
+class FootstepPlannerTest : public ::testing::Test {
+    public: 
+    FootstepPlannerTest() = default; 
+    ~FootstepPlannerTest() override = default; 
+};
