6d3d48fac8ebcff9acd6f54b7296013b051aa99e
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-01-30 13:19:18 +0000

    Implement torque converter in state estimator including dynamical joint acceleration, and test script

--- ros2/src/march_state_estimator/include/march_state_estimator/robot_mass.hpp
+++ ros2/src/march_state_estimator/include/march_state_estimator/robot_mass.hpp
@@ -8,12 +8,10 @@
 
 #include "march_state_estimator/robot_node.hpp"
 
-class RobotMass : public RobotNode
-{
+class RobotMass : public RobotNode {
 public:
-    RobotMass(const std::string & name, const uint64_t & id, const double & mass);
+    RobotMass(const std::string& name, const uint64_t& id, const double& mass);
     ~RobotMass() = default;
-
 };
 
-#endif  // MARCH_STATE_ESTIMATOR__ROBOT_MASS_HPP_
+#endif // MARCH_STATE_ESTIMATOR__ROBOT_MASS_HPP_
