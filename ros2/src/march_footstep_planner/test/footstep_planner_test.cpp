359750f6dc9a1d486c06172ed4f1725cb5b58f18
Femke Buiks <femke.buiks@icloud.com>
2024-01-30 15:28:42 +0100

    Initialize test class

--- ros2/src/march_footstep_planner/test/footstep_planner_test.cpp
+++ ros2/src/march_footstep_planner/test/footstep_planner_test.cpp
@@ -5,4 +5,15 @@ class FootstepPlannerTest : public ::testing::Test {
     public: 
     FootstepPlannerTest() = default; 
     ~FootstepPlannerTest() override = default; 
+    protected: 
+    void SetUp(){
+        m_footstep_planner = std::make_unique<FootstepPlanner>(); 
+    }
+    std::unique_ptr<FootstepPlanner> m_footstep_planner; 
 };
+
+// TEST_F(FootstepPlannerTest, test_compare_function)
+
+// TEST_F(FootstepPlannerTest, test_check_centroid_function)
+
+// TEST_F(FootstepPlannerTest, test_find_safe_plane_function)
