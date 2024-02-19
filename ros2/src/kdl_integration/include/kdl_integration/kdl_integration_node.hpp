677f07fd56588183706117d44550cc1a416a4964
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-02-20 00:49:49 +0100

    Extend jacobians for left and right legs

--- ros2/src/kdl_integration/include/kdl_integration/kdl_integration_node.hpp
+++ ros2/src/kdl_integration/include/kdl_integration/kdl_integration_node.hpp
@@ -6,6 +6,16 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "kdl_parser/kdl_parser.hpp"
+#include <kdl/chain.hpp>
+#include <kdl/chainjnttojacsolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/frames.hpp>
+#include <kdl/jntarray.hpp>
+#include <kdl/jacobian.hpp>
+
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
+#include <eigen3/Eigen/Dense>
 
 class KDLIntegrationNode : public rclcpp::Node
 {
@@ -15,6 +25,8 @@ class KDLIntegrationNode : public rclcpp::Node
 
   private:
     KDL::Tree kdl_tree_;
+    KDL::Chain kdl_chain_left_;
+    KDL::Chain kdl_chain_right_;
 };
 
 #endif  // KDL_INTEGRATION_NODE_HPP
--- ros2/src/kdl_integration/src/kdl_integration_node.cpp
+++ ros2/src/kdl_integration/src/kdl_integration_node.cpp
@@ -15,11 +15,75 @@ KDLIntegrationNode::KDLIntegrationNode()
     if (!kdl_parser::treeFromFile(urdf_model, kdl_tree_))
     {
         RCLCPP_ERROR(this->get_logger(), "Failed to construct kdl tree");
+        return;
     }
     else
     {
         RCLCPP_INFO(this->get_logger(), "Successfully constructed kdl tree");
     }
+
+    // Get the chain from the tree
+    if (!kdl_tree_.getChain("backpack", "left_ankle", kdl_chain_left_))
+    {
+        RCLCPP_ERROR(this->get_logger(), "Failed to construct kdl chain for left leg");
+        return;
+    }
+    else
+    {
+        RCLCPP_INFO(this->get_logger(), "Successfully constructed kdl chain for left leg");
+    }
+
+    if (!kdl_tree_.getChain("backpack", "right_ankle", kdl_chain_right_))
+    {
+        RCLCPP_ERROR(this->get_logger(), "Failed to construct kdl chain for right leg");
+        return;
+    }
+    else
+    {
+        RCLCPP_INFO(this->get_logger(), "Successfully constructed kdl chain for right leg");
+    }
+
+    // Get Jacobian for left leg
+    KDL::ChainJntToJacSolver jac_solver_left_(kdl_chain_left_);  // (kdl_chain_left_ is a private member of KDLIntegrationNode
+    KDL::Jacobian jac_left_;
+    KDL::JntArray q_left_;
+
+    jac_left_.resize(kdl_chain_left_.getNrOfJoints());
+    q_left_.resize(kdl_chain_left_.getNrOfJoints());
+
+    jac_solver_left_.JntToJac(q_left_, jac_left_);
+
+    // Convert Jacobian to Eigen
+    Eigen::MatrixXd jac_left_eigen = Eigen::MatrixXd::Zero(6, kdl_chain_left_.getNrOfJoints());
+    for (unsigned int i = 0; i < jac_left_.columns(); i++)
+    {
+        for (unsigned int j = 0; j < jac_left_.rows(); j++)
+        {
+            jac_left_eigen(j, i) = jac_left_(j, i);
+        }
+    }
+    std::cout << "Jacobian for left leg: " << std::endl << jac_left_eigen << std::endl;
+
+    // Get Jacobian for right leg
+    KDL::ChainJntToJacSolver jac_solver_right_(kdl_chain_right_);
+    KDL::Jacobian jac_right_;
+    KDL::JntArray q_right_;
+
+    jac_right_.resize(kdl_chain_right_.getNrOfJoints());
+    q_right_.resize(kdl_chain_right_.getNrOfJoints());
+
+    jac_solver_right_.JntToJac(q_right_, jac_right_);
+
+    // Convert Jacobian to Eigen
+    Eigen::MatrixXd jac_right_eigen = Eigen::MatrixXd::Zero(6, kdl_chain_right_.getNrOfJoints());
+    for (unsigned int i = 0; i < jac_right_.columns(); i++)
+    {
+        for (unsigned int j = 0; j < jac_right_.rows(); j++)
+        {
+            jac_right_eigen(j, i) = jac_right_(j, i);
+        }
+    }
+    std::cout << "Jacobian for right leg: " << std::endl << jac_right_eigen << std::endl;
 }
 
 int main(int argc, char * argv[])
