17c60544a9c36def95f8f0d7e1e2cd2c7438af19
Femke Buiks <femke.buiks@icloud.com>
2024-04-02 09:57:06 +0200

    Succesfully plot interpolated bezier trajectory

--- ros2/src/march_visualization/march_visualization/bezier_visualization_node.py
+++ ros2/src/march_visualization/march_visualization/bezier_visualization_node.py
@@ -11,12 +11,14 @@ class BezierVisualization(Node):
             '/bezier_visualization',  # Change this to the topic you want to subscribe to
             self.callback,
             10)
-        # self.visualization_subscriber  # prevent unused variable warning
+        self.visualization_subscriber  # prevent unused variable warning
 
     def callback(self, msg):
         try:
             self.figure = plt.figure("Bezier Curve")
-            # plt.plot(msg.poses[:, 0], msg.poses[:,1])        
+            x = [pose.position.x for pose in msg.poses]
+            z = [pose.position.z for pose in msg.poses]
+            plt.plot(x, z, color="green")
             self.get_logger().info("Tried to plot bezier points")
             plt.show()
         except Exception as e:
@@ -28,3 +30,6 @@ def main():
     rclpy.spin(bezier_visualization)
     bezier_visualization.destroy_node()
     rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
