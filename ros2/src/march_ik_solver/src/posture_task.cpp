12f624bf870b8438c08e14bd0547f5b1255b8bb9
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-05-03 11:14:27 +0200

    Change workspace dimension of Posture Task from 3 to 1 (pitch)

--- ros2/src/march_ik_solver/src/posture_task.cpp
+++ ros2/src/march_ik_solver/src/posture_task.cpp
@@ -18,15 +18,15 @@ PostureTask::PostureTask() : Task()
     m_task_name = "posture";
     m_reference_frame = "base_link";
     m_joint_indices = {LEFT_ANKLE_INDEX, RIGHT_ANKLE_INDEX};
-    setTaskM(m_joint_indices.size() * EUCLIDEAN_SIZE);
+    setTaskM(m_joint_indices.size());
 }
 
 void PostureTask::computeCurrentTaskCoordinates()
 {
     for (unsigned long int i = 0; i < m_joint_indices.size(); i++) {
         // Fixed-size template cannot be used with aliases
-        m_current_task.segment<3>(EUCLIDEAN_SIZE * i) = m_data->oMi[m_joint_indices[i]].rotation().eulerAngles(
-            ROTATION_YAW_INDEX, ROTATION_PITCH_INDEX, ROTATION_ROLL_INDEX);
+        m_current_task(i) = m_data->oMi[m_joint_indices[i]].rotation().eulerAngles(
+            ROTATION_YAW_INDEX, ROTATION_PITCH_INDEX, ROTATION_ROLL_INDEX).y();
     }
 }
 
@@ -34,10 +34,10 @@ void PostureTask::computeCurrentTaskJacobian()
 {
     for (unsigned long int i = 0; i < m_joint_indices.size(); i++) {
         // Fixed-size template cannot be used with aliases
-        pinocchio::Data::Matrix6x jacobian_joint(SE3_SIZE, m_model.nv);
+        pinocchio::Data::Matrix6x jacobian_joint(m_task_m, m_task_n);
         jacobian_joint.setZero();
 
         pinocchio::computeJointJacobian(m_model, *m_data, *m_current_joint_positions_ptr, m_joint_indices[i], jacobian_joint);
-        m_jacobian.block(EUCLIDEAN_SIZE * i, 0, EUCLIDEAN_SIZE, m_model.nv) = jacobian_joint.bottomRows(EUCLIDEAN_SIZE);
+        m_jacobian.block(i, 0, 1, m_task_n) = jacobian_joint.row(ROTATION_PITCH_INDEX + EUCLIDEAN_SIZE);
     }
 }
