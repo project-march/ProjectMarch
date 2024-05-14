d22b069eaf02ce247eca26d006ac3d60f5d2a683
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-05-14 17:51:19 +0200

    Create ROS2 Python package to optimize sensor fusion

---
+++ ros2/src/test/march_sensor_fusion_optimization/march_sensor_fusion_optimization/sensor_fusion_optimizer_node.py
@@ -0,0 +1,25 @@
+"""
+Project MARCH IX, 2023-2024
+Author: Alexander James Becoy @alexanderjamesbecoy
+"""
+
+import rclpy
+from rclpy.node import Node
+
+class SensorFusionOptimizerNode(Node):
+
+    def __init__(self) -> None:
+        super().__init__('sensor_fusion_optimizer')
+
+        self.get_logger().info('Sensor Fusion Optimizer Node has been initialized.')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SensorFusionOptimizerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros2/src/test/march_sensor_fusion_optimization/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'march_sensor_fusion_optimization'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Alexander James Becoy',
+    maintainer_email='alexanderjames.becoy@projectmarch.nl',
+    description='This package optimizes the sensor fusion in march state estimator via noise parameter tuning.',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'sensor_fusion_optimizer_node = march_sensor_fusion_optimization.sensor_fusion_optimizer_node:main'
+        ],
+    },
+)
