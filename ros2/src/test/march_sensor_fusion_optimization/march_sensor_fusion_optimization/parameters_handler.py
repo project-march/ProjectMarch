973df57b4949fc5395999f514df7a30e64b4bc34
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-05-16 23:35:35 +0200

    Implement parameters handler to load and save noise parameters as YAMLs including test

---
+++ ros2/src/test/march_sensor_fusion_optimization/march_sensor_fusion_optimization/parameters_handler.py
@@ -0,0 +1,31 @@
+"""
+Project MARCH IX, 2023-2024
+Author: Alexander James Becoy @alexanderjamesbecoy
+"""
+
+import yaml
+import os
+import sys
+import numpy as np
+
+class ParametersHandler:
+
+    def __init__(self, path: str) -> None:
+        self.path = path
+        self.parameters = self.load_parameters()
+
+    def load_parameters(self) -> dict:
+        with open(self.path, 'r') as file:
+            return yaml.safe_load(file)
+        
+    def get_parameter(self, type: str, parameter: str) -> dict:
+        return self.parameters['state_estimator']['ros__parameters']['noise_parameters'][type][parameter]
+    
+    def set_parameter(self, type: str, parameter: str, values: list) -> None:
+        self.parameters['state_estimator']['ros__parameters']['noise_parameters'][type][parameter] = values
+
+    def save_parameters(self, path: str = None) -> None:
+        if path is None:
+            path = self.path
+        with open(path, 'w') as file:
+            yaml.dump(self.parameters, file)
---
+++ ros2/src/test/march_sensor_fusion_optimization/test/test_parameters_handler.py
@@ -0,0 +1,85 @@
+"""
+Project MARCH IX, 2023-2024
+Author: Alexander James Becoy @alexanderjamesbecoy
+"""
+
+from march_sensor_fusion_optimization.parameters_handler import ParametersHandler
+
+filepath = 'test/mocks/dummy_parameters.yaml'
+
+def set_new_parameters_in_parameters_handler(ph: ParametersHandler) -> None:
+    ph.set_parameter('process_noise', 'linear_acceleration', [1e-6, 1e-6, 1e-6])
+    ph.set_parameter('process_noise', 'angular_velocity', [1e-6, 1e-6, 1e-6])
+    ph.set_parameter('process_noise', 'foot_position', [1e-6, 1e-6, 1e-6])
+    ph.set_parameter('process_noise', 'accelerometer_bias', [1e-6, 1e-6, 1e-6])
+    ph.set_parameter('process_noise', 'gyroscope_bias', [1e-6, 1e-6, 1e-6])
+    ph.set_parameter('process_noise', 'foot_slippage', [1e-6, 1e-6, 1e-6])
+    ph.set_parameter('observation_noise', 'foot_position', [1e6, 1e6, 1e6])
+    ph.set_parameter('observation_noise', 'foot_slippage', [1e6, 1e6, 1e6])
+    ph.set_parameter('observation_noise', 'joint_position', [1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6])
+
+def test_parameters_handler_load_parameters():
+    ph = ParametersHandler(filepath)
+    assert ph.load_parameters() == {'state_estimator': {
+                                        'ros__parameters': {
+                                            'noise_parameters': {
+                                                'process_noise': {
+                                                        'linear_acceleration': [1e-12, 1e-12, 1e-12],
+                                                        'angular_velocity': [1e-12, 1e-12, 1e-12],
+                                                        'foot_position': [1e-12, 1e-12, 1e-12],
+                                                        'accelerometer_bias': [1e-12, 1e-12, 1e-12],
+                                                        'gyroscope_bias': [1e-12, 1e-12, 1e-12],
+                                                        'foot_slippage': [1e-12, 1e-12, 1e-12],
+                                                    },
+                                                'observation_noise': {
+                                                        'foot_position': [1e3, 1e3, 1e3],
+                                                        'foot_slippage': [1e3, 1e3, 1e3],
+                                                        'joint_position': [1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3],
+                                                    }}}}}
+
+def test_parameters_handler_get_parameter():
+    ph = ParametersHandler(filepath)
+    assert ph.get_parameter('process_noise', 'linear_acceleration') == [1e-12, 1e-12, 1e-12]
+    assert ph.get_parameter('process_noise', 'angular_velocity') == [1e-12, 1e-12, 1e-12]
+    assert ph.get_parameter('process_noise', 'foot_position') == [1e-12, 1e-12, 1e-12]
+    assert ph.get_parameter('process_noise', 'accelerometer_bias') == [1e-12, 1e-12, 1e-12]
+    assert ph.get_parameter('process_noise', 'gyroscope_bias') == [1e-12, 1e-12, 1e-12]
+    assert ph.get_parameter('process_noise', 'foot_slippage') == [1e-12, 1e-12, 1e-12]
+    assert ph.get_parameter('observation_noise', 'foot_position') == [1e3, 1e3, 1e3]
+    assert ph.get_parameter('observation_noise', 'foot_slippage') == [1e3, 1e3, 1e3]
+    assert ph.get_parameter('observation_noise', 'joint_position') == [1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3, 1e3]
+
+def test_parameters_handler_set_parameter():
+    ph = ParametersHandler(filepath)
+    set_new_parameters_in_parameters_handler(ph)
+    assert ph.get_parameter('process_noise', 'linear_acceleration') == [1e-6, 1e-6, 1e-6]
+    assert ph.get_parameter('process_noise', 'angular_velocity') == [1e-6, 1e-6, 1e-6]
+    assert ph.get_parameter('process_noise', 'foot_position') == [1e-6, 1e-6, 1e-6]
+    assert ph.get_parameter('process_noise', 'accelerometer_bias') == [1e-6, 1e-6, 1e-6]
+    assert ph.get_parameter('process_noise', 'gyroscope_bias') == [1e-6, 1e-6, 1e-6]
+    assert ph.get_parameter('process_noise', 'foot_slippage') == [1e-6, 1e-6, 1e-6]
+    assert ph.get_parameter('observation_noise', 'foot_position') == [1e6, 1e6, 1e6]
+    assert ph.get_parameter('observation_noise', 'foot_slippage') == [1e6, 1e6, 1e6]
+    assert ph.get_parameter('observation_noise', 'joint_position') == [1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6]
+
+def test_parameters_handler_save_parameters():
+    ph = ParametersHandler(filepath)
+    set_new_parameters_in_parameters_handler(ph)
+    ph.save_parameters('test/mocks/result_test_parameters_handler_save_parameters.yaml')
+    ph = ParametersHandler('test/mocks/result_test_parameters_handler_save_parameters.yaml')
+    assert ph.load_parameters() == {'state_estimator': {
+                                        'ros__parameters': {
+                                            'noise_parameters': {
+                                                'process_noise': {
+                                                        'linear_acceleration': [1e-6, 1e-6, 1e-6],
+                                                        'angular_velocity': [1e-6, 1e-6, 1e-6],
+                                                        'foot_position': [1e-6, 1e-6, 1e-6],
+                                                        'accelerometer_bias': [1e-6, 1e-6, 1e-6],
+                                                        'gyroscope_bias': [1e-6, 1e-6, 1e-6],
+                                                        'foot_slippage': [1e-6, 1e-6, 1e-6],
+                                                    },
+                                                'observation_noise': {
+                                                        'foot_position': [1e6, 1e6, 1e6],
+                                                        'foot_slippage': [1e6, 1e6, 1e6],
+                                                        'joint_position': [1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6, 1e6]
+                                                    }}}}}
