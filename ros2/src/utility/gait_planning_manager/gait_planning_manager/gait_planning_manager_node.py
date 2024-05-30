f4c256e07a91e13f02412e1ac077f10e3e0e7527
Femke Buiks <femke.buiks@icloud.com>
2024-05-30 12:43:15 +0200

    remove get state and return dummy future in change_state to prevent logic loop

--- ros2/src/utility/gait_planning_manager/gait_planning_manager/gait_planning_manager_node.py
+++ ros2/src/utility/gait_planning_manager/gait_planning_manager/gait_planning_manager_node.py
@@ -4,7 +4,6 @@ from rclpy.executors import MultiThreadedExecutor
 from lifecycle_msgs.msg import State, Transition
 from lifecycle_msgs.srv import GetState, ChangeState
 from march_shared_msgs.msg import ExoMode 
-# from march_mode_machine.include.march_mode_machine import ExoMode 
 
 
 COLOR_GREEN = "\033[32m"
@@ -15,11 +14,8 @@ class ServiceClient(Node):
     def __init__(self): 
         super().__init__('gait_planning_manager_node')
 
-        self.angles_client_get_state = self.create_client(GetState, 'gait_planning_angles_node/get_state')
         self.angles_client_change_state = self.create_client(ChangeState, 'gait_planning_angles_node/change_state')
-        self.cartesian_client_get_state = self.create_client(GetState, 'gait_planning_cartesian_node/get_state')
         self.cartesian_client_change_state = self.create_client(ChangeState, 'gait_planning_cartesian_node/change_state')
-
         
         self.mode_subscriber = self.create_subscription(
             ExoMode, 'current_mode', self.mode_callback, 10)
@@ -28,6 +24,7 @@ class ServiceClient(Node):
 
         self.angles_active = True
         self.cartesian_active = True
+        self.future = True
 
         if not self.change_angles_state(ChangeState.Request().transition.TRANSITION_CONFIGURE):
             self.get_logger().info(COLOR_MAGENTA + 'Angles not able to configure' + RESET)
@@ -43,7 +40,6 @@ class ServiceClient(Node):
         self.get_logger().info('Gait Planning node manager has been initialized')
 
     def publish_mode_to_gait_planning(self, msg):
-        self.get_logger().info(COLOR_MAGENTA + 'publishing to gait planning!' + RESET)
         mode_msg = ExoMode()
         mode_msg.mode = msg.mode
         mode_msg.node_type = msg.node_type
@@ -54,20 +50,16 @@ class ServiceClient(Node):
 
         if msg.mode == 1:
             if self.angles_active and self.cartesian_active:
-                # self.get_logger().info(f'Angles currently in state {self.get_angles_state()}')
                 self.change_cartesian_state(ChangeState.Request().transition.TRANSITION_DEACTIVATE)
                 self.angles_active = True
                 self.cartesian_active = False
                 self.publish_mode_to_gait_planning(msg)
-                self.get_logger().info(COLOR_MAGENTA + 'sent mode to gait planning!' + RESET)
             elif self.angles_active and not self.cartesian_active:
                 self.get_logger().info('Letting the gait finish')
                 self.publish_mode_to_gait_planning(msg)
-                self.get_logger().info(COLOR_MAGENTA + 'sent mode to gait planning!' + RESET)
             elif not self.angles_active and self.cartesian_active:
                 self.get_logger().info('Letting the gait finish')
                 self.publish_mode_to_gait_planning(msg)
-                self.get_logger().info(COLOR_MAGENTA + 'sent mode to gait planning!' + RESET)
             else:
                 self.get_logger().error('Both gait planning nodes inactive!')
 
@@ -92,32 +84,6 @@ class ServiceClient(Node):
         else:
             self.get_logger().warn(f'Unknown node type: {msg.node_type}')
 
-    def get_angles_state(self) -> State:
-        self.get_logger().info('Getting Angles state...')
-        
-        while not self.angles_client_get_state.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Service not available, waiting again...')
-        
-        get_angles_state_request = GetState.Request()
-        get_angles_state_future = self.angles_client_get_state.call_async(get_angles_state_request)
-        rclpy.spin_until_future_complete(self, get_angles_state_future)
-
-        self.get_logger().info('Service has been called.')
-        return get_angles_state_future.result().current_state
-    
-    def get_cartesian_state(self) -> State:
-        self.get_logger().info('Getting Cartesian state...')
-        
-        while not self.cartesian_client_get_state.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Service not available, waiting again...')
-        
-        get_cartesian_state_request = GetState.Request()
-        get_cartesian_state_future = self.cartesian_client_get_state.call_async(get_cartesian_state_request)
-        rclpy.spin_until_future_complete(self, get_cartesian_state_future)
-
-        self.get_logger().info('Service has been called.')
-        return get_cartesian_state_future.result().current_state
-
     def change_angles_state(self, transition: str) -> bool: 
         while not self.angles_client_change_state.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Service not available, waiting again...')
@@ -125,10 +91,9 @@ class ServiceClient(Node):
         change_angles_state_request = ChangeState.Request()
         change_angles_state_request.transition = Transition(id=transition)
         change_angles_state_future = self.angles_client_change_state.call_async(change_angles_state_request)
-        rclpy.spin_until_future_complete(self, change_angles_state_future)
 
         self.get_logger().info('Service has been called.')
-        return change_angles_state_future.result().success
+        return self.future
 
     def change_cartesian_state(self, transition: str) -> bool: 
         while not self.cartesian_client_change_state.wait_for_service(timeout_sec=1.0):
@@ -137,10 +102,9 @@ class ServiceClient(Node):
         change_cartesian_state_request = ChangeState.Request()
         change_cartesian_state_request.transition = Transition(id=transition)
         change_cartesian_state_future = self.cartesian_client_change_state.call_async(change_cartesian_state_request)
-        rclpy.spin_until_future_complete(self, change_cartesian_state_future)
 
         self.get_logger().info('Service has been called.')
-        return change_cartesian_state_future.result().success
+        return self.future
 
 def main(args=None):
     rclpy.init(args=args)
