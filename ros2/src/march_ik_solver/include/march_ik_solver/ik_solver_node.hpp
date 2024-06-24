39fbb2753c8880a8c5664c62a51f55f2f47566ad
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-06-24 10:40:41 +0000

    Modify ik_solver to track according to previous desired joint positions, and use soft limits from joints_airgaiting.yaml

--- ros2/src/march_ik_solver/include/march_ik_solver/ik_solver_node.hpp
+++ ros2/src/march_ik_solver/include/march_ik_solver/ik_solver_node.hpp
@@ -6,21 +6,29 @@
 #ifndef IK_SOLVER__IK_SOLVER_NODE_HPP_
 #define IK_SOLVER__IK_SOLVER_NODE_HPP_
 
+#include "rclcpp/rclcpp.hpp"
+#include "march_ik_solver/ik_solver.hpp"
+#include "message_filters/subscriber.h"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/synchronizer.h"
+
 #include <functional>
 #include <future>
 #include <string>
 #include <vector>
 
 #include "geometry_msgs/msg/point.hpp"
-#include "march_ik_solver/ik_solver.hpp"
+#include "march_shared_msgs/msg/exo_mode.hpp"
 #include "march_shared_msgs/msg/iks_command.hpp"
 #include "march_shared_msgs/msg/iks_foot_positions.hpp"
 #include "march_shared_msgs/msg/iks_status.hpp"
 #include "march_shared_msgs/msg/state_estimation.hpp"
-#include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "trajectory_msgs/msg/joint_trajectory.hpp"
+#include "std_msgs/msg/header.hpp"
 #include "std_msgs/msg/float64_multi_array.hpp"
+
+typedef message_filters::sync_policies::ApproximateTime<march_shared_msgs::msg::ExoMode, march_shared_msgs::msg::StateEstimation> SyncPolicy_NewGait;
 
 class IKSolverNode : public rclcpp::Node {
 public:
@@ -28,9 +36,12 @@ public:
     ~IKSolverNode();
 
 private:
+    void clockCallback(const std_msgs::msg::Header::SharedPtr msg);
     void iksCommandCallback(const march_shared_msgs::msg::IksCommand::SharedPtr msg);
     void iksFootPositionsCallback(const march_shared_msgs::msg::IksFootPositions::SharedPtr msg);
     void stateEstimationCallback(const march_shared_msgs::msg::StateEstimation::SharedPtr msg);
+    void newGaitCallback(const march_shared_msgs::msg::ExoMode::SharedPtr exo_mode_msg, 
+        const march_shared_msgs::msg::StateEstimation::SharedPtr state_estimation_msg);
     void publishJointTrajectory();
     void publishDesiredJointPositions();
 
@@ -62,9 +73,15 @@ private:
     trajectory_msgs::msg::JointTrajectoryPoint m_joint_trajectory_point_prev;
 
     // ROS2 communication
+    message_filters::Subscriber<march_shared_msgs::msg::ExoMode> m_new_gait_exo_mode_sub;
+    message_filters::Subscriber<march_shared_msgs::msg::StateEstimation> m_new_gait_state_estimation_sub;
+    std::shared_ptr<message_filters::Synchronizer<SyncPolicy_NewGait>> m_new_gait_sync;
+
     rclcpp::Subscription<march_shared_msgs::msg::IksCommand>::SharedPtr m_ik_solver_command_sub;
     rclcpp::Subscription<march_shared_msgs::msg::IksFootPositions>::SharedPtr m_ik_solver_foot_positions_sub;
     rclcpp::Subscription<march_shared_msgs::msg::StateEstimation>::SharedPtr m_state_estimation_sub;
+    rclcpp::Subscription<std_msgs::msg::Header>::SharedPtr m_clock_sub;
+
     rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr m_joint_trajectory_pub;
     rclcpp::Publisher<march_shared_msgs::msg::IksStatus>::SharedPtr m_iks_status_pub;
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr m_desired_joint_positions_pub;
