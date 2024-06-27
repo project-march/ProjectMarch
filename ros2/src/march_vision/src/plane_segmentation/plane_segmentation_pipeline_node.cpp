4683ec40647000da29b006529a3ff15459f6febf
alexander.andonov <alexandar.andonov@projectmarch.nl>
2024-06-27 14:06:11 +0200

    Fix bug with reading config params

--- ros2/src/march_vision/src/plane_segmentation/plane_segmentation_pipeline_node.cpp
+++ ros2/src/march_vision/src/plane_segmentation/plane_segmentation_pipeline_node.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
      
-  auto nodeHandle = rclcpp::Node::make_shared("plane_segmentation_pipeline");
+  auto nodeHandle = rclcpp::Node::make_shared("plane_segmentation");
 
   nodeHandle->declare_parameter("frequency_plane_segmentation_pipeline", 15.0); 
   double frequency_plane_segmentation_pipeline;
