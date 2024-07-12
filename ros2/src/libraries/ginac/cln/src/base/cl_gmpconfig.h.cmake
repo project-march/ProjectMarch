ba30318b9cd043b7774a815dbaf6f487fafe1bef
Alexander James Becoy <bec_alexanderjames@outlook.com>
2024-07-12 12:26:42 +0200

    Remove ginac

--- ros2/src/libraries/ginac/cln/src/base/cl_gmpconfig.h.cmake
+++
@@ -1,7 +0,0 @@
-#ifndef CL_GMPCONFIG_H
-#define CL_GMPCONFIG_H
-
-#cmakedefine CL_USE_GMP 1
-
-#endif /* CL_GMPCONFIG_H */
-
--- ros2/src/libraries/ginac/cln/src/timing/cl_t_config.h.cmake
+++
@@ -1,6 +0,0 @@
-#ifndef CL_T_CONFIG_H
-#define CL_T_CONFIG_H
-#cmakedefine HAVE_GETTIMEOFDAY
-
-#endif /* CL_T_CONFIG_H */
-
