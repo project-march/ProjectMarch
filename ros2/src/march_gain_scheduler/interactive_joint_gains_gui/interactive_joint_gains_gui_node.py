c6323e5d21554afcf9c6d34f6293db54e6ad944c
Nike Celie <nikecelie@live.nl>
2024-07-19 15:26:32 +0200

    Update subscriber and gui display logic.

--- ros2/src/march_gain_scheduler/interactive_joint_gains_gui/interactive_joint_gains_gui_node.py
+++ ros2/src/march_gain_scheduler/interactive_joint_gains_gui/interactive_joint_gains_gui_node.py
@@ -7,6 +7,7 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from sensor_msgs.msg import JointState
 from march_shared_msgs.msg import JointGains, JointMotorControllerState
+from std_msgs.msg import Float64MultiArray
 
 CHECK_MESSAGE_STATUS_INTERVAL = 5000  # milliseconds
 
@@ -22,12 +23,14 @@ class InteractiveJointGainsGuiNode(Node, QWidget):
         self.temp_integral_gain = self.integral_gain
         self.temp_derivative_gain = self.derivative_gain
         self.gains_changed = False
+        self.is_publish_button_pressed = False
 
         self.init_ui()
 
         # ROS 2 Publishers and Subscribers
         self.pub = self.create_publisher(JointGains, 'joint_gains', 10)
         self.sub_joint_state = self.create_subscription(JointState, 'joint_states', self.joint_state_callback, 10)
+        self.sub_joint_position_commands = self.create_subscription(Float64MultiArray, '/march_joint_position_controller/commands', self.joint_position_commands_callback, 10)
         self.sub_motor_controller_state = self.create_subscription(JointMotorControllerState, 'joint_motor_controller_state', self.joint_motor_controller_state_callback, 10)
 
         # Timer to check for received messages
@@ -36,6 +39,7 @@ class InteractiveJointGainsGuiNode(Node, QWidget):
         self.timer.start(CHECK_MESSAGE_STATUS_INTERVAL)
         self.joint_state_message_received = False
         self.motor_controller_message_received = False
+        self.joint_position_commands_received = False
 
     def init_ui(self):
         self.layout = QVBoxLayout()
@@ -48,15 +52,15 @@ class InteractiveJointGainsGuiNode(Node, QWidget):
         self.layout.addWidget(self.publish_button)
 
         # Proportional Gain Label
-        self.pg_label = QLabel(f'Proportional Gain: {self.proportional_gain}', self)
+        self.pg_label = QLabel(f'Current proportional gain: {self.proportional_gain}', self)
         self.layout.addWidget(self.pg_label)
 
         # Integral Gain Label
-        self.ig_label = QLabel(f'Integral Gain: {self.integral_gain}', self)
+        self.ig_label = QLabel(f'Current integral gain: {self.integral_gain}', self)
         self.layout.addWidget(self.ig_label)
 
         # Derivative Gain Label
-        self.dg_label = QLabel(f'Derivative Gain: {self.derivative_gain}', self)
+        self.dg_label = QLabel(f'Current derivative gain: {self.derivative_gain}', self)
         self.layout.addWidget(self.dg_label)
 
         # Proportional Gain Input Field
@@ -85,46 +89,79 @@ class InteractiveJointGainsGuiNode(Node, QWidget):
     def update_proportional_gain(self, value):
         try:
             self.temp_proportional_gain = float(value)
-            self.pg_label.setText(f'Proportional Gain: {self.temp_proportional_gain}')
         except ValueError:
             self.pg_label.setText('Invalid input for Proportional Gain')
 
     def update_integral_gain(self, value):
         try:
             self.temp_integral_gain = float(value)
-            self.ig_label.setText(f'Integral Gain: {self.temp_integral_gain}')
         except ValueError:
             self.ig_label.setText('Invalid input for Integral Gain')
 
     def update_derivative_gain(self, value):
         try:
             self.temp_derivative_gain = float(value)
-            self.dg_label.setText(f'Derivative Gain: {self.temp_derivative_gain}')
         except ValueError:
             self.dg_label.setText('Invalid input for Derivative Gain')
 
     def joint_state_callback(self, msg):
         self.label.setText(f'Received Joint State Data')
         self.joint_state_message_received = True
+        if self.joint_state_message_received:
+            self.all_joint_positions = self.get_all_joint_positions(msg)
 
     def joint_motor_controller_state_callback(self, msg):
         self.label.setText(f'Received Motor Controller State Data')
         self.motor_controller_message_received = True
+        if self.motor_controller_message_received:
+            self.joint_currents = self.get_all_joint_currents(msg)
+
+    def joint_position_commands_callback(self, msg):
+        self.label.setText(f'Received Joint Position Commands Data')
+        self.joint_position_commands_received = True
+        if self.joint_position_commands_received:
+            self.joint_position_commands = self.get_all_joint_position_commands(msg)
+
+    def get_all_joint_positions(self, msg):
+        joint_position_tuples = []
+        self.all_joint_names = []
+        for i in range(len(msg.name)):
+            if 'ie' not in msg.name[i]:
+                self.all_joint_names.append(msg.name[i])
+                joint_position_tuples.append((msg.name[i], msg.position[i]))
+        return joint_position_tuples
+
+    def get_all_joint_currents(self, msg):
+        joint_current_tuples = [] 
+        for i in range(len(msg.joint_name)):
+            if 'ie' not in msg.joint_name[i]:
+                joint_current_tuples.append((msg.joint_name[i], msg.motor_current[i]))
+        return joint_current_tuples
+    
+    def get_all_joint_position_commands(self, msg):
+        joint_position_commands = []
+        # Assuming that the joint names are in the same order as the joint position commands
+        for i in range(len(msg.data)):
+            joint_position_commands.append(self.all_joint_names[i], msg.data[i])
+        return joint_position_commands
 
     def publish_new_gains(self):
+        self.is_publish_button_pressed = True
         self.publish_gains()
-        
-        # Update the actual gain attributes
-        self.proportional_gain = self.temp_proportional_gain
-        self.integral_gain = self.temp_integral_gain
-        self.derivative_gain = self.temp_derivative_gain
-        
-        # Update the labels to reflect the new gains
-        self.pg_label.setText(f'Proportional Gain: {self.proportional_gain:.2f}')
-        self.ig_label.setText(f'Integral Gain: {self.integral_gain:.2f}')
-        self.dg_label.setText(f'Derivative Gain: {self.derivative_gain:.2f}')
+
+        if self.gains_changed and self.is_publish_button_pressed:
+            # Update the actual gain attributes
+            self.proportional_gain = self.temp_proportional_gain
+            self.integral_gain = self.temp_integral_gain
+            self.derivative_gain = self.temp_derivative_gain
+
+            # Update the labels to reflect the new gains
+            self.pg_label.setText(f'Proportional Gain: {self.proportional_gain:.2f}')
+            self.ig_label.setText(f'Integral Gain: {self.integral_gain:.2f}')
+            self.dg_label.setText(f'Derivative Gain: {self.derivative_gain:.2f}')
 
         self.gains_changed = False
+        self.is_publish_button_pressed = False
         self.publish_button.setEnabled(self.gains_changed)
 
     def on_gain_changed(self):
@@ -144,7 +181,9 @@ class InteractiveJointGainsGuiNode(Node, QWidget):
         if not self.joint_state_message_received:
             message += 'No joint state data received.\n'
         if not self.motor_controller_message_received:
-            message += 'No motor controller state data received.'
+            message += 'No motor controller state data received.\n'
+        if not self.joint_position_commands_received:
+            message += 'No joint position commands data received'
         self.label.setText(message if message else 'All data received.')
 
 def main(args=None):
