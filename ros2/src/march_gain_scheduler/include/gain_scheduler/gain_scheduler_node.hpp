e88a636fe3c8cb84f0f2fc88a35fdac72b9fb28c
Mick de Groot <m.degroot9000@gmail.com>
2024-07-19 15:47:43 +0200

    created subscriber to the robot description

--- ros2/src/march_gain_scheduler/include/gain_scheduler/gain_scheduler_node.hpp
+++ ros2/src/march_gain_scheduler/include/gain_scheduler/gain_scheduler_node.hpp
@@ -4,7 +4,9 @@
 #include "march_shared_msgs/msg/joint_gains.hpp"     
 #include "march_shared_msgs/msg/exo_mode.hpp"
 #include "march_shared_msgs/msg/state_estimation.hpp"
-class GainSchedulerNode:public rclcpp::Node {
+#include <std_msgs/msg/string.hpp>
+
+class GainSchedulerNode : public rclcpp::Node {
     public: 
         explicit GainSchedulerNode();
 
@@ -13,15 +15,18 @@ class GainSchedulerNode:public rclcpp::Node {
         rclcpp::Subscription<march_shared_msgs::msg::ExoMode>::SharedPtr m_mode_subscriber;
         rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr m_joint_states_subscriber;
         rclcpp::Subscription<march_shared_msgs::msg::StateEstimation>::SharedPtr m_state_estimation_subscriber;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr m_robot_description_subscriber;
         sensor_msgs::msg::JointState::SharedPtr m_latest_joint_state;
         rclcpp::TimerBase::SharedPtr m_timer; 
         uint8_t m_current_stance_leg;
+        std_msgs::msg::String::SharedPtr m_robot_description;
 
         GainScheduler m_gain_scheduler;
 
         void currentModeCallback(const march_shared_msgs::msg::ExoMode::SharedPtr msg);   
         void jointStatesCallback(const sensor_msgs::msg::JointState::SharedPtr msg);
         void stateEstimationCallback(const march_shared_msgs::msg::StateEstimation::SharedPtr msg);
+        void robotDescriptionCallback(const std_msgs::msg::String::SharedPtr msg);
         joints_with_gains getJointsWithGains();
         void publishJointsWithGains();
 };
