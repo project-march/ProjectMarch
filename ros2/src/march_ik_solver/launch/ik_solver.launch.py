4e89b1fa9f0484df8731c063b4ec54f798bcf7f6
Nike Celie <nikecelie@live.nl>
2024-08-28 10:37:45 +0000

    Use hardware limits for IKS instead of URDF defined limits.

--- ros2/src/march_ik_solver/launch/ik_solver.launch.py
+++ ros2/src/march_ik_solver/launch/ik_solver.launch.py
@@ -37,24 +37,55 @@ def generate_launch_description():
         condition=IfCondition(test),
     )
 
-    # Load the soft joint limits from the robot config
-    joints_airgaiting_config_filepath = os.path.join(
+    # Load the exo hardware configuration file
+    exo_hardware_config_filepath = os.path.join(
                 get_package_share_directory('march_hardware_builder'),
                 'robots',
                 'hardware_izzy.yaml'
             )
-    joints_airgaiting_config = yaml.safe_load(open(joints_airgaiting_config_filepath, 'r'))
-    actuator_names = [list(actuator.keys())[0] for actuator in joints_airgaiting_config['march9']['joints']]
-    actuators_info = dict()
-    for actuator_idx, actuator in enumerate(actuator_names):
-        actuator_info = joints_airgaiting_config['march9']['joints'][actuator_idx][actuator]['motor_controller']['absoluteEncoder']
-        actuators_info[actuator] = {
-            'name': actuator,
-            'soft_lower_limit': np.rad2deg(actuator_info['lowerSoftLimitMarginRad']),
-            'soft_upper_limit': np.rad2deg(actuator_info['upperSoftLimitMarginRad']),
+
+    # Use context manager for file operations
+    with open(exo_hardware_config_filepath, 'r') as file:
+        exo_hardware_config = yaml.safe_load(file)
+
+    # Extract joint names
+    joint_names = [list(joint.keys())[0] for joint in exo_hardware_config['march9']['joints']]
+ 
+    # Calculate joint information and store it in a dictionary
+    joints_info = {}
+    for joint_idx, joint in enumerate(joint_names):
+        joint_info = exo_hardware_config['march9']['joints'][joint_idx][joint]['motor_controller']['absoluteEncoder']
+        joints_info[joint] = {
+            'name': joint,
+            'cpr_absolute': 1 << joint_info['resolution'],
+            'direction': joint_info['direction'],
+            'min_position_iu': joint_info['minPositionIU'],
+            'max_position_iu': joint_info['maxPositionIU'],
+            'zero_position_iu': joint_info['zeroPositionIU'],
+            'lower_soft_limit': np.rad2deg(joint_info['lowerSoftLimitMarginRad']),
+            'upper_soft_limit': np.rad2deg(joint_info['upperSoftLimitMarginRad']),
         }
-    soft_upper_limits = [actuators_info[actuator]['soft_upper_limit'] for actuator in actuators_info]
-    soft_lower_limits = [actuators_info[actuator]['soft_lower_limit'] for actuator in actuators_info]
+    
+    # Calculate min and max position degrees for each joint
+    min_position_degrees = [
+        360 * (info['min_position_iu'] - info['zero_position_iu']) / info['cpr_absolute']
+        for info in joints_info.values()
+    ]
+    
+    max_position_degrees = [
+        360 * (info['max_position_iu'] - info['zero_position_iu']) / info['cpr_absolute']
+        for info in joints_info.values()
+    ]
+    
+    # Adjust positions based on direction
+    for i, info in enumerate(joints_info.values()):
+        if info['direction'] == -1:
+            min_position_degrees[i], max_position_degrees[i] = -max_position_degrees[i], -min_position_degrees[i]
+    
+    # Extract lower and upper error soft limits
+    lower_soft_limits = [info['lower_soft_limit'] for info in joints_info.values()]
+    upper_soft_limits = [info['upper_soft_limit'] for info in joints_info.values()]
+    
 
     return LaunchDescription([
         Node(
@@ -65,9 +96,11 @@ def generate_launch_description():
             parameters=[
                 ik_solver_config,
                 {'state_estimator_timer_period': state_estimator_clock_period},
-                {'actuator_names': actuator_names},
-                {'actuator_soft_upper_limits': soft_upper_limits},
-                {'actuator_soft_lower_limits': soft_lower_limits},
+                {'joint_names': joint_names},
+                {'joint_min_position_degrees': min_position_degrees},
+                {'joint_max_position_degrees': max_position_degrees},
+                {'joint_lower_soft_limits': lower_soft_limits},
+                {'joint_upper_soft_limits': upper_soft_limits},
             ],
         ),
         Node(
